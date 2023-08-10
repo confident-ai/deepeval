85d3e4c0c2e8131c4b8ab05c134a0e6cda5ecb5e
Jacky Wong <colabdog@gmail.com>
2023-08-10 18:53:40 +1000

    update evals

--- evals/__init__.py
+++ evals/__init__.py
@@ -1,1 +1,3 @@
+from .client import Evaluator
+
 __version__: str = "0.0.0"
---
+++ setup.py
@@ -0,0 +1,13 @@
+from evals import __version__
+from setuptools import setup, find_packages
+
+setup(
+    name="Evals",
+    version=__version__,
+    url="https://github.com/mr-gpt/evals",
+    author="Twilix",
+    author_email="jacky@twilix.io",
+    description="Eval",
+    packages=find_packages(),
+    install_requires=["requests"],
+)
