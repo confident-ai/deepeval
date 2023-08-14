ad149b7108d386fdc336f1d44c397a175a09eede
Jacky Wong <colabdog@gmail.com>
2023-08-14 15:32:50 +1000

    add example testings uite

--- evals/__init__.py
+++ evals/__init__.py
@@ -1,4 +1,5 @@
 from .client import Evaluator
 from .query_generator import QueryGenerator
+from .test_utils import assert_exact_match, TestEvalCase
 
 __version__: str = "0.1.0"
--- evals/test_utils.py
+++ evals/test_utils.py
@@ -1,6 +1,35 @@
+import functools
+import time
+
+
 def assert_exact_match(text_input, text_output):
     assert text_input == text_output, f"{text_output} != {text_input}"
 
 
 class TestEvalCase:
     pass
+
+
+def timing_decorator(func):
+    @functools.wraps(func)  # Preserves the original function's metadata
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time:.4f} seconds to execute.")
+        return result
+
+    return wrapper
+
+
+def tags(tags: list):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print(f"Tags are: {tags}")
+            return result
+
+        return wrapper
+
+    return decorator
