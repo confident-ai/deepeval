b2e0bfe4268c6ba61e6ef4418d2e7ebdcdf5d754
Jacky Wong <colabdog@gmail.com>
2023-08-15 05:30:48 +1000

    update the test case

--- evals/test_case.py
+++ evals/test_case.py
@@ -1,5 +1,6 @@
 import hashlib
 from typing import Any, List
+from collections import UserList
 from .metrics.metric import Metric
 from .metrics.randomscore import RandomMetric
 from .metrics.entailment_metric import EntailmentScore
@@ -24,3 +25,33 @@ class TestCase:
             self.id = hashlib.md5(id_string.encode()).hexdigest()
         else:
             self.id = id
+
+
+class TestCases(UserList):
+    """A list of test cases for easy access"""
+
+    @classmethod
+    def from_csv(
+        self,
+        csv_filename: str,
+        input_column: str,
+        expected_output_column: str,
+        id_column: str = None,
+        metrics: List[Metric] = None,
+    ):
+        import pandas as pd
+
+        df = pd.read_csv(csv_filename)
+        inputs = df[input_column].values
+        expected_outputs = df[expected_output_column].values
+        if id_column is not None:
+            ids = df[id_column].values
+        for i, input in enumerate(inputs):
+            self.data.append(
+                TestCase(
+                    input=input,
+                    expected_output=expected_outputs[i],
+                    metrics=metrics,
+                    id=ids[i],
+                )
+            )
