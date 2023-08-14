382ec0a2aa7787abea1c6b059c3db5a626a6373d
Jacky Wong <colabdog@gmail.com>
2023-08-14 12:07:26 +1000

    fix utils

--- evals/metric.py
+++ evals/metric.py
@@ -46,11 +46,12 @@ class CohereRerankerMetric(Metric):
 
 class EntailmentMetric(Metric):
     def __init__(self, model_name: str = "cross-encoder/nli-deberta-base"):
+        # We use a smple cross encoder model
         from sentence_transformers import CrossEncoder
 
         self.model = CrossEncoder(model_name)
 
-    def measure(self, a, b):
+    def measure(self, a: str, b: str):
         scores = self.model.predict([(a, b)])
         # https://huggingface.co/cross-encoder/nli-deberta-base
         # label_mapping = ["contradiction", "entailment", "neutral"]
