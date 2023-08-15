782a3953ce71fc29d2ed8e2593a6f8144cc94165
Jacky Wong <colabdog@gmail.com>
2023-08-15 21:23:15 +1000

    add setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from deepeval._version import __version__
 setup(
     name="deepeval",
     version=__version__,
-    url="https://github.com/mr-gpt/evals",
-    author="Twilix",
+    url="https://github.com/mr-gpt/deepeval",
+    author="Confident AI",
     author_email="jacky@twilix.io",
     description="Deep eval provides evaluation platform to accelerate development of LLMs and Agents",
     packages=find_packages(),
