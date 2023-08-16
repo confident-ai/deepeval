6c00fdca5732c3fb28af3581d7d5de4cf6aae3b8
Jacky Wong <colabdog@gmail.com>
2023-08-16 19:08:39 +1000

    add new testing file

---
+++ tests/test_quickstart.py
@@ -0,0 +1,27 @@
+"""This is the ideal user flow
+"""
+import pytest
+from deepeval.test_utils import assert_llm_output
+from deepeval.metrics.bertscore_metric import BertScoreMetric
+
+
+def generate_llm_output(input: str):
+    expected_output = "Our customer success phone line is 1200-231-231."
+    return expected_output
+
+
+def test_llm_output():
+    input = "What is the customer success phone line?"
+    expected_output = "Our customer success phone line is 1200-231-231."
+    output = generate_llm_output(input)
+    assert_llm_output(output, expected_output, metric="entailment")
+    assert_llm_output(output, expected_output, metric="bertscore")
+
+
+def test_llm_output_custom():
+    input = "What is the customer success phone line?"
+    expected_output = "Dogs and cats love to walk around the beach."
+    output = generate_llm_output(input)
+    metric = BertScoreMetric(minimum_score=0.98)
+    with pytest.raises(AssertionError):
+        assert_llm_output(output, expected_output, metric=metric)
