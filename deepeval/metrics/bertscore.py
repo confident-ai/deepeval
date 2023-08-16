5b5f90cd3935e1608f50478c80998d3eb74741d9
Jacky Wong <colabdog@gmail.com>
2023-08-16 14:42:25 +1000

    add black

--- deepeval/metrics/bertscore.py
+++ deepeval/metrics/bertscore.py
@@ -1,8 +1,34 @@
 from .metric import Metric
+from typing import Optional
+import numpy as np
 
+def cosine_similarity(vector_a, vector_b):
+    dot_product = np.dot(vector_a, vector_b)
+    norm_a = np.linalg.norm(vector_a)
+    norm_b = np.linalg.norm(vector_b)
+    similarity = dot_product / (norm_a * norm_b)
+    return similarity
 
 class BertScore(Metric):
     """basic implementation of BertScore"""
+    def __init__(
+            self, 
+            model_name: Optional[str]="sentence-transformers/all-mpnet-base-v2",
+            minimum_score: float=0.7
+        ):
+        from sentence_transformers import SentenceTransformer
+        self.model_name = model_name
+        self.model = SentenceTransformer(self.model_name)
+        self.minimum_score = minimum_score
+
+    def _vectorize(self, text_a: str, text_b: str):
+        vectors = self.model.encode([text_a, text_b])
+        return vectors
 
     def measure(self, a: str, b: str):
-        raise NotImplementedError
+        vectors = self._vectorize(a, b)
+        self.score = cosine_similarity(vectors[0], vectors[1])
+        return self.score
+
+    def is_successful(self) -> bool:
+        return self.score >= self.minimum_score
