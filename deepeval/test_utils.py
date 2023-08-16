7d788bdb4ba1d2ea08c08501fe2d80c1f17a8e23
Jacky Wong <colabdog@gmail.com>
2023-08-16 17:07:54 +1000

    update docs

--- deepeval/test_utils.py
+++ deepeval/test_utils.py
@@ -2,7 +2,7 @@ import functools
 import time
 from .metrics.randomscore import RandomMetric
 from .metrics.metric import Metric
-from .metrics.BertScoreMetric import BertScoreMetric
+from .metrics.bertscore import BertScoreMetric
 from .metrics.entailment_metric import EntailmentScoreMetric
 from typing import Any
 
