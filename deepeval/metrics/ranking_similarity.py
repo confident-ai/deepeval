f300d421b7348c5820737122eefa147ed7ab75fc
Jacky Wong <colabdog@gmail.com>
2023-08-20 20:59:25 +1000

    add ranking similarity metric

---
+++ deepeval/metrics/ranking_similarity.py
@@ -0,0 +1,186 @@
+# Testing for ranking similarity
+import numpy as np
+import asyncio
+from typing import List, Optional, Union, Any
+from tqdm import tqdm
+from .metric import Metric
+
+
+class RBO:
+    """
+    This class will include some similarity measures between two different
+    ranked lists.
+    """
+
+    def __init__(
+        self,
+        S: Union[List, np.ndarray],
+        T: Union[List, np.ndarray],
+        verbose: bool = False,
+    ) -> None:
+        """
+        Initialize the object with the required lists.
+        Examples of lists:
+        S = ["a", "b", "c", "d", "e"]
+        T = ["b", "a", 1, "d"]
+
+        Both lists reflect the ranking of the items of interest, for example,
+        list S tells us that item "a" is ranked first, "b" is ranked second,
+        etc.
+
+        Args:
+            S, T (list or numpy array): lists with alphanumeric elements. They
+                could be of different lengths. Both of the them should be
+                ranked, i.e., each element"s position reflects its respective
+                ranking in the list. Also we will require that there is no
+                duplicate element in each list.
+            verbose: If True, print out intermediate results. Default to False.
+        """
+
+        assert type(S) in [list, np.ndarray]
+        assert type(T) in [list, np.ndarray]
+
+        assert len(S) == len(set(S))
+        assert len(T) == len(set(T))
+
+        self.S, self.T = S, T
+        self.N_S, self.N_T = len(S), len(T)
+        self.verbose = verbose
+        self.p = 0.5  # just a place holder
+
+    def assert_p(self, p: float) -> None:
+        """Make sure p is between (0, 1), if so, assign it to self.p.
+
+        Args:
+            p (float): The value p.
+        """
+        assert 0.0 < p < 1.0, "p must be between (0, 1)"
+        self.p = p
+
+    def _bound_range(self, value: float) -> float:
+        """Bounds the value to [0.0, 1.0]."""
+
+        try:
+            assert 0 <= value <= 1 or np.isclose(1, value)
+            return value
+
+        except AssertionError:
+            print("Value out of [0, 1] bound, will bound it.")
+            larger_than_zero = max(0.0, value)
+            less_than_one = min(1.0, larger_than_zero)
+            return less_than_one
+
+    def rbo(
+        self,
+        k: Optional[float] = None,
+        p: float = 1.0,
+        ext: bool = False,
+    ) -> float:
+        """
+        This the weighted non-conjoint measures, namely, rank-biased overlap.
+        Unlike Kendall tau which is correlation based, this is intersection
+        based.
+        The implementation if from Eq. (4) or Eq. (7) (for p != 1) from the
+        RBO paper: http://www.williamwebber.com/research/papers/wmz10_tois.pdf
+
+        If p = 1, it returns to the un-bounded set-intersection overlap,
+        according to Fagin et al.
+        https://researcher.watson.ibm.com/researcher/files/us-fagin/topk.pdf
+
+        The fig. 5 in that RBO paper can be used as test case.
+        Note there the choice of p is of great importance, since it
+        essentially control the "top-weightness". Simply put, to an extreme,
+        a small p value will only consider first few items, whereas a larger p
+        value will consider more items. See Eq. (21) for quantitative measure.
+
+        Args:
+            k: The depth of evaluation.
+            p: Weight of each agreement at depth d:
+                p**(d-1). When set to 1.0, there is no weight, the rbo returns
+                to average overlap.
+            ext: If True, we will extrapolate the rbo, as in Eq. (23).
+
+        Returns:
+            The rbo at depth k (or extrapolated beyond).
+        """
+
+        if not self.N_S and not self.N_T:
+            return 1  # both lists are empty
+
+        if not self.N_S or not self.N_T:
+            return 0  # one list empty, one non-empty
+
+        if k is None:
+            k = float("inf")
+        k = min(self.N_S, self.N_T, k)
+
+        # initialize the agreement and average overlap arrays
+        A, AO = [0] * k, [0] * k
+        if p == 1.0:
+            weights = [1.0 for _ in range(k)]
+        else:
+            self.assert_p(p)
+            weights = [1.0 * (1 - p) * p**d for d in range(k)]
+
+        # using dict for O(1) look up
+        S_running, T_running = {self.S[0]: True}, {self.T[0]: True}
+        A[0] = 1 if self.S[0] == self.T[0] else 0
+        AO[0] = weights[0] if self.S[0] == self.T[0] else 0
+
+        for d in tqdm(range(1, k), disable=~self.verbose):
+
+            tmp = 0
+            # if the new item from S is in T already
+            if self.S[d] in T_running:
+                tmp += 1
+            # if the new item from T is in S already
+            if self.T[d] in S_running:
+                tmp += 1
+            # if the new items are the same, which also means the previous
+            # two cases did not happen
+            if self.S[d] == self.T[d]:
+                tmp += 1
+
+            # update the agreement array
+            A[d] = 1.0 * ((A[d - 1] * d) + tmp) / (d + 1)
+
+            # update the average overlap array
+            if p == 1.0:
+                AO[d] = ((AO[d - 1] * d) + A[d]) / (d + 1)
+            else:  # weighted average
+                AO[d] = AO[d - 1] + weights[d] * A[d]
+
+            # add the new item to the running set (dict)
+            S_running[self.S[d]] = True
+            T_running[self.T[d]] = True
+
+        if ext and p < 1:
+            return self._bound_range(AO[-1] + A[-1] * p**k)
+
+        return self._bound_range(AO[-1])
+
+
+class RankingSimilarity(Metric):
+    def __init__(self, success_threshold: float = 0.1):
+        self.success_threshold = success_threshold
+
+    def __call__(self, list_1: List[Any], list_2: List[Any]):
+        score = self.measure(list_1, list_2)
+        if self._is_send_okay():
+            asyncio.create_task(
+                self._send_to_server(
+                    entailment_score=score,
+                    query=str(list_1),
+                    output=str(list_2),
+                )
+            )
+        return score
+
+    def measure(self, list_1, list_2):
+        scorer = RBO(list_1, list_2)
+        result = scorer.rbo(list_1, list_2, p=0.9, ext=True)
+        self.success = result > self.success_threshold
+        return result
+
+    def is_successful(self):
+        return self.success
