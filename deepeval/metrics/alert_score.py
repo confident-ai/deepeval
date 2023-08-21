6fdb032cea7dc2af4bcdad0f38ecd7ab5f09e545
Jacky Wong <colabdog@gmail.com>
2023-08-21 20:37:08 +1000

    update alert score

--- deepeval/metrics/alert_score.py
+++ deepeval/metrics/alert_score.py
@@ -11,17 +11,17 @@ class AlertScore(Metric):
         self.entailment_metric = EntailmentScoreMetric()
         self.answer_relevancy = AnswerRelevancy()
 
-    def __call__(self, gemerated_text: str, expected_output: str, context: str):
-        score = self.measure(gemerated_text, expected_output, context)
+    def __call__(self, generated_text: str, expected_output: str, context: str):
+        score = self.measure(generated_text, expected_output, context)
         return score
 
-    def measure(self, gemerated_text: str, expected_output: str, context: str) -> float:
+    def measure(self, generated_text: str, expected_output: str, context: str) -> float:
         entailment_score = self.entailment_metric.measure(
-            gemerated_text,
+            generated_text,
             context,
         )
         answer_relevancy_score = self.answer_relevancy.measure(
-            gemerated_text, expected_output
+            generated_text, expected_output
         )
         alert_score = min(entailment_score, answer_relevancy_score)
         self.success = alert_score > self.success_threshold
@@ -35,10 +35,10 @@ class AlertScore(Metric):
 
 
 def assert_alert_score(
-    gemerated_text: str, expected_output: str, success_threshold: float = 0.5
+    generated_text: str, expected_output: str, success_threshold: float = 0.5
 ):
     metric = AlertScore(success_threshold=success_threshold)
     score = metric.measure(
-        gemerated_text=gemerated_text, expected_output=expected_output
+        generated_text=generated_text, expected_output=expected_output
     )
     assert metric.is_successful(), f"Metric is not conceptually similar - got {score}"
