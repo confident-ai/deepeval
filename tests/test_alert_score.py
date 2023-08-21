5a38d6629b18128f60429b04f77bd2f6b39a6a50
Jacky Wong <colabdog@gmail.com>
2023-08-21 21:22:11 +1000

    update this to an overall metric

--- tests/test_alert_score.py
+++ tests/test_alert_score.py
@@ -8,7 +8,8 @@ from deepeval.metrics.alert_score import assert_alert_score
 @pytest.mark.asyncio
 async def test_alert_score():
     assert_alert_score(
-        generated_text="Who won the FIFA World Cup in 2018?",
+        query="Who won the FIFA World Cup in 2018?",
+        generated_text="French national football team",
         expected_output="French national football team",
         context="The FIFA World Cup in 2018 was won by the French national football team. They defeated Croatia 4-2 in the final match to claim the championship.",
     )
