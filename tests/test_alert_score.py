406328d9c31094b164a61922a473b844701849f2
Jacky Wong <colabdog@gmail.com>
2023-08-21 21:48:31 +1000

    update alert score

--- tests/test_alert_score.py
+++ tests/test_alert_score.py
@@ -9,7 +9,7 @@ from deepeval.metrics.alert_score import assert_alert_score
 async def test_alert_score():
     assert_alert_score(
         query="Who won the FIFA World Cup in 2018?",
-        generated_text="French national football team",
+        generated_text="Winners of the FIFA world cup were the French national football team",
         expected_output="French national football team",
         context="The FIFA World Cup in 2018 was won by the French national football team. They defeated Croatia 4-2 in the final match to claim the championship.",
     )
