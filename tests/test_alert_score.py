907e026657f80ce0623758eae3085cf8dd4051a7
Jacky Wong <colabdog@gmail.com>
2023-08-21 21:12:44 +1000

    update name

--- tests/test_alert_score.py
+++ tests/test_alert_score.py
@@ -6,7 +6,7 @@ from deepeval.metrics.alert_score import assert_alert_score
 
 
 @pytest.mark.asyncio
-async def test_factual_consistency():
+async def test_alert_score():
     assert_alert_score(
         generated_text="Who won the FIFA World Cup in 2018?",
         expected_output="French national football team",
