3ceb6367b9fcb6fc1def62bb751afcdfdad03c5b
Jacky Wong <colabdog@gmail.com>
2023-08-22 23:14:10 +1000

    remove additional min us

--- deepeval/metrics/bias_classifier.py
+++ deepeval/metrics/bias_classifier.py
@@ -21,13 +21,11 @@ class UnBiasedMetric(Metric):
         # sample output
         # [{'label': 'Biased', 'score': 0.9938021898269653}]
         self.success = True
-        # measure how unbiased it is
         if results[0]["label"] == "Biased":
             v = 0.5 - (results[0]["score"] / 2)
         else:
             # if it's unbiased - use normal score
             v = 0.5 + (results[0]["score"] / 2)
-        v = 1 - results[0]["score"]
 
         if v > self.minimum_score:
             self.success = False
