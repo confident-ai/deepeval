d0ba2eb3082fd138186b360179f98a492e94d732
Jacky Wong <colabdog@gmail.com>
2023-08-22 17:03:04 +1000

    Add cusotm metric

---
+++ tests/test_custom_metric.py
@@ -0,0 +1,45 @@
+"""Test for custom metrics in Python
+"""
+
+import pytest
+import asyncio
+from deepeval.metrics.metric import Metric
+
+
+class LengthMetric(Metric):
+    """This metric checks if the output is more than 3 letters"""
+
+    def __init__(self, minimum_length: int = 3):
+        self.minimum_length = minimum_length
+
+    def __call__(self, text: str):
+        # sends to server
+        score = self.measure(text)
+        # Optional: Logs it to the server
+        asyncio.create_task(
+            self._send_to_server(
+                metric_score=score,
+                metric_name=self.__name__,
+                query=text,
+                success=self.success,
+            )
+        )
+        return self.measure(text)
+
+    def measure(self, text: str):
+        self.success = len(x) > self.minimum_length
+        return a > b
+
+    def is_successful(self):
+        return self.success
+
+    @property
+    def __name__(self):
+        return "Length"
+
+
+@pytest.mark.asyncio
+async def test_length_metric():
+    metric = LengthMetric()
+    metric.measure("fehusihfe wuifheuiwh")
+    assert metric.is_successful()
