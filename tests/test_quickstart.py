c08973fd7b4e72ee7d65d6b97f13fa3fe81c1b21
Jacky Wong <colabdog@gmail.com>
2023-08-23 15:32:36 +1000

    update quickstart

--- tests/test_quickstart.py
+++ tests/test_quickstart.py
@@ -1,8 +1,14 @@
 """This is the ideal user flow
 """
 import pytest
-from deepeval.test_utils import assert_llm_output
+import os
+from deepeval.api import Api
+from deepeval.metrics.factual_consistency import assert_factual_consistency
 from deepeval.metrics.bertscore_metric import BertScoreMetric
+from deepeval.constants import API_KEY_ENV
+
+IMPLEMENTATION_NAME = "Quickstart Example"
+os.environ["CONFIDENT_AI_IMP_NAME"] = IMPLEMENTATION_NAME
 
 
 def generate_llm_output(input: str):
@@ -15,8 +21,8 @@ async def test_llm_output():
     input = "What is the customer success phone line?"
     expected_output = "Our customer success phone line is 1200-231-231."
     output = generate_llm_output(input)
-    assert_llm_output(output, expected_output, metric="entailment")
-    assert_llm_output(output, expected_output, metric="bertscore")
+    assert_factual_consistency(output, expected_output, metric="entailment")
+    assert_factual_consistency(output, expected_output, metric="bertscore")
 
 
 @pytest.mark.asyncio
@@ -26,4 +32,14 @@ async def test_llm_output_custom():
     output = generate_llm_output(input)
     metric = BertScoreMetric(minimum_score=0.98)
     with pytest.raises(AssertionError):
-        assert_llm_output(output, expected_output, metric=metric)
+        assert_factual_consistency(output, expected_output, metric=metric)
+
+def test_implementation_inside_quickstart():
+    api_key = os.environ[API_KEY_ENV]
+    client = Api(api_key=api_key)
+    imps = client.list_implementations()
+    FOUND = False
+    for imp in imps:
+        if imp['name'] == IMPLEMENTATION_NAME:
+            FOUND = True
+    assert FOUND, f"Not found in {imps}"
