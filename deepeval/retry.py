f243c29fb533a1bbca12eb5ecba31b62ceab1095
Jacky Wong <colabdog@gmail.com>
2023-08-23 21:53:04 +1000

    Fix retry

--- deepeval/retry.py
+++ deepeval/retry.py
@@ -2,6 +2,10 @@
 """
 import time
 
+"""Simple Retry class
+"""
+import time
+
 
 class Retry:
     def __init__(self, max_retries=3, delay=1, min_success=1):
@@ -18,6 +22,15 @@ class Retry:
                 self.success_count += 1
                 if self.success_count >= self.min_success:
                     return  # Operation succeeded the required number of times.
+            except AssertionError as e:
+                print(f"Attempt {self.retry_count + 1} failed: {str(e)}")
+                self.retry_count += 1
+                if self.retry_count < self.max_retries:
+                    print(f"Retrying in {self.delay} seconds...")
+                    time.sleep(self.delay)
+                else:
+                    print(f"Max retries ({self.max_retries}) exceeded.")
+                    raise e
             except Exception as e:
                 print(f"Attempt {self.retry_count + 1} failed: {str(e)}")
                 self.retry_count += 1
