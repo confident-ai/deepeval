2065918c64cdd8e1271819848b1763cbc3dc9e19
Jacky Wong <colabdog@gmail.com>
2023-08-24 16:57:57 +1000

    update output

--- deepeval/test_utils.py
+++ deepeval/test_utils.py
@@ -5,7 +5,6 @@ from .metrics.randomscore import RandomMetric
 from .metrics.metric import Metric
 from .metrics.bertscore_metric import BertScoreMetric
 from .metrics.entailment_metric import EntailmentScoreMetric
-from .metrics.answer_relevancy import AnswerRelevancy
 from .metrics.ranking_similarity import RankingSimilarity
 
 
