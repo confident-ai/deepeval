4e99aef5184433d37804e6ef0d423a9a9771cab5
Jacky Wong <colabdog@gmail.com>
2023-08-25 17:25:16 +1000

    update overall score

--- deepeval/_version.py
+++ deepeval/_version.py
@@ -1,1 +1,1 @@
-__version__: str = "0.9.8"
+__version__: str = "0.9.9"
--- deepeval/metrics/overall_score.py
+++ deepeval/metrics/overall_score.py
@@ -3,6 +3,7 @@
 import asyncio
 from .metric import Metric
 from .entailment_metric import EntailmentScoreMetric
+from .answer_relevancy import AnswerRelevancy
 from ..singleton import Singleton
 
 
@@ -10,9 +11,15 @@ class OverallScoreMetric(Metric, metaclass=Singleton):
     def __init__(self, minimum_score: float = 0.5):
         self.minimum_score = minimum_score
         self.entailment_metric = EntailmentScoreMetric()
+        self.answer_relevancy = AnswerRelevancy()
 
-    def __call__(self, generated_text: str, expected_output: str, context: str):
-        score = self.measure(generated_text, expected_output, context)
+    def __call__(self, query, generated_text: str, expected_output: str, context: str):
+        score = self.measure(
+            query=query,
+            generated_text=generated_text,
+            expected_output=expected_output,
+            context=context,
+        )
         success = score > self.minimum_score
         asyncio.create_task(
             self._send_to_server(
@@ -26,7 +33,9 @@ class OverallScoreMetric(Metric, metaclass=Singleton):
         )
         return score
 
-    def measure(self, generated_text: str, expected_output: str, context: str) -> float:
+    def measure(
+        self, query: str, generated_text: str, expected_output: str, context: str
+    ) -> float:
         entailment_score = self.entailment_metric.measure(
             context,
             generated_text,
@@ -37,7 +46,15 @@ class OverallScoreMetric(Metric, metaclass=Singleton):
             expected_output,
         )
 
-        overall_score = 0.5 * entailment_score + 0.5 * answer_expected_score
+        answer_relevancy_score = self.answer_relevancy.measure(
+            query=query, output=generated_text
+        )
+
+        overall_score = (
+            0.33 * entailment_score
+            + 0.33 * answer_expected_score
+            + 0.33 * answer_relevancy_score
+        )
         self.success = overall_score > self.minimum_score
         return overall_score
 
