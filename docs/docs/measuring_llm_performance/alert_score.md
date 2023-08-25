25d6ae18525653cad45e707e1e31c03ec7d0a943
Jacky Wong <colabdog@gmail.com>
2023-08-26 02:42:22 +1000

    mom i did it

--- docs/docs/measuring_llm_performance/alert_score.md
+++ docs/docs/measuring_llm_performance/alert_score.md
@@ -13,7 +13,7 @@ It then takes the minimum of those scores to alert.
 ```python
 from deepeval.metrics.alert_score import assert_alert_score
 assert_alert_score(
-    generated_text="Who won the FIFA World Cup in 2018?",
+    output="Who won the FIFA World Cup in 2018?",
     expected_output="French national football team",
     context="The FIFA World Cup in 2018 was won by the French national football team. They defeated Croatia 4-2 in the final match to claim the championship.",
     minimum_score=0.3
@@ -26,7 +26,7 @@ assert_alert_score(
 from deepeval.metrics.alert_score import AlertScoreMetric
 metric = AlertScoreMetric()
 score = metric.measure(
-    generated_text="Who won the FIFA World Cup in 2018?",
+    output="Who won the FIFA World Cup in 2018?",
     expected_output="French national football team"
 )
 score
--- tests/test_alert_score.py
+++ tests/test_alert_score.py
@@ -12,7 +12,7 @@ os.environ["CONFIDENT_AI_IMP_NAME"] = IMPLEMENTATION_NAME
 os.environ["CONFIDENT_AI_API_KEY"] = TEST_API_KEY
 
 query = "Who won the FIFA World Cup in 2018?"
-generated_text = "Winners of the FIFA world cup were the French national football team"
+output = "Winners of the FIFA world cup were the French national football team"
 expected_output = "French national football team"
 context = "The FIFA World Cup in 2018 was won by the French national football team. They defeated Croatia 4-2 in the final match to claim the championship."
 
@@ -20,7 +20,7 @@ context = "The FIFA World Cup in 2018 was won by the French national football te
 def test_alert_score():
     assert_alert_score(
         query="Who won the FIFA World Cup in 2018?",
-        generated_text="Winners of the FIFA world cup were the French national football team",
+        output="Winners of the FIFA world cup were the French national football team",
         expected_output="French national football team",
         context="The FIFA World Cup in 2018 was won by the French national football team. They defeated Croatia 4-2 in the final match to claim the championship.",
     )
@@ -30,7 +30,7 @@ def test_alert_score_metric():
     metric = AlertScoreMetric()
     score = metric.measure(
         query=query,
-        generated_text=generated_text,
+        output=output,
         expected_output=expected_output,
         context=context,
     )
