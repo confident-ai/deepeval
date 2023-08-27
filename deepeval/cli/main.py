5243b3e32d14af510a0f7b537657dd1b6bc58b26
Jacky Wong <colabdog@gmail.com>
2023-08-27 23:26:41 +1000

    add basic CLI imp

---
+++ deepeval/cli/main.py
@@ -0,0 +1,37 @@
+import typer
+import os
+
+# Rich has a few dependency issues
+try:
+    from rich import print
+except Exception as e:
+    pass
+from ..key_handler import KeyFileHandler
+from .test import app as test_app
+from ..api import Api
+from ..constants import IMPLEMENTATION_ID_NAME
+
+app = typer.Typer(name="deepeval")
+
+app.add_typer(test_app, name="test")
+
+handler = KeyFileHandler()
+
+
+@app.command()
+def login():
+    """Login to the DeepEval platform."""
+    print("Welcome to [bold]DeepEval[/bold]!")
+    print("Grab your API key here: https://app.confident-ai.com/organization")
+    api_key = input("Paste it here:")
+    handler.write_api_key(api_key)
+    client = Api(api_key=api_key)
+    print("What is the name of your project?")
+    project = input("Name:")
+    handler.write_data(IMPLEMENTATION_ID_NAME, project)
+    print("Success! :raising_hands:")
+    print("Run: [bold]deepeval test sample[/bold]")
+
+
+if __name__ == "__main__":
+    app()
---
+++ deepeval/cli/test.py
@@ -0,0 +1,37 @@
+import typer
+from .cli_key_handler import set_env_vars
+from ..metrics.factual_consistency import assert_factual_consistency
+
+try:
+    from rich import print
+except Exception as e:
+    pass
+
+app = typer.Typer(name="test")
+
+
+@app.command()
+def sample():
+    set_env_vars()
+    print("Sending sample test results...")
+    try:
+        assert_factual_consistency(
+            output="Python is an interpreted high-level programming language.",
+            context="Python is a programming language.",
+        )
+    except AssertionError as e:
+        pass
+    try:
+        assert_factual_consistency(
+            output="Python is an interpreted high-level programming language.",
+            context="Python is a programming language.",
+        )
+    except AssertionError as e:
+        pass
+    try:
+        assert_factual_consistency(
+            output="Water boils at 100 degrees Fahrenheit.",
+            context="Water boils at 100 degrees Celsius.",
+        )
+    except AssertionError as e:
+        pass
