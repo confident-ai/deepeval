017bea15387dda56f3e6f3bea6fd17e56b93800e
Jacky Wong <colabdog@gmail.com>
2023-08-29 09:41:06 +1000

    get API key

---
+++ deepeval/get_api_key.py
@@ -0,0 +1,13 @@
+"""Utility function for getting API key.
+"""
+import os
+from .key_handler import KEY_FILE_HANDLER
+from .constants import API_KEY_ENV
+
+
+def _get_api_key():
+    """Get an API key here."""
+    api_key = KEY_FILE_HANDLER.fetch_api_key()
+    if api_key is None or api_key == "":
+        api_key = os.getenv(API_KEY_ENV, "")
+    return api_key
