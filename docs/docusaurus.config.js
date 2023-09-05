6fed9dcd88c7882b2e0b514a6967e60969c7e856
Jacky Wong <colabdog@gmail.com>
2023-09-05 11:30:56 +1000

    remove

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'static/img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://litellm.vercel.app/',
+  url: 'https://docs.confident-ai.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
