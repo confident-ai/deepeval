7d43b5e43410790980b94bf8ac900b42f771d778
Jacky Wong <colabdog@gmail.com>
2023-09-10 23:53:41 +1000

    Fix deepeval

--- deepeval/__init__.py
+++ deepeval/__init__.py
@@ -2,6 +2,15 @@
 # from .query_generator import QueryGenerator
 # from .test_utils import assert_exact_match, TestEvalCase
 # from .bulk_runner import TestCase, BulkTestRunner
-from . import (_version, bulk_runner, dataset, evaluator, metrics,
-               query_generator, test_case, test_utils, utils)
+from . import (
+    _version,
+    bulk_runner,
+    dataset,
+    evaluator,
+    metrics,
+    query_generator,
+    test_case,
+    test_utils,
+    utils,
+)
 from ._version import __version__
