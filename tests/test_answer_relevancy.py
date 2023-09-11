fa4664bc30398a7f24d651bae31733df4f6a560a
Jacky Wong <colabdog@gmail.com>
2023-09-11 17:27:05 +1000

    update the test cases

--- tests/test_answer_relevancy.py
+++ tests/test_answer_relevancy.py
@@ -6,6 +6,7 @@ from deepeval.metrics.answer_relevancy import (
     AnswerRelevancyMetric,
     assert_answer_relevancy,
 )
+from deepeval.run_test import run_test
 
 query = "What is Python?"
 answer = "Python is a programming language?"
@@ -39,13 +40,14 @@ def test_compare_answer_relevancy_2():
 
 
 def test_compare_answer_relevancy():
-    scorer = AnswerRelevancyMetric(minimum_score=0.5)
+    metric = AnswerRelevancyMetric(minimum_score=0.5)
     query = "what is python"
     test_case = LLMTestCase(query=query, output="Programming lang")
-    result = scorer.measure(test_case)
+    result = metric.measure(test_case)
 
     test_case_2 = LLMTestCase(
         query=query, output="Python is a programming lang"
     )
-    result_2 = scorer.measure(test_case_2)
+    result_2 = metric.measure(test_case_2)
+    run_test([test_case, test_case_2], metrics=[metric])
     assert result_2 > result
