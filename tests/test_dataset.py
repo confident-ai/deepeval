66fe73369005afd19123504617ed5bd12b5b0a8c
Jacky Wong <colabdog@gmail.com>
2023-09-11 22:09:59 +1000

    update test datset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -41,3 +41,39 @@ def test_create_synthetic_dataset():
         n=1,
     )
     assert len(dataset) == 1
+
+
+def test_dataset_evaluation():
+    """
+    Test dataset evaluation
+    """
+    from deepeval.dataset import EvaluationDataset
+    from deepeval.metrics.conceptual_similarity import (
+        ConceptualSimilarityMetric,
+    )
+
+    csv_filename = "sample.csv"
+
+    csv_file = """id,query,expected_output,output
+    1,"Hello, world!","This is a greeting.","This is a friendly greeting."
+    2,"OpenAI GPT-3","A powerful language model.","A powerful AI language model."
+    3,"CSV Example","Working with CSV data.","Working with data in CSV format."
+    4,"Python Programming","Coding in Python.","Writing code in Python."
+    5,"Data Science","Analyzing data.","Exploring and analyzing data."
+    """
+
+    with open(csv_filename, "w") as file:
+        file.write(csv_file)
+
+    dataset: EvaluationDataset = EvaluationDataset.from_csv(
+        csv_filename,
+        query_column="query",
+        expected_output_column="expected_output",
+        id_column="id",
+        output_column="output",
+    )
+    metric = ConceptualSimilarityMetric()
+    result = dataset.run_evaluation(
+        outputs="output",
+        metrics=[metric],
+    )
