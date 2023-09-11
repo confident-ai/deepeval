8729450cff725a7cd41fe427558462d518e3d4dc
Jacky Wong <colabdog@gmail.com>
2023-09-11 23:31:52 +1000

    Fix answer relevnacy

--- tests/test_answer_relevancy.py
+++ tests/test_answer_relevancy.py
@@ -6,7 +6,7 @@ from deepeval.metrics.answer_relevancy import (
     AnswerRelevancyMetric,
     assert_answer_relevancy,
 )
-from deepeval.run_test import run_test
+from deepeval.run_test import run_test, assert_test
 
 query = "What is Python?"
 answer = "Python is a programming language?"
@@ -24,30 +24,25 @@ def test_answer_not_relevant():
 def test_query_answer_relevancy():
     scorer = AnswerRelevancyMetric(minimum_score=0.5)
     test_case = LLMTestCase(query=query, output=answer)
-    score = scorer.measure(test_case)
-    assert scorer.is_successful(), f"Failed - {score}'"
+    assert_test(test_case, [scorer])
 
 
 def test_compare_answer_relevancy_2():
     scorer = AnswerRelevancyMetric(minimum_score=0.5)
     test_case = LLMTestCase(query=query, output="Programming lang")
-    result = scorer.measure(test_case)
     test_case_2 = LLMTestCase(
         query=query, output="Python is a programming lang"
     )
-    result_2 = scorer.measure(test_case_2)
-    assert result_2 > result
+    results = run_test([test_case, test_case_2], metrics=[scorer])
+    assert results[1].score > results[0].score
 
 
 def test_compare_answer_relevancy():
     metric = AnswerRelevancyMetric(minimum_score=0.5)
     query = "what is python"
     test_case = LLMTestCase(query=query, output="Programming lang")
-    result = metric.measure(test_case)
-
     test_case_2 = LLMTestCase(
         query=query, output="Python is a programming lang"
     )
-    result_2 = metric.measure(test_case_2)
-    run_test([test_case, test_case_2], metrics=[metric])
-    assert result_2 > result
+    result = run_test([test_case, test_case_2], metrics=[metric])
+    assert result[1].score > result[0].score
