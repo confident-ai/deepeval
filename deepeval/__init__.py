9c07185f1d6d1f423d06f54d82c3c3ab573a5add
Jacky Wong <colabdog@gmail.com>
2023-09-11 16:51:13 +1000

    remove init

--- deepeval/__init__.py
+++ deepeval/__init__.py
@@ -1,15 +1,15 @@
 # from .client import Evaluator
 # from .query_generator import QueryGenerator
 # from .test_utils import assert_exact_match, TestEvalCase
-from . import (
-    _version,
-    dataset,
-    evaluator,
-    metrics,
-    query_generator,
-    test_case,
-    test_utils,
-    utils,
-)
+# from . import (
+#     _version,
+#     dataset,
+#     evaluator,
+#     metrics,
+#     query_generator,
+#     test_case,
+#     test_utils,
+#     utils,
+# )
 from ._version import __version__
 from .run_test import run_test, assert_test
