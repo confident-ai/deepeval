bfc9491459cf38d68ea986beb95c331968ae0c24
Jacky Wong <colabdog@gmail.com>
2023-09-12 12:05:44 +1000

    update deepeval

--- deepeval/__init__.py
+++ deepeval/__init__.py
@@ -10,3 +10,7 @@
 #     utils,
 # )
 from ._version import __version__
+import warnings
+from urllib3.exceptions import NotOpenSSLWarning
+
+warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
