90811e0a49cd57f8ea11a6469e6cc1d9481ff67b
Jacky Wong <colabdog@gmail.com>
2023-09-21 17:38:34 -0700

    update the deepeval plugins

--- deepeval/plugins/plugin.py
+++ deepeval/plugins/plugin.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from deepeval.api import Api, TestRun
 
 from datetime import datetime
@@ -20,6 +21,14 @@ def pytest_sessionstart(session):
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_teardown(item, nextitem):
+    # Code before yield will run before the test teardown
     api: Api = Api()
+
+    # yield control back to pytest for the actual teardown
+    yield
+
+    # Code after yield will run after the test teardown
     test_run = TestRun.load(test_filename)
     api.post_test_run(test_run)
+    # if os.path.exists(test_filename):
+    #     os.remove(test_filename)
