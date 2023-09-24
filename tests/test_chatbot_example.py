1b875d29253a68904568e92cfa8f9302415cc2a8
Jacky Wong <colabdog@gmail.com>
2023-09-24 15:36:35 -0700

    add chatbot test

--- tests/test_chatbot_example.py
+++ tests/test_chatbot_example.py
@@ -6,7 +6,9 @@ from deepeval.run_test import assert_test
 
 def test_1():
     input = "What does your company do?"
-    output = "My company doesn't do anything."
+    output = (
+        "Specializes in cloud computing, data analytics, and machine learning."
+    )
     context = "Our company specializes in cloud computing, data analytics, and machine learning. We offer a range of services including cloud storage solutions, data analytics platforms, and custom machine learning models."
     factual_consistency_metric = FactualConsistencyMetric(minimum_score=1.0)
     test_case = LLMTestCase(output=output, context=context)
@@ -16,8 +18,14 @@ def test_1():
 
 def test_2():
     input = "What does your company do?"
-    output = "My company is a cloud computing company."
+    output = (
+        "Specializes in cloud computing, data analytics, and machine learning."
+    )
     context = "Our company specializes in cloud computing, data analytics, and machine learning. We offer a range of services including cloud storage solutions, data analytics platforms, and custom machine learning models."
-    factual_consistency_metric = FactualConsistencyMetric(minimum_score=0.5)
+    factual_consistency_metric_half = FactualConsistencyMetric(
+        minimum_score=0.5
+    )
+    if factual_consistency_metric_half.minimum_score != 0.5:
+        raise ValueError("Minimum score should be 0.5")
     test_case = LLMTestCase(output=output, context=context)
-    assert_test(test_case, [factual_consistency_metric])
+    assert_test(test_case, [factual_consistency_metric_half])
