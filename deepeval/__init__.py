b35e243263ef460459de5d8aae41f35928f6a00f
Jacky Wong <colabdog@gmail.com>
2023-09-24 14:34:29 -0700

    update deepeval

--- deepeval/__init__.py
+++ deepeval/__init__.py
@@ -9,20 +9,22 @@
 #     test_utils,
 #     utils,
 # )
-import requests
 import warnings
 from ._version import __version__
 
 
 def check_for_update():
     try:
+        import requests
+
         response = requests.get("https://pypi.org/pypi/deepeval/json")
         latest_version = response.json()["info"]["version"]
         if latest_version > __version__:
             warnings.warn(
                 f'You are using deepeval version {__version__}, however version {latest_version} is available. You should consider upgrading via the "pip install --upgrade deepeval" command.'
             )
-    except Exception:
+    except ModuleNotFoundError:
+        # they're just getting the version
         pass
 
 
