3ad167b6471aa250118f2059c7228ecc39c5ddc6
Jacky Wong <colabdog@gmail.com>
2023-09-24 14:10:40 -0700

    add test for failing

--- tests/test_chatbot_example.py
+++ tests/test_chatbot_example.py
@@ -1,3 +1,4 @@
+import pytest
 from deepeval.metrics.factual_consistency import FactualConsistencyMetric
 from deepeval.test_case import LLMTestCase
 from deepeval.run_test import assert_test
@@ -9,7 +10,8 @@ def test_1():
     context = "Our company specializes in cloud computing, data analytics, and machine learning. We offer a range of services including cloud storage solutions, data analytics platforms, and custom machine learning models."
     factual_consistency_metric = FactualConsistencyMetric(minimum_score=1.0)
     test_case = LLMTestCase(output=output, context=context)
-    assert_test(test_case, [factual_consistency_metric])
+    with pytest.raises(AssertionError):
+        assert_test(test_case, [factual_consistency_metric])
 
 
 def test_2():
