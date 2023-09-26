aa9af312cd59664c2df45850946c0d150b9891c4
Jon Bennion <jb@Jons-MacBook-Pro.local>
2023-09-25 20:19:38 -0700

    adding length metric including test and documentation

---
+++ tests/test_answer_length.py
@@ -0,0 +1,13 @@
+"""Tests for answer length
+"""
+from deepeval.test_case import LLMTestCase
+from deepeval.metrics.answer_length import LengthMetric
+from deepeval.run_test import assert_test
+
+
+def test_answer_length():
+    metric = LengthMetric()
+    test_case = LLMTestCase(
+        output=" Some output ", expected_output="Some output"
+    )
+    assert_test(test_case, [metric])
