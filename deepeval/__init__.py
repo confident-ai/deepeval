4061ddd711d80357e9dc494c199ea061cf528650
Jacky Wong <colabdog@gmail.com>
2023-09-29 00:43:10 -0700

    fix setup

--- deepeval/__init__.py
+++ deepeval/__init__.py
@@ -10,8 +10,15 @@
 #     utils,
 # )
 import warnings
-from packaging import version
+import re
 from ._version import __version__
+
+
+def compare_versions(version1, version2):
+    def normalize(v):
+        return [int(x) for x in re.sub(r"(\.0+)*$", "", v).split(".")]
+
+    return normalize(version1) > normalize(version2)
 
 
 def check_for_update():
@@ -21,7 +28,7 @@ def check_for_update():
         response = requests.get("https://pypi.org/pypi/deepeval/json")
         latest_version = response.json()["info"]["version"]
 
-        if version.parse(latest_version) > version.parse(__version__):
+        if compare_versions(latest_version, __version__):
             warnings.warn(
                 f'You are using deepeval version {__version__}, however version {latest_version} is available. You should consider upgrading via the "pip install --upgrade deepeval" command.'
             )
