385e1c790881de36c219ce46ea2d3eb9f98e56b1
Jacky Wong <colabdog@gmail.com>
2023-10-01 14:15:13 -0700

    add clip similarity metric

--- deepeval/metrics/clip_similarity_metric.py
+++ deepeval/metrics/clip_similarity_metric.py
@@ -1,30 +1,26 @@
 from deepeval.metrics.metric import Metric
 from deepeval.test_case import ImageTestCase
+from ..singleton import Singleton
 
 
-class ClipSimilarityMetric(Metric):
-    def __init__(self, model_name="ViT-B/32", minimum_score: float = 0.3):
+class ClipModel(metaclass=Singleton):
+    def __init__(self, model_name="ViT-B/32"):
         self.model_name = model_name
         import torch
         import clip
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model, self.preprocess = clip.load(model_name, device=self.device)
-        self.minimum_score = minimum_score
 
-    def measure(self, test_case: ImageTestCase):
+    def get_similarity(self, image_path, query):
         import clip
         import torch
         from PIL import Image
 
         image_input = (
-            self.preprocess(Image.open(test_case.image_path))
-            .unsqueeze(0)
-            .to(self.device)
+            self.preprocess(Image.open(image_path)).unsqueeze(0).to(self.device)
         )
-        text_inputs = torch.cat([clip.tokenize(test_case.query)]).to(
-            self.device
-        )
+        text_inputs = torch.cat([clip.tokenize(query)]).to(self.device)
         # Calculate features
         with torch.no_grad():
             image_features = self.model.encode_image(image_input)
@@ -35,6 +31,18 @@ class ClipSimilarityMetric(Metric):
         text_features /= text_features.norm(dim=-1, keepdim=True)
         similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
         score = float(similarity[0])
+        return score
+
+
+class ClipSimilarityMetric(Metric):
+    def __init__(self, model_name="ViT-B/32", minimum_score: float = 0.3):
+        self.clip_model = ClipModel(model_name)
+        self.minimum_score = minimum_score
+
+    def measure(self, test_case: ImageTestCase):
+        score = self.clip_model.get_similarity(
+            test_case.image_path, test_case.query
+        )
         self.success = score > self.minimum_score
         return score
 
