182e54301a4c3a932ab132a8af68b588d91b586b
Jacky Wong <colabdog@gmail.com>
2023-10-01 14:25:58 -0700

    add modifier bsae

--- deepeval/modifier/base.py
+++ deepeval/modifier/base.py
@@ -12,5 +12,11 @@ class Modifier:
         self.input = input
 
 
-class QueryModifier(Modifier):
-    """Add a query modifier based on the result."""
+class ClipInterrogatorModifier(Modifier):
+    def modify(self, input: str):
+        from PIL import Image
+        from clip_interrogator import Config, Interrogator
+
+        image = Image.open(image_path).convert("RGB")
+        ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
+        return ci.interrogate(image)
