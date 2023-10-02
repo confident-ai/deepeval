d5aa513b33c5f666b2a10010d5a0b87481f10412
Jacky Wong <colabdog@gmail.com>
2023-10-01 18:31:46 -0700

    update modifier

--- deepeval/modifier/base.py
+++ deepeval/modifier/base.py
@@ -1,6 +1,6 @@
 """Providing modifier abstractions
 """
-from typing import Any
+from typing import Any, Callable
 from abc import abstractmethod
 
 
@@ -13,12 +13,31 @@ class Modifier:
 
 
 class ClipInterrogatorModifier(Modifier):
-    """Modifies a query"""
+    """WIP - Modifies a query using the Clip Interrogator."""
 
-    def modify(self, image_path: str, model_name="ViT-L-14/openai"):
+    def __init__(self, completion_function):
+        self.completion_function = completion_function
+
+    def clip_interrogate(self, image_path: str, model_name="ViT-L-14/openai"):
         from PIL import Image
         from clip_interrogator import Config, Interrogator
 
         image = Image.open(image_path).convert("RGB")
         ci = Interrogator(Config(clip_model_name=model_name))
         return ci.interrogate(image)
+
+    def refine_prompt(self, prompt: str, image_path: str = "images/sf_1.jpg"):
+        print("refining")
+        similar_query = self.clip_interrogate(image_path)
+        print("Feeding through to chatgpt")
+        new_prompt = self.completion_function(
+            f"""Extract the artistic style of the prompt below and modify the original prompt to make it more similar to the prompt below.
+
+    Artistic style to copy: {prompt}
+    Original prompt: {similar_query}
+
+    New Prompt:"""
+        )
+        print("new prompt: ")
+        print(new_prompt)
+        return new_prompt
