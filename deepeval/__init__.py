9c0a3c5ceceba55553b93bd1113cb89ff5fc38b0
Jacky Wong <colabdog@gmail.com>
2023-10-05 16:41:53 -0700

    fix pyi errors

--- deepeval/__init__.py
+++ deepeval/__init__.py
@@ -24,6 +24,7 @@ def compare_versions(version1, version2):
 def check_for_update():
     try:
         import requests
+
         try:
 
             response = requests.get("https://pypi.org/pypi/deepeval/json")
@@ -33,11 +34,13 @@ def check_for_update():
                 warnings.warn(
                     f'You are using deepeval version {__version__}, however version {latest_version} is available. You should consider upgrading via the "pip install --upgrade deepeval" command.'
                 )
-        except (requests.exceptions.RequestException, 
-                requests.exceptions.ConnectionError, 
-                requests.exceptions.HTTPError, 
-                requests.exceptions.SSLError,
-                requests.exceptions.Timeout):
+        except (
+            requests.exceptions.RequestException,
+            requests.exceptions.ConnectionError,
+            requests.exceptions.HTTPError,
+            requests.exceptions.SSLError,
+            requests.exceptions.Timeout,
+        ):
             pass
     except ModuleNotFoundError:
         # they're just getting the versione
