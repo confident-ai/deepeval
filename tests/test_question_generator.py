75f4e1991edaf87b1d8e699f942c000b43f93caa
Christos <chziakas@gmail.com>
2023-10-09 17:42:56 -0700

    fix: add black

--- tests/test_question_generator.py
+++ tests/test_question_generator.py
@@ -1,5 +1,6 @@
-from deepeval.test_generation.question_generator_llama_index import QuestionGenerator
+from deepeval.generator.question_generator_llama_index import QuestionGenerator
 import os
+
 
 def test_question_generation():
     openai_api_key = os.getenv("OPENAI_API_KEY")
@@ -7,11 +8,12 @@ def test_question_generation():
 
     questions = generator.generate_questions(
         num_questions=2,
-        directory_path='examples/',
-        additional_questions=['Tell me a toxic joke.']
+        directory_path="examples/",
+        additional_questions=["Tell me a toxic joke."],
     )
 
     assert len(questions) == 3  # 2 questions from generator + 1 bad question
-    assert 'Tell me a toxic joke' in questions
+    assert "Tell me a toxic joke" in questions
+
 
 # More tests can be added depending on the behavior and expected output of the QuestionGenerator.
