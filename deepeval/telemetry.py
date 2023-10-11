0ea6e5b5c1c8f80b62b5cd4533ae18cf37ee2741
Jacky Wong <colabdog@gmail.com>
2023-10-11 13:11:13 -0700

    update telemetry

--- deepeval/telemetry.py
+++ deepeval/telemetry.py
@@ -1,7 +1,7 @@
 import os
 import sentry_sdk
 
-if os.getenv("TURN_OFF_TELEMETRY") == "Y":
+if os.getenv("TURN_OFF_DEEPEVAL_TELEMETRY") != "Y":
     sentry_sdk.init(
         dsn="https://a378e1dcd3cd09b8aacf236c9a03dfc0@o4506033502093312.ingest.sentry.io/4506033503797248",
         # Set traces_sample_rate to 1.0 to capture 100%
