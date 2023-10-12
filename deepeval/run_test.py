5ee3358fa6674528f77540af47a0e2a6ccf6f634
penguine-up <jeffreyip@confident-ai.com>
2023-10-12 15:26:00 -0700

    .

--- deepeval/run_test.py
+++ deepeval/run_test.py
@@ -87,9 +87,7 @@ def create_test_result(
             score=score,
             metric_name=metric.__name__,
             query=test_case.input if test_case.input else "-",
-            output=test_case.actual_output
-            if test_case.actual_output
-            else "-",
+            output=test_case.actual_output if test_case.actual_output else "-",
             expected_output=test_case.expected_output
             if test_case.expected_output
             else "-",
@@ -102,9 +100,7 @@ def create_test_result(
             score=score,
             metric_name=metric.__name__,
             query=test_case.input if test_case.input else "-",
-            output=test_case.output_list
-            if test_case.output_list
-            else "-",
+            output=test_case.output_list if test_case.output_list else "-",
             expected_output=test_case.golden_list
             if test_case.golden_list
             else "-",
