4cf72f99977c004dca97d9a1f3b7e9ccd63680d6
Jacky Wong <colabdog@gmail.com>
2023-10-12 02:48:31 -0700

    fix baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://docs.confident-ai.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
