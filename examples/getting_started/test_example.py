9f3c6a1205f989099cb6063d3e3145bf368daae1
penguine-up <jeffreyip@confident-ai.com>
2023-10-13 01:16:14 -0700

    reformat

--- examples/getting_started/test_example.py
+++ examples/getting_started/test_example.py
@@ -5,9 +5,12 @@ from deepeval.run_test import assert_test
 from deepeval.metrics.llm_eval_metric import LLMEvalMetric
 from deepeval.metrics.metric import Metric
 
+
 def test_factual_consistency():
     query = "What if these shoes don't fit?"
-    context = "All customers are eligible for a 30 day full refund at no extra cost."
+    context = (
+        "All customers are eligible for a 30 day full refund at no extra cost."
+    )
 
     # Replace this with the actual output from your LLM application
     actual_output = "We offer a 30-day full refund at no extra cost."
@@ -21,14 +24,16 @@ def test_humor():
 
     # Replace this with the actual output from your LLM application
     actual_output = "We offer a 30-day full refund at no extra cost."
-    funny_metric = LLMEvalMetric(name="Funny Metric", criteria="How funny it is", minimum_score=0.3)
+    funny_metric = LLMEvalMetric(
+        name="Funny Metric", criteria="How funny it is", minimum_score=0.3
+    )
     test_case = LLMTestCase(query=query, output=actual_output)
     assert_test(test_case, [length_metric])
 
 
 class LengthMetric(Metric):
     # This metric checks if the output length is greater than 10 characters
-    def __init__(self, max_length: int=10):
+    def __init__(self, max_length: int = 10):
         self.max_length = max_length
 
     def measure(self, test_case: LLMTestCase):
@@ -41,7 +46,7 @@ class LengthMetric(Metric):
     @property
     def __name__(self):
         return "Length"
-    
+
 
 def test_length():
     query = "What if these shoes don't fit?"
@@ -55,13 +60,19 @@ def test_length():
 
 def test_everything():
     query = "What if these shoes don't fit?"
-    context = "All customers are eligible for a 30 day full refund at no extra cost."
+    context = (
+        "All customers are eligible for a 30 day full refund at no extra cost."
+    )
 
     # Replace this with the actual output from your LLM application
     actual_output = "We offer a 30-day full refund at no extra cost."
     factual_consistency_metric = FactualConsistencyMetric(minimum_score=0.7)
     length_metric = LengthMetric(max_length=10)
-    funny_metric = LLMEvalMetric(name="Funny Metric", criteria="How funny it is", minimum_score=0.3)
+    funny_metric = LLMEvalMetric(
+        name="Funny Metric", criteria="How funny it is", minimum_score=0.3
+    )
 
     test_case = LLMTestCase(query=query, output=actual_output, context=context)
-    assert_test(test_case, [factual_consistency_metric, length_metric, funny_metric])
+    assert_test(
+        test_case, [factual_consistency_metric, length_metric, funny_metric]
+    )
