d85a7d773748787f12d5533ff67eb1634e7638c1
Anindyadeep <proanindyadeep@gmail.com>
2023-10-28 18:53:35 +0530

    run black

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -3,27 +3,36 @@
 
 from deepeval.metrics.scoring import StatisticalCalculator
 
-# Testing exact_match metric 
+# Testing exact_match metric
+
 
 def test_exact_equal_strings():
-    target = "Winners of the FIFA world cup were the French national football team"
-    prediction = "Winners of the FIFA world cup were the French national football team"
+    target = (
+        "Winners of the FIFA world cup were the French national football team"
+    )
+    prediction = (
+        "Winners of the FIFA world cup were the French national football team"
+    )
     assert StatisticalCalculator.exact_match(target, prediction) == 1
+
 
 def test_exact_match_unequal_strings():
     target = "Hello, World!"
     prediction = "Goodbye, World!"
     assert StatisticalCalculator.exact_match(target, prediction) == 0
 
+
 def test_exact_match_whitespace_difference():
     target = "Hello, World!"
     prediction = " Hello, World! "
     assert StatisticalCalculator.exact_match(target, prediction) == 1
 
+
 def test_exact_match_empty_prediction():
     target = "Hello, World!"
     prediction = ""
     assert StatisticalCalculator.exact_match(target, prediction) == 0
+
 
 def test_exact_match_empty_target():
     target = ""
