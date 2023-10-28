0b92f92ae69f4da8f1b4aa503962f0c0ee78da6a
Anindyadeep <proanindyadeep@gmail.com>
2023-10-28 18:39:54 +0530

    renamed file from test_calculator.py to test_scoring.py

---
+++ tests/test_scoring.py
@@ -0,0 +1,31 @@
+"""Tests for metrics calculator
+"""
+
+from deepeval.metrics.scoring import StatisticalCalculator
+
+# Testing exact_match metric 
+
+def test_exact_equal_strings():
+    target = "Winners of the FIFA world cup were the French national football team"
+    prediction = "Winners of the FIFA world cup were the French national football team"
+    assert StatisticalCalculator.exact_match(target, prediction) == 1
+
+def test_exact_match_unequal_strings():
+    target = "Hello, World!"
+    prediction = "Goodbye, World!"
+    assert StatisticalCalculator.exact_match(target, prediction) == 0
+
+def test_exact_match_whitespace_difference():
+    target = "Hello, World!"
+    prediction = " Hello, World! "
+    assert StatisticalCalculator.exact_match(target, prediction) == 1
+
+def test_exact_match_empty_prediction():
+    target = "Hello, World!"
+    prediction = ""
+    assert StatisticalCalculator.exact_match(target, prediction) == 0
+
+def test_exact_match_empty_target():
+    target = ""
+    prediction = "Hello, World!"
+    assert StatisticalCalculator.exact_match(target, prediction) == 0
