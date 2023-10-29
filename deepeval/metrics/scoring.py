1515d3fb138c332c307fc2dc2255eb9fef59a782
Anindyadeep <proanindyadeep@gmail.com>
2023-10-29 12:20:36 +0530

    Feat: Add BertScore

--- deepeval/metrics/scoring.py
+++ deepeval/metrics/scoring.py
@@ -1,3 +1,4 @@
+import torch
 from rouge_score import rouge_scorer
 from nltk.tokenize import word_tokenize
 from nltk.translate.bleu_score import sentence_bleu
@@ -106,13 +107,59 @@ class Scorer:
 
     @classmethod
     def bert_score(
-        cls, target: str, prediction: str, model: Optional[Any] = None
+        cls, references: Union[str, List[str]], predictions: Union[str, List[str]], 
+        model: Optional[str] = "microsoft/deberta-large-mnli", lang: Optional[str] = "en"
     ) -> float:
-        raise NotImplementedError()
+        """
+        Calculate BERTScore for one or more reference sentences compared to one or more prediction sentences using a specified BERT model.
+
+        Args:
+            references (Union[str, List[str]]): A single reference sentence or a list of reference sentences.
+            predictions (Union[str, List[str]]): A single prediction sentence or a list of prediction sentences.
+            model (Optional[str], optional): The name of the BERT model to be used for scoring. Defaults to "microsoft/deberta-large-mnli".
+            lang (Optional[str], optional): The language code of the text, e.g., "en" for English. Defaults to "en".
+
+        Returns:
+            Dict[str, float]: A dictionary containing BERTScore metrics including precision, recall, and F1 score.
+                - 'bert-precision' (float): BERTScore precision.
+                - 'bert-recall' (float): BERTScore recall.
+                - 'bert-f1' (float): BERTScore F1 score.
+
+        Note:
+            Before using this function, make sure to install the 'bert_score' module by running the following command:
+            ```
+            pip install bert-score
+            ```
+        """
+        try:
+            from bert_score import BERTScorer
+        except ModuleNotFoundError as e:
+            print("Please install bert_score module. Command: pip install bert-score")
+        
+        # FIXME: Fix the case for mps 
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        bert_scorer = BERTScorer(model_type=model, lang=lang, rescale_with_baseline=True, device=device)
+
+        if isinstance(predictions, str):
+            predictions = [predictions]
+        
+        if isinstance(references, str):
+            references = [references]
+        
+        if isinstance(predictions, list) and isinstance(references, list) and not isinstance(references[0], list):
+            if len(predictions) != len(references):
+                references = [references]
+        
+        precision, recall, f1 = bert_scorer.score(cands=predictions, refs=references)
+        return {
+            'bert-precision': precision.detach().numpy().tolist(),
+            'bert-recall': recall.detach().numpy().tolist(),
+            'bert-f1': f1.detach().numpy().tolist()
+        }
 
     @classmethod
     def faithfulness_score(
-        cls, target: str, prediction: str, model: Optional[Any] = None
+        cls, target: str, prediction: str, model: Optional[str] = None
     ) -> float:
         raise NotImplementedError()
 
