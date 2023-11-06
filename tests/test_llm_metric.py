e357f2d44ed174e9be6124620ded3b40bfcf5ff2
Jeffrey Ip <jeffreyip@confident-ai.com>
2023-11-06 02:14:15 -0800

    .

--- tests/test_llm_metric.py
+++ tests/test_llm_metric.py
@@ -2,6 +2,7 @@ import pytest
 from deepeval.test_case import LLMTestCase
 from deepeval.metrics.llm_eval_metric import LLMEvalMetric
 from deepeval.types import LLMTestCaseParams
+from deepeval.run_test import assert_test
 
 
 def test_chat_completion():
@@ -21,6 +22,7 @@ def test_chat_completion():
         expected_output="Paris",
         context="Geography",
     )
-    metric.measure(test_case)
-    assert metric.is_successful() is True
-    assert metric.measure(test_case) <= 1.0
+    # metric.measure(test_case)
+    # assert metric.is_successful() is True
+    # assert metric.measure(test_case) <= 1.0
+    assert_test(test_case, [metric])
