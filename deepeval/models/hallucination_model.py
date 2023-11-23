e401a25a235de8018be900a051488f896bb5b0d4
Anindyadeep <anindya@pop-os.localdomain>
2023-11-23 09:27:04 +0530

    Refactor: removed model_map and it's imports.
    Since, the reason of removing of model_map is explained in previous commits, so
    removed those dependecies inside hallucination_model too.

--- deepeval/models/hallucination_model.py
+++ deepeval/models/hallucination_model.py
@@ -2,11 +2,9 @@ import os
 from deepeval.singleton import Singleton
 from sentence_transformers import CrossEncoder
 from deepeval.progress_context import progress_context
-from deepeval.models.model_map import model_map, name_to_card
-
 
 class HallucinationModel(metaclass=Singleton):
-    def __init__(self, model_name: str = "vectara-hallucination"):
+    def __init__(self, model_name: str = "vectara/hallucination_evaluation_model"):
         # We use a smple cross encoder model
         os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -14,5 +12,4 @@ class HallucinationModel(metaclass=Singleton):
         with progress_context(
             "Downloading HallucinationEvaluationModel (may take up to 2 minutes if running for the first time)..."
         ):
-            model_name = name_to_card(model_name)
             self.model = CrossEncoder(model_name)
