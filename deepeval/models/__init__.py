97762bc670cd4c645b9008c190515deb1bcd6c56
Anindyadeep <anindya@pop-os.localdomain>
2023-11-26 19:44:37 +0530

    added answer_relevancy Classes inside __init__.py

---
+++ deepeval/models/__init__.py
@@ -0,0 +1,5 @@
+from deepeval.models.answer_relevancy_model import (
+    AnswerRelevancyModel,
+    CrossEncoderAnswerRelevancyModel,
+)
+from deepeval.models.summac_model import SummaCZS, SummaCConv
