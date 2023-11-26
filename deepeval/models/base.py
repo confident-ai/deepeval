e1578fe4a3f254e60dc7920ab91907aeec75864d
Anindyadeep <anindya@pop-os.localdomain>
2023-11-26 23:54:34 +0530

    added *args and **kwargs in __init__ of BaseModel

--- deepeval/models/base.py
+++ deepeval/models/base.py
@@ -3,9 +3,9 @@ from typing import Any, Optional
 
 
 class DeepEvalBaseModel(ABC):
-    def __init__(self, model_name: Optional[str] = None):
+    def __init__(self, model_name: Optional[str] = None, *args, **kwargs):
         self.model_name = model_name
-        self.model = self.load_model()
+        self.model = self.load_model(*args, **kwargs)
 
     @abstractmethod
     def load_model(self, *args, **kwargs):
