cd75dcd238ae6cb3ab53d723d4c16de0ff21dda1
Anindyadeep <anindya@pop-os.localdomain>
2023-11-26 23:54:04 +0530

    added SummaCModels inside __init__ file for easier import

--- deepeval/models/__init__.py
+++ deepeval/models/__init__.py
@@ -2,4 +2,4 @@ from deepeval.models.answer_relevancy_model import (
     AnswerRelevancyModel,
     CrossEncoderAnswerRelevancyModel,
 )
-from deepeval.models.summac_model import SummaCZS, SummaCConv
+from deepeval.models.summac_model import SummaCModels
