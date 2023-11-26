4a7870896ce4ba30517eedb7d0948046aa191192
Anindyadeep <anindya@pop-os.localdomain>
2023-11-27 00:24:15 +0530

    added factual_consistency_model in __init__

--- deepeval/models/__init__.py
+++ deepeval/models/__init__.py
@@ -3,3 +3,4 @@ from deepeval.models.answer_relevancy_model import (
     CrossEncoderAnswerRelevancyModel,
 )
 from deepeval.models.summac_model import SummaCModels
+from deepeval.models.factual_consistency_model import FactualConsistencyModel
