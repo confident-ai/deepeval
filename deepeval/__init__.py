181be29dedb8bd0e881419f43dadb4f5a39ead48
Jeffrey Ip <jeffreyip@confident-ai.com>
2023-12-05 19:05:41 -0800

    reformat

--- deepeval/__init__.py
+++ deepeval/__init__.py
@@ -9,7 +9,13 @@ from .decorators.hyperparameters import set_hyperparameters
 from deepeval.event import track
 from deepeval import evaluate, run_test, assert_test
 
-__all__ = ["set_hyperparameters", "track", "evaluate", "run_test", "assert_test"]
+__all__ = [
+    "set_hyperparameters",
+    "track",
+    "evaluate",
+    "run_test",
+    "assert_test",
+]
 
 
 def compare_versions(version1, version2):
