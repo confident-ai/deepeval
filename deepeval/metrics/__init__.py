96c8689ecbfc8140f5f27ab9d23f377a158e0dcb
Jeffrey Ip <jeffreyip@confident-ai.com>
2023-12-15 10:16:27 -0800

    added summarization metric

--- deepeval/metrics/__init__.py
+++ deepeval/metrics/__init__.py
@@ -1,6 +1,7 @@
 from .base_metric import BaseMetric
 from .answer_relevancy import AnswerRelevancyMetric
 from .base_metric import BaseMetric
+from .summarization import SummarizationMetric
 from .judgemental_gpt import JudgementalGPT
 from .llm_eval_metric import LLMEvalMetric
 from .ragas_metric import (
