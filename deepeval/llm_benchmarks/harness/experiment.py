0fbc21942e3b38fbb6e0ac1da1a5ef5126fb2129
Anindyadeep <anindya@pop-os.localdomain>
2023-12-17 01:16:41 +0530

    initial commit for experimentations

---
+++ deepeval/llm_benchmarks/harness/experiment.py
@@ -0,0 +1,20 @@
+from deepeval.llm_benchmarks.harness.task import HarnessTasks
+from deepeval.llm_benchmarks.harness.evaluate import (
+    HarnessEvaluate,
+    HarnessConfig,
+)
+
+
+class Experiment:
+    """An experiment will make a seperate folder in the following order:
+
+    ExperimentName:
+        - trial 1:
+            config files
+            result files
+        - trial 2:
+            config files
+            result files
+    """
+
+    raise NotImplementedError()
