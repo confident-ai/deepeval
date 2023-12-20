7bf757568282f625cb48c13f72c206cdbc9cf2f5
Jeffrey Ip <jeffreyip@confident-ai.com>
2023-12-19 20:39:16 -0800

    POC azure openai

---
+++ deepeval/cli/azure_openai.py
@@ -0,0 +1,66 @@
+import os
+import typer
+from typing import Optional
+
+try:
+    from rich import print
+except Exception as e:
+    pass
+from deepeval.key_handler import KEY_FILE_HANDLER, KeyValues
+
+app = typer.Typer(name="azure-openai")
+
+
+@app.command(name="set")
+def set_azure_openai_env(
+    azure_openai_api_key: str = typer.Option(
+        ..., "--azure-openai-api-key", help="Azure OpenAI API key"
+    ),
+    azure_openai_endpoint: str = typer.Option(
+        ..., "--azure-openai-endpoint", help="Azure OpenAI endpoint"
+    ),
+    openai_api_version: str = typer.Option(
+        ..., "--openai-api-version", help="OpenAI API version"
+    ),
+    azure_deployment_name: str = typer.Option(
+        ..., "--azure-deployment-name", help="Azure deployment name"
+    ),
+    azure_model_version: Optional[str] = typer.Option(
+        None, "--azure-model-version", help="Azure model version (optional)"
+    ),
+):
+    KEY_FILE_HANDLER.write_key(
+        KeyValues.AZURE_OPENAI_API_KEY, azure_openai_api_key
+    )
+    KEY_FILE_HANDLER.write_key(
+        KeyValues.AZURE_OPENAI_ENDPOINT, azure_openai_endpoint
+    )
+    KEY_FILE_HANDLER.write_key(KeyValues.OPENAI_API_VERSION, openai_api_version)
+    KEY_FILE_HANDLER.write_key(
+        KeyValues.AZURE_DEPLOYMENT_NAME, azure_deployment_name
+    )
+
+    if azure_model_version is not None:
+        KEY_FILE_HANDLER.write_key(
+            KeyValues.AZURE_MODEL_VERSION, azure_model_version
+        )
+
+    KEY_FILE_HANDLER.write_key(KeyValues.USE_AZURE_OPENAI, "YES")
+
+    print(
+        ":raising_hands: Congratulations! You're now using Azure OpenAI for all evals that require an LLM."
+    )
+
+
+@app.command(name="unset")
+def unset_azure_openai_env():
+    KEY_FILE_HANDLER.remove_key(KeyValues.AZURE_OPENAI_API_KEY)
+    KEY_FILE_HANDLER.remove_key(KeyValues.AZURE_OPENAI_ENDPOINT)
+    KEY_FILE_HANDLER.remove_key(KeyValues.OPENAI_API_VERSION)
+    KEY_FILE_HANDLER.remove_key(KeyValues.AZURE_DEPLOYMENT_NAME)
+    KEY_FILE_HANDLER.remove_key(KeyValues.AZURE_MODEL_VERSION)
+    KEY_FILE_HANDLER.remove_key(KeyValues.USE_AZURE_OPENAI)
+
+    print(
+        ":raising_hands: Congratulations! You're now using regular OpenAI for all evals that require an LLM."
+    )
--- deepeval/models/gpt_model.py
+++ deepeval/models/gpt_model.py
@@ -1,6 +1,8 @@
+import os
 from typing import Dict, Optional
 
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOpenAI, AzureChatOpenAI
+from deepeval.key_handler import KeyValues, KEY_FILE_HANDLER
 from deepeval.models.base import DeepEvalBaseModel
 from deepeval.chat_completion.retry import call_openai_with_retry
 
@@ -38,6 +40,33 @@ class GPTModel(DeepEvalBaseModel):
         super().__init__(model_name, *args, **kwargs)
 
     def load_model(self):
+        if self.should_use_azure_openai():
+            os.environ[
+                KeyValues.AZURE_OPENAI_API_KEY.value
+            ] = KEY_FILE_HANDLER.fetch_data(KeyValues.AZURE_OPENAI_API_KEY)
+            os.environ[
+                KeyValues.AZURE_OPENAI_ENDPOINT.value
+            ] = KEY_FILE_HANDLER.fetch_data(KeyValues.AZURE_OPENAI_ENDPOINT)
+
+            model_version = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.AZURE_MODEL_VERSION
+            )
+            model_kwargs = {}
+            if model_version is not None:
+                model_kwargs["model_version"] = model_version
+
+            openai_api_version = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.OPENAI_API_VERSION
+            )
+            azure_deployment = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.AZURE_DEPLOYMENT_NAME
+            )
+
+            return AzureChatOpenAI(
+                openai_api_version=openai_api_version,
+                azure_deployment=azure_deployment,
+                model_kwargs=model_kwargs,
+            )
         return ChatOpenAI(
             model_name=self.model_name, model_kwargs=self.model_kwargs
         )
@@ -45,3 +74,8 @@ class GPTModel(DeepEvalBaseModel):
     def _call(self, prompt: str):
         chat_model = self.load_model()
         return call_openai_with_retry(lambda: chat_model.invoke(prompt))
+
+    def should_use_azure_openai(self):
+        value = KEY_FILE_HANDLER.fetch_data(KeyValues.USE_AZURE_OPENAI)
+        print(value)
+        return value.lower() == "yes" if value is not None else False
