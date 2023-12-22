73962f10e9f7406b4d2d65e8619c0c54c0797a1b
Pratyush-exe <pratyush21225@gmail.com>
2023-12-22 08:08:40 +0530

    CHORE: Created some methods for on_epoch_end, 'DeepEvalCallback'

--- deepeval/callbacks/huggingface/__init__.py
+++ deepeval/callbacks/huggingface/__init__.py
@@ -1,2 +1,2 @@
 from deepeval.callbacks.huggingface.deepeval_callback import DeepEvalCallback
-from deepeval.callbacks.huggingface.deepeval_harness_callback import DeepEvalHarnessCallback
+# from deepeval.callbacks.huggingface.deepeval_harness_callback import DeepEvalHarnessCallback
--- deepeval/callbacks/huggingface/deepeval_callback.py
+++ deepeval/callbacks/huggingface/deepeval_callback.py
@@ -1,38 +1,121 @@
-from typing import Union, List
+from typing import Union, List, Dict
 
 from deepeval.metrics import BaseMetric
 from deepeval.dataset import EvaluationDataset
+from deepeval.evaluate import execute_test
+
 from transformers.trainer_callback import TrainerCallback
 
 
+# TODO:
+#   1. Dataset has to be created dynamically
+#   2. add score to default dict returned by on_epoch_end
+#   3. Make code more presentable
+
 class DeepEvalCallback(TrainerCallback):
     """
-    A [transformers.TrainerCallback] that logs various LLM evaluation metrics to DeepEval
+    A Transformers TrainerCallback that logs various Language Model (LM) evaluation metrics to DeepEval.
     """
-    
-    def __init__(self, metrics: Union[BaseMetric, List[BaseMetric]], evaluation_dataset: EvaluationDataset):
+
+    def __init__(
+        self,
+        metrics: Union[BaseMetric, List[BaseMetric]] = None,
+        evaluation_dataset: EvaluationDataset = None,
+        tokenizer_args: Dict = {},
+        aggregation_method: str = "average",
+    ):
+        """
+        Initialize the DeepEvalCallback.
+
+        Args:
+            metrics (Union[BaseMetric, List[BaseMetric]], optional): Evaluation metrics to calculate.
+                Defaults to None.
+            evaluation_dataset (EvaluationDataset, optional): Dataset for evaluation. Defaults to None.
+            tokenizer_args (Dict, optional): Additional arguments for tokenizer. Defaults to {}.
+            aggregation_method (str, optional): Aggregation method for metric scores ("average", "max", "min").
+                Defaults to "average".
+        """
         super().__init__()
         self.metrics = metrics
         self.evaluation_dataset = evaluation_dataset
-        
-    
-        
+        self.tokenizer_args = tokenizer_args
+        self.aggregation_method = aggregation_method
+
+    def _calculate_scores(self) -> Dict[str, List[float]]:
+        """
+        Calculate scores for each evaluation metric.
+
+        Returns:
+            Dict[str, List[float]]: Dictionary containing metric names and corresponding scores.
+        """
+        test_results = execute_test(
+            test_cases=self.evaluation_dataset.test_cases,
+            metrics=self.metrics
+        )
+
+        scores = {}
+        for test in test_results:
+            for metric in test.metrics:
+                metric_name = str(metric.__name__).lower().replace(" ", "_")
+                metric_score = metric.score
+                scores.setdefault(metric_name, []).append(metric_score)
+
+        return scores
+
+    def _aggregate_scores(
+        self, aggregation_method: str, 
+        scores: Dict[str, List[float]]
+    ) -> Dict[str, float]:
+        """
+        Aggregate metric scores based on the specified method.
+
+        Args:
+            aggregation_method (str): Aggregation method ("average", "max", "min").
+            scores (Dict[str, List[float]]): Dictionary containing metric names and scores.
+
+        Returns:
+            Dict[str, float]: Dictionary containing aggregated metric names and scores.
+        """
+        if aggregation_method in ["average", "avg"]:
+            scores = {key: (sum(value) / len(value)) for key, value in scores.items()}
+        elif aggregation_method == "max":
+            scores = {key: (max(value)) for key, value in scores.items()}
+        elif aggregation_method == "min":
+            scores = {key: (min(value)) for key, value in scores.items()}
+        else:
+            raise ValueError("Incorrect 'aggregation_method' passed, only accepts ['avg', 'min, 'max']")
+        return scores
+
     def on_epoch_end(self, args, state, control, model, tokenizer, **kwargs):
-        # if self.eval_steps is not None and state.global_step % self.eval_steps == 0:
-        #     input_text = "What if these shoes don't fit?"
-        #     context = ["All customers are eligible for a 30 day full refund at no extra costs."]
-        #     actual_output = "We offer a 30-day full refund at no extra costs."
+        """
+        Called at the end of each training epoch.
 
-        #     # Replace with actual logic for metric calculation
-        #     hallucination_metric = HallucinationMetric(minimum_score=0.7)
-        #     test_case = LLMTestCase(input=input_text, actual_output=actual_output, context=context)
-        #     assert_test(test_case, [hallucination_metric])
-
-        #     # Log or save the metric values as needed
-        print("---------ONE EPOCH ENDED---------")
-        print(model)
+        Args:
+            args: Training arguments.
+            state: Training state.
+            control: Training control.
+            model: The current model.
+            tokenizer: Tokenizer used for evaluation.
+            kwargs: Additional keyword arguments.
+        """
+        scores = self._calculate_scores()
+        scores = self._aggregate_scores(self.aggregation_method, scores)
+        print(scores)
 
     def on_train_end(self, args, state, control, model, tokenizer, **kwargs):
-        print("---------TRAIN ENDED---------")
-        print(model)
-        
+        """
+        Called at the end of the training process.
+
+        Args:
+            args: Training arguments.
+            state: Training state.
+            control: Training control.
+            model: The final model.
+            tokenizer: Tokenizer used for evaluation.
+            kwargs: Additional keyword arguments.
+        """
+        print("---------TRAIN END---------")
+
+
+
+# REWRITE THIS IN A BETTER PROFESSIONAL WAY, MAKE DOCS AND ERRORS MORE PROFESSIONAL, MAKE CODE BETTER, OPTIMIZE THE CODE AS MUCH AS YOU CAN TO LOOK BETTER
