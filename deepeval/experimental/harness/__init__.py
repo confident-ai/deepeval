b1bb07af892eea3e40ae3cb3d2a777b2ee3b5a2c
Anindyadeep <anindya@pop-os.localdomain>
2023-12-26 21:28:18 +0530

    run black

--- deepeval/experimental/harness/__init__.py
+++ deepeval/experimental/harness/__init__.py
@@ -1,2 +1,5 @@
 from deepeval.experimental.harness.task import HarnessTasks
-from deepeval.experimental.harness.experiment import HarnessConfig, HarnessExperiment
+from deepeval.experimental.harness.experiment import (
+    HarnessConfig,
+    HarnessExperiment,
+)
