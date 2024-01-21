bf81870b09b32d3bf05350f025e3b89fa7408aed
Pratyush-exe <pratyush21225@gmail.com>
2024-01-21 09:14:24 +0530

    CHORE: Added docstrings to deepeval.callbacks.huggingface.utils

--- deepeval/callbacks/huggingface/utils.py
+++ deepeval/callbacks/huggingface/utils.py
@@ -1,18 +1,42 @@
 from deepeval.test_case import LLMTestCase
+from deepeval.dataset import EvaluationDataset
 from deepeval.dataset.utils import convert_goldens_to_test_cases
-from typing import List
+from typing import List, Dict
 
 
-def get_column_order(scores: dict):
+def get_column_order(scores: Dict) -> List[str]:
+    """
+    Determine the order of columns for displaying scores.
+
+    Args:
+        scores (Dict): Dictionary containing scores.
+
+    Returns:
+        List[str]: List of column names in the desired order.
+    """
     order = ["epoch", "step", "loss", "learning_rate"]
     order.extend([key for key in scores.keys() if key not in order])
-
     return order
 
 
 def generate_test_cases(
-    model, tokenizer, tokenizer_args, evaluation_dataset
+    model,
+    tokenizer,
+    tokenizer_args: Dict,
+    evaluation_dataset: EvaluationDataset,
 ) -> List[LLMTestCase]:
+    """
+    Generate test cases based on a language model.
+
+    Args:
+        model: The language model to generate outputs.
+        tokenizer: The tokenizer for processing prompts.
+        tokenizer_args (Dict): Arguments for the tokenizer.
+        evaluation_dataset (EvaluationDataset): The dataset containing Golden.
+
+    Returns:
+        List[LLMTestCase]: List of generated test cases.
+    """
     goldens = evaluation_dataset.goldens
     for golden in goldens:
         prompt = f"""{'CONTEXT: ' + str("; ".join(golden.context)) if golden.context else ''}
