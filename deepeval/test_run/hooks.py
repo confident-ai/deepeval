5d449cc8fa5af995f17eb2bb9b374c6d16644722
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-01-23 03:38:20 -0800

    reformat

--- deepeval/test_run/hooks.py
+++ deepeval/test_run/hooks.py
@@ -4,6 +4,7 @@ on_test_run_end_hook = None
 def on_test_run_end(func):
     global on_test_run_end_hook
     on_test_run_end_hook = func
+
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
 
