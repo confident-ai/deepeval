5a54e642b928b82c3b818faa22b9e701ab9ba608
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-02-06 00:12:08 +0530

    Added a seperate config files to make things cleaner

---
+++ deepeval/experimental/harness/config.py
@@ -0,0 +1,45 @@
+import json
+from pydantic import BaseModel 
+from typing import Optional
+
+# todo: Add desc for config 
+
+class GeneralConfigMeta(BaseModel.__class__):
+    def __new__(cls, name, bases, classdict):
+        return super().__new__(cls, name, bases, classdict)
+
+class APIEndpointConfigMeta(GeneralConfigMeta):
+    def __new__(cls, name, bases, classdict):
+        return super().__new__(cls, name, bases, classdict)
+
+class GeneralConfig(BaseModel, metaclass=GeneralConfigMeta):
+    """A config common for all the other configs"""
+    top_p: Optional[float] = 0.95
+    n_samples: Optional[int] = 1
+    temperature: Optional[float] = 0.1
+    context_length: Optional[int] = 512
+    max_generation_length: Optional[int] = 512 
+    
+    # limit helps to limit the number of samples to evaluate on benchmarks
+    # right now limit, limit_start are unused
+    limit: Optional[int] = None
+    limit_start: Optional[int] = 0
+    
+    # load the generation path from an existing json
+    load_generations_path: Optional[str] = None
+    save_generations: Optional[bool] = False
+    save_generations_path: Optional[str] = 'default_generations.json'
+    metric_output_path: Optional[str] = 'default_evaluations.json'
+    
+    # information about the tokenizer
+    tokenizer_name_or_path: str 
+    
+class APIEndpointConfig(GeneralConfig, metaclass=APIEndpointConfigMeta):
+    """Config for APIs. Right now this follows only OpenAI and Endpoints with OpenAI Spec"""
+    
+    openai_api_key: Optional[str] = "sk-deepeval-default-none-key"
+    openai_api_base: Optional[str] = ""
+    openai_api_organization: Optional[str] = ""
+    model: Optional[str] = "text-davinci-003"
+
+
