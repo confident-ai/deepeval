e2b9c4eb9dfbe92ae4b2be3efda4486d1b88fdbe
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-02-25 19:07:44 +0800

    updated tests

--- tests/test_hallucination.py
+++ tests/test_hallucination.py
@@ -1,5 +1,4 @@
 import pytest
-import deepeval
 from deepeval.test_case import LLMTestCase
 from deepeval.metrics import HallucinationMetric
 from deepeval import assert_test
@@ -20,7 +19,7 @@ def test_hallucination_metric():
     assert_test(test_case, [metric])
 
 
-# @pytest.mark.skip(reason="openai is expensive")
+@pytest.mark.skip(reason="openai is expensive")
 def test_hallucination_metric_2():
     metric = HallucinationMetric(threshold=0.6)
     test_case = LLMTestCase(
@@ -30,11 +29,10 @@ def test_hallucination_metric_2():
         cost=1,
         latency=0.2,
     )
-    with pytest.raises(AssertionError):
-        assert_test(test_case, [metric])
+    assert_test(test_case, [metric])
 
 
-# @pytest.mark.skip(reason="openai is expensive")
+@pytest.mark.skip(reason="openai is expensive")
 def test_hallucination_metric_3():
     metric = HallucinationMetric(threshold=0.6)
     test_case = LLMTestCase(
@@ -44,8 +42,7 @@ def test_hallucination_metric_3():
         cost=0.1,
         latency=13.0,
     )
-    with pytest.raises(AssertionError):
-        assert_test(test_case, [metric])
+    assert_test(test_case, [metric])
 
 
 # @deepeval.set_hyperparameters(model="gpt-4")
