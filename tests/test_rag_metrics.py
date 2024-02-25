c989fd3e77f83d44774b85e3301513e9e63ece4e
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-02-25 18:06:22 +0800

    reformat

--- tests/test_rag_metrics.py
+++ tests/test_rag_metrics.py
@@ -1,10 +1,18 @@
 import pytest
 from deepeval import assert_test
-from deepeval.metrics import FaithfulnessMetric, AnswerRelevancyMetric, ContextualPrecisionMetric, ContextualRecallMetric, ContextualRelevancyMetric
+from deepeval.metrics import (
+    FaithfulnessMetric,
+    AnswerRelevancyMetric,
+    ContextualPrecisionMetric,
+    ContextualRecallMetric,
+    ContextualRelevancyMetric,
+)
 from deepeval.test_case import LLMTestCase
 
 input = "Who won the FIFA World Cup in 2018 and what was the score?"
-actual_output = "Winners of the FIFA world cup were the French national football team"
+actual_output = (
+    "Winners of the FIFA world cup were the French national football team"
+)
 expected_output = "French national football team"
 retrieval_context = [
     "The FIFA World Cup in 2018 was won by the French national football team.",
@@ -20,7 +28,7 @@ def test_rag_metrics():
         input=input,
         actual_output=actual_output,
         retrieval_context=retrieval_context,
-        expected_output=expected_output
+        expected_output=expected_output,
     )
     metric1 = AnswerRelevancyMetric(threshold=0.5)
     metric2 = FaithfulnessMetric(threshold=0.5)
