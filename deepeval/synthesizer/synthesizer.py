0181a7dd36f344fb2c153f7af0f406d6fe633936
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-02-26 23:29:41 +0800

    reformat

--- deepeval/synthesizer/synthesizer.py
+++ deepeval/synthesizer/synthesizer.py
@@ -3,8 +3,9 @@ from typing import List
 from deepeval.synthesizer.template import EvolutionTemplate
 from deepeval.test_case import LLMTestCase
 
+
 class Synthesizer:
-    
+
     def __init__(self):
         pass
 
