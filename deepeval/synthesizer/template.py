de05beb7dd5e5e70268f4978bf9c1f300c87d1c9
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-02-26 23:27:44 +0800

    reformat

--- deepeval/synthesizer/template.py
+++ deepeval/synthesizer/template.py
@@ -18,35 +18,44 @@ The LENGTH and complexity of the #Created Prompt# should be similar to that of t
 The #Created Prompt# must be reasonable and must be understood and responded by humans.\r\n\
 '#Given Prompt#', '#Created Prompt#', 'given prompt' and 'created prompt' are not allowed to appear in #Created Prompt#\r\n"
 
-class EvolInstructTemplate():
+
+class EvolInstructTemplate:
     @staticmethod
     def generate_constraints_prompt(instruction):
-        prompt = depth_base_instruction.format("Please add one more constraints/requirements into #The Given Prompt#'")
+        prompt = depth_base_instruction.format(
+            "Please add one more constraints/requirements into #The Given Prompt#'"
+        )
         prompt += "#The Given Prompt#: \r\n {} \r\n".format(instruction)
         prompt += "#Rewritten Prompt#:\r\n"
         return prompt
 
     @staticmethod
     def generate_deepen_prompt(instruction):
-        prompt = depth_base_instruction.format("If #The Given Prompt# contains inquiries about certain issues, the depth and breadth of the inquiry can be increased.")
+        prompt = depth_base_instruction.format(
+            "If #The Given Prompt# contains inquiries about certain issues, the depth and breadth of the inquiry can be increased."
+        )
         prompt += "#The Given Prompt#: \r\n {} \r\n".format(instruction)
         prompt += "#Rewritten Prompt#:\r\n"
         return prompt
 
     @staticmethod
     def generate_concretizing_prompt(instruction):
-        prompt = depth_base_instruction.format("Please replace general concepts with more specific concepts.")
+        prompt = depth_base_instruction.format(
+            "Please replace general concepts with more specific concepts."
+        )
         prompt += "#The Given Prompt#: \r\n {} \r\n".format(instruction)
         prompt += "#Rewritten Prompt#:\r\n"
         return prompt
-    
+
     @staticmethod
     def generate_reasoning_prompt(instruction):
-        prompt = depth_base_instruction.format("If #The Given Prompt# can be solved with just a few simple thinking processes, you can rewrite it to explicitly request multiple-step reasoning.")
+        prompt = depth_base_instruction.format(
+            "If #The Given Prompt# can be solved with just a few simple thinking processes, you can rewrite it to explicitly request multiple-step reasoning."
+        )
         prompt += "#The Given Prompt#: \r\n {} \r\n".format(instruction)
         prompt += "#Rewritten Prompt#:\r\n"
         return prompt
-        
+
     @staticmethod
     def generate_breadth_prompt(instruction):
         prompt = breadth_base_instruction
