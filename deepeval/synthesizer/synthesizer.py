5c32f05ba2ee5d8b1cb379bb0f85ad316ef23ac2
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-02-27 18:57:53 +0800

    POC

--- deepeval/synthesizer/synthesizer.py
+++ deepeval/synthesizer/synthesizer.py
@@ -4,20 +4,21 @@ from threading import Thread, Lock
 from pydantic import BaseModel, Field
 
 from deepeval.synthesizer.template import EvolutionTemplate, SynthesizerTemplate
-from deepeval.models import GPTModel, DeepEvalBaseLLM
-from deepeval.test_case import LLMTestCase, LLMTestCaseParams
+from deepeval.models import GPTModel, DeepEvalBaseLLM, DeepEvalBaseEmbeddingModel
 from deepeval.utils import trimAndLoadJson
+from deepeval.dataset import Golden
 
 
 class SyntheticData(BaseModel):
     input: str
-    expected_output: str = Field(default=None)
+    expected_output: str
 
 
 class Synthesizer:
     def __init__(
         self,
         model: Optional[Union[str, DeepEvalBaseLLM]] = None,
+        embedder: Optional[Union[str, DeepEvalBaseEmbeddingModel]] = None,
         multithreading: bool = True,
         batch_size: int = 50,
     ):
@@ -25,35 +26,46 @@ class Synthesizer:
             self.model = model
         else:
             self.model = GPTModel(model=model)
+
+        self.embedder = embedder
         self.synthesizer_model = self.model.get_model_name()
         self.multithreading = multithreading
         self.batch_size = batch_size
+        self.synthetic_data = None
 
     # TODO
-    def synthesize(self, context: List[str]) -> List[LLMTestCase]:
+    def synthesize(self, context: List[str]) -> List[Golden]:
         # 1. get embeddings for context eg., [1,2,3,4,5]
         # 2. group randomly based on embedding similarity eg., [[1,2], [5,2], [4], [3,1,5]]
         # 3. supply as context, generate for each List[str]
         # 4. generation can happen in batches, and each batch can be processed in threads
         # 5. optional evolution
-        # 6. return test cases
+        # 6. optional review
+        # 7. return goldens
 
+        # TODO: logic to group and vary contexts
+
+        # TODO: batch generation
         prompt = SynthesizerTemplate.generate_synthetic_data(context=context)
         res = self.model(prompt)
         data = trimAndLoadJson(res)
-        synthetic_data = [SyntheticData(**item) for item in data["data"]]
+        self.synthetic_data = [SyntheticData(**item) for item in data["data"]]
 
-        test_cases: List[LLMTestCase] = []
+        # TODO: optional evolution
 
-        for data in synthetic_data:
-            test_case = LLMTestCase(
+        # TODO: review synthetic data
+
+        goldens: List[Golden] = []
+
+        for data in self.synthetic_data:
+            golden = Golden(
                 input=data.input,
-                expected_output=data.expected_output,
+                expectedOutput=data.expected_output,
                 context=context,
             )
-            test_cases.append(test_case)
+            goldens.append(golden)
 
-        return test_cases
+        return goldens
 
     # TODO
     def synthesize_from_docs(self, path: str):
