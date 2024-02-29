115014a70a192d73e08fdc6238d8e4c67bbcda29
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-02-29 18:56:23 +0800

    example benchmark prototype

--- deepeval/benchmarks/base_benchmark.py
+++ deepeval/benchmarks/base_benchmark.py
@@ -2,11 +2,21 @@ from abc import ABC, abstractmethod
 from typing import Any, Optional, List
 
 from deepeval.test_case import LLMTestCase
+from deepeval.dataset import Golden
 
 
 class DeepEvalBaseBenchmark(ABC):
     def __init__(self, *args, **kwargs):
         self._test_cases: Optional[List[LLMTestCase]] = None
+        self._goldens: Optional[List[Golden]] = None
+
+    @property
+    def goldens(self) -> Optional[List[Golden]]:
+        return self._goldens
+
+    @goldens.setter
+    def goldens(self, goldens: List[Golden]):
+        self._goldens = goldens
 
     @property
     def test_cases(self) -> Optional[List[LLMTestCase]]:
@@ -17,6 +27,6 @@ class DeepEvalBaseBenchmark(ABC):
         self._test_cases = test_cases
 
     @abstractmethod
-    def load_benchmark_dataset(self, *args, **kwargs) -> None:
-        """Load the benchmark dataset and initialize test cases."""
-        pass
+    def load_benchmark_dataset(self, *args, **kwargs) -> List[Golden]:
+        """Load the benchmark dataset and initialize goldens."""
+        raise NotImplementedError
--- deepeval/benchmarks/example.py
+++ deepeval/benchmarks/example.py
@@ -1,12 +1,17 @@
+from typing import List
+
+from deepeval.dataset import Golden
 from deepeval.benchmarks.base_benchmark import DeepEvalBaseBenchmark
+from deepeval.models import DeepEvalBaseLLM
 
 
 class ExampleBenchmark(DeepEvalBaseBenchmark):
-    def __init__(self):
+    def __init__(self, model : DeepEvalBaseLLM):
         super().__init__()
         # Now call the method to load the benchmark dataset and set test cases.
-        self.load_benchmark_dataset()
+        self.goldens = self.load_benchmark_dataset()
+        self.model = model
 
-    def load_benchmark_dataset(self):
+    def load_benchmark_dataset(self)  -> List[Golden]:
         # load from hugging face
         pass
