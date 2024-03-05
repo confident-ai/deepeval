ee188d9a8f6628175d251229801266baad8f1f5a
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-05 11:32:16 +0530

    not related to harness. adding for black

--- deepeval/metrics/accuracy_metric.py
+++ deepeval/metrics/accuracy_metric.py
@@ -3,23 +3,26 @@ from deepeval.test_case import LLMTestCase
 from deepeval.metrics import BaseMetric
 from deepeval.scorer import Scorer
 
+
 class ExactMatchAccuracyMetric(BaseMetric, metaclass=Singleton):
     def __init__(self, minimum_score: float = 0.5) -> None:
         self.minimum_score = minimum_score
-    
+
     def __call__(self, test_case: LLMTestCase) -> float:
         self.score = self.measure(test_case)
         self.success = self.score > self.minimum_score
-        return self.score 
-    
+        return self.score
+
     def measure(self, test_case: LLMTestCase) -> float:
-        self.score = Scorer.exact_match_score(target=test_case.expected_output, prediction=test_case.actual_output)
+        self.score = Scorer.exact_match_score(
+            target=test_case.expected_output, prediction=test_case.actual_output
+        )
         self.success = self.score > self.minimum_score
-        return self.score 
-      
+        return self.score
+
     def is_successful(self) -> bool:
         return self.success
-    
+
     @property
     def __name__(self):
         return "ExactMatchAccuracyMetric"
