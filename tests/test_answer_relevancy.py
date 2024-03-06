60efb5e5635cf4ea4aa67a47c4000d2ff53f4c7e
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-03-06 08:42:01 +0800

    updated tests

--- tests/test_answer_relevancy.py
+++ tests/test_answer_relevancy.py
@@ -46,7 +46,7 @@ enhancing brain functions related to concentration and attention.
 """
 
 
-# @pytest.mark.skip(reason="openai is very expensive")
+@pytest.mark.skip(reason="openai is very expensive")
 def test_answer_relevancy():
     metric = AnswerRelevancyMetric(threshold=0.5, strict_mode=True)
     test_case = LLMTestCase(
