d68063b26c7fe3ef8916b843580087b13e0e235e
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-03-08 19:38:07 +0800

    .

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -74,7 +74,9 @@ strict_mode = True
 
 @pytest.mark.skip(reason="openai is expensive")
 def test_everything():
-    metric1 = AnswerRelevancyMetric(threshold=0.5, strict_mode=strict_mode, asynchronous=True)
+    metric1 = AnswerRelevancyMetric(
+        threshold=0.5, strict_mode=strict_mode, asynchronous=True
+    )
     metric2 = FaithfulnessMetric(threshold=0.5, strict_mode=strict_mode)
     metric3 = ContextualPrecisionMetric(threshold=0.5, strict_mode=strict_mode)
     metric4 = ContextualRecallMetric(threshold=0.5, strict_mode=strict_mode)
