9521c933432567bf349dca3ad3c9f6708e1ba987
Kritin Vongthongsri <73642562+kritinv@users.noreply.github.com>
2024-03-19 19:36:48 -0400

    Created OpenAIEmbedding from base and implemented fast_mode
    - Created OpenAIEmbeddingModel using DeepEvalBaseEmbeddingModel
    - Created fast mode by introducing option to use Faiss as DB store (ANN)

--- deepeval/synthesizer/context_generator.py
+++ deepeval/synthesizer/context_generator.py
@@ -1,24 +1,34 @@
-from deepeval.synthesizer.doc_chunker import DocumentChunker, Chunk, Similarity
-from langchain_openai import OpenAIEmbeddings
+from deepeval.synthesizer.doc_chunker import DocumentChunker, Chunk, get_embedding_similarity
+from deepeval.models.openai_embedding_model import OpenAIEmbeddingModel
+from deepeval.models.base_model import DeepEvalBaseEmbeddingModel
 from typing import List, Optional
+import numpy as np
 import random
+import faiss
 
 class ContextGenerator:
     def __init__(self, 
              document_paths: List[str],
              chunk_size: int = 1024,
-             chunk_overlap: int = 0
+             chunk_overlap: int = 0,
+             fast_mode: bool = True
         ):
-        self.document_paths: List[str] = document_paths
-        self.combined_chunks: Optional[List[Chunk]] = None
-        self.embedder: OpenAIEmbeddings = OpenAIEmbeddings()
+        
+        self.embedder: DeepEvalBaseEmbeddingModel = OpenAIEmbeddingModel()
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
 
+        self.document_paths: List[str] = document_paths
+        self.combined_chunks: List[Chunk] = self._load_docs()
+        
+        self.fast_mode = fast_mode
+        if fast_mode:
+            self.index = self._build_faiss_index()
+
     ############### Generate Topics ########################
     def generate_contexts(self, output_size: int, max_context_size: int = 2):
-        chunks = self._load_docs()
-        num_chunks = len(chunks)
+        
+        num_chunks = len(self.combined_chunks)
         if output_size > num_chunks:
             raise ValueError("Not enough chunks (" + str(num_chunks) + 
                              ") to return the requested number of outputs ("  + str(output_size) + 
@@ -32,14 +42,25 @@ class ContextGenerator:
     
     
      ############### Load Docs #############################
-    def _load_docs(self):
+    def _load_docs(self) -> List[Chunk]:
         combined_chunks = []
         for path in self.document_paths:
-            doc_chunker = DocumentChunker(self.chunk_size, self.chunk_overlap)
+            doc_chunker = DocumentChunker(self.embedder, self.chunk_size, self.chunk_overlap)
             chunks = doc_chunker.load_doc(path)
             combined_chunks.extend(chunks)
-        self.combined_chunks = combined_chunks
         return combined_chunks
+    
+    def _build_faiss_index(self) -> faiss.IndexFlatL2:
+        d = len(self.combined_chunks[0].embedding)
+        index = faiss.IndexFlatL2(d)
+        embeddings = np.array([chunk.embedding for chunk in self.combined_chunks])
+        # Normalize embeddings to unit length
+        norms = np.linalg.norm(embeddings, axis=1, keepdims=True)
+        normalized_embeddings = embeddings / norms
+        
+        # Add normalized embeddings to the FAISS index
+        index.add(normalized_embeddings)
+        return index
     
     ############### Search N Chunks ########################
     def _get_n_random_clusters(self, n: int, cluster_size: int):
@@ -47,7 +68,7 @@ class ContextGenerator:
         random_chunks = self._get_n_random_chunks(n)
         for chunk in random_chunks:
             cluster = [chunk]
-            cluster.extend(self._get_n_similar_chunks(chunk, n = cluster_size - 1))
+            cluster.extend(self._get_n_other_similar_chunks(chunk, n = cluster_size - 1))
             chunk_cluster.append(cluster)
         return chunk_cluster
 
@@ -56,17 +77,48 @@ class ContextGenerator:
             raise ValueError("Not enough chunks to return the requested number of random nodes.")
         return random.sample(self.combined_chunks, n)
     
-    def _get_n_similar_chunks(
-        self, chunk: Chunk, n: int, threshold: float = 0.7, 
+    def _get_n_other_similar_chunks(
+        self, query_chunk: Chunk, n: int, threshold: float = 0.7, fast_threshold: float = 0.6
     ): 
         if not self.combined_chunks or len(self.combined_chunks) < n:
             raise ValueError("Not enough chunks to return the requested number of random nodes.")
-        embedding = self.embedder.embed_query(chunk.content)
-        similarities = [Similarity.get_embedding_similarity(embedding, (c.embedding)) for c in self.combined_chunks]
-        filtered_indices = [i for i, sim in enumerate(similarities) if sim > threshold and self.combined_chunks[i].id != chunk.id]
-        sorted_indices = sorted(filtered_indices, key=lambda i: similarities[i], reverse=True)
-        top_n_indices = sorted_indices[:n]
-        similar_chunks = [self.combined_chunks[i] for i in top_n_indices]
+        query_embedding = self.embedder.embed_query(query_chunk.content)
+        similar_chunks = None
+
+        if not self.fast_mode:
+            similarities = []
+            filtered_indices = []
+
+            for (i, c) in enumerate(self.combined_chunks):
+                sim = get_embedding_similarity(query_embedding, (c.embedding))
+                similarities.append(sim)
+
+                if sim > threshold and self.combined_chunks[i].id != query_chunk.id:
+                    filtered_indices.append(i)
+
+            sorted_indices = sorted(filtered_indices, key=lambda i: similarities[i], reverse=True)
+            top_n_indices = sorted_indices[:n]
+            similar_chunks = [self.combined_chunks[i] for i in top_n_indices]
+
+        else:
+            query_embedding = np.array(query_embedding).reshape(1, -1) 
+            query_norm = np.linalg.norm(query_embedding, axis=1, keepdims=True)
+            normalized_query_embedding = query_embedding / query_norm
+            distances, indices = self.index.search(normalized_query_embedding, n + 1)
+            similar_chunks = []
+            found = 0  # Counter for found chunks that meet the criteria
+
+            for i in range(len(indices[0])):
+                curr_idx = indices[0][i]
+                curr_dis = distances[0][i]
+                if self.combined_chunks[curr_idx].id == query_chunk.id: 
+                    continue  
+                if curr_dis > fast_threshold: 
+                    continue
+                similar_chunks.append(self.combined_chunks[curr_idx])
+                found += 1
+                if found == n:  # Stop once we have found 'n' similar chunks
+                    break
 
         return similar_chunks    
     
@@ -74,8 +126,29 @@ class ContextGenerator:
 ################# Example Usage# ###################
 ####################################################
 
+'''
+import time
+
 if __name__ == "__main__":
-    paths = ["example_data/txt_example.txt", "example_data/docx_example.docx",  "example_data/pdf_example.pdf"]
-    generator = ContextGenerator(paths)#, chunk_size=10, chunk_overlap=0)
-    contexts = generator.generate_contexts(5)
-    print(contexts)
+    paths = ["example_data/txt_example.txt", "example_data/docx_example.docx", "example_data/pdf_example.pdf"]
+    
+    # Without Fast Mode (FAISS)
+    start_no_faiss = time.time()
+    generator_no_faiss = ContextGenerator(paths, chunk_size=100, fast_mode=False)
+    contexts_no_faiss = generator_no_faiss.generate_contexts(5)
+    end_no_faiss = time.time()
+    
+    contexts_no_faiss_shapes = [len(context) for context in contexts_no_faiss]
+    print(f"Shapes of contexts without FAISS: {contexts_no_faiss_shapes}")
+    print(f"Time taken without FAISS: {end_no_faiss - start_no_faiss} seconds\n")
+    
+    # With Fast Mode (FAISS)
+    start_with_faiss = time.time()
+    generator_with_faiss = ContextGenerator(paths, chunk_size=100, fast_mode=True)
+    contexts_with_faiss = generator_with_faiss.generate_contexts(5)
+    end_with_faiss = time.time()
+    
+    contexts_with_faiss_shapes = [len(context) for context in contexts_with_faiss]
+    print(f"Shapes of contexts with FAISS: {contexts_with_faiss_shapes}")
+    print(f"Time taken with FAISS: {end_with_faiss - start_with_faiss} seconds")
+'''
--- deepeval/synthesizer/doc_chunker.py
+++ deepeval/synthesizer/doc_chunker.py
@@ -2,11 +2,14 @@ from langchain_core.documents import Document as LCDocument
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
 from langchain_text_splitters import TokenTextSplitter
 from langchain_text_splitters.base import TextSplitter
-from langchain_openai import OpenAIEmbeddings
-from typing import Optional, List, Tuple
+
+from deepeval.models.openai_embedding_model import OpenAIEmbeddingModel
+from deepeval.models.base_model import DeepEvalBaseEmbeddingModel
+from typing import Optional, List, Tuple, Dict, Type
 from pydantic import Field
 from enum import Enum
 import numpy as np
+import uuid
 import os
 
 
@@ -25,22 +28,17 @@ class Chunk:
 class DocumentChunker:
     def __init__(
         self,
+        embedder: DeepEvalBaseEmbeddingModel, 
         chunk_size: int = 1024,
-        chunk_overlap: int = 0
+        chunk_overlap: int = 0, 
     ):
-        # Unique ID for each document
         self.doc_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
-
-        # Choose TokenTextSplitter as only text_splitter for now
-        self.text_splitter: TextSplitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-
-        # Chunks contain LCDocument, while nodes contain strings (LCDocument.page_content)
         self.chunks: Optional[List[Chunk]] = None
-        
-        # Embedder is the embedding model and embeddings are the embedded nodes
-        self.embedder: OpenAIEmbeddings = OpenAIEmbeddings()
+
+        self.text_splitter: TextSplitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        self.embedder: DeepEvalBaseEmbeddingModel = embedder
         self.mean_embedding: Optional[float] = None
 
          # Mapping of file extensions to their respective loader classes
@@ -53,29 +51,29 @@ class DocumentChunker:
     ############### Load and Chunk ###############
     def load_doc(self, path: str) -> List[LCDocument]:
 
+        # Find appropiate doc loader
         _, extension = os.path.splitext(path)
         extension = extension.lower()
-        # Select the appropriate loader based on the file extension
         loader_class = self.loader_mapping.get(extension)
         if loader_class is None:
             raise ValueError(f"Unsupported file format: {extension}")
         
+        # Load and split text in doc
         loader = loader_class(path)
-        raw_chunks = loader.load_and_split(self.text_splitter)
-        contents = [rc.page_content for rc in raw_chunks] 
+        raw_chunks: List[LCDocument] = loader.load_and_split(self.text_splitter)
 
-        # Utilize lang_chain for run async
+        # Load results into Chunk class
+        contents = [rc.page_content for rc in raw_chunks] 
         embeddings = self.embedder.embed_documents(contents)
         embeddings_np = np.array(embeddings)
         mean_embedding = np.mean(embeddings_np, axis=0)
-
         chunks = []
         for i in range(len(raw_chunks)):
             chunk = Chunk()
             chunk.content = contents[i]
             chunk.embedding = embeddings[i]
             chunk.source_doc_id = self.doc_id,
-            chunk.similarity_to_mean = Similarity.get_embedding_similarity((embeddings_np[i]), mean_embedding)
+            chunk.similarity_to_mean = get_embedding_similarity((embeddings_np[i]), mean_embedding)
             chunks.append(chunk)
         self.chunks = chunks
 
@@ -84,25 +82,12 @@ class DocumentChunker:
 ####################################################
 ############### Similarity Funcs ###################
 ####################################################
-    
-class SIMILARITY_MEASURE(str, Enum):
-    COSINE = "cosine"
-    DOT_PRODUCT = "dot_product"
-    EUCLIDEAN = "euclidean"
 
-class Similarity:
-    
-    @staticmethod
-    def get_embedding_similarity(
-        embedding1: List,
-        embedding2: List,
-        type: Enum = SIMILARITY_MEASURE.COSINE,
-        ) -> float:
-         if type == SIMILARITY_MEASURE.COSINE:
-             product = np.dot(embedding1, embedding2)
-             norm = np.linalg.norm(embedding1) * np.linalg.norm(embedding2)
-             return product / norm
-         elif type == SIMILARITY_MEASURE.EUCLIDEAN:
-            return -float(np.linalg.norm(np.array(embedding1) - np.array(embedding2)))
-         elif type == SIMILARITY_MEASURE.DOT_PRODUCT:
-                return np.dot(embedding1, embedding2)    
+def get_embedding_similarity(
+    embedding1: List,
+    embedding2: List,
+    ) -> float:
+    product = np.dot(embedding1, embedding2)
+    norm = np.linalg.norm(embedding1) * np.linalg.norm(embedding2)
+    return product / norm
+       
