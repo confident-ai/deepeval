856a9390159faa58378528849a33bd80a66f4ad0
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2024-05-07 17:37:13 +0800

    fix failing test case

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -10,17 +10,49 @@ from deepeval.evaluate import aggregate_metric_pass_rates, TestResult
 from deepeval.metrics import AnswerRelevancyMetric, BiasMetric
 
 
-class StubMetric(BaseMetric):
-    def __init__(self, _success: bool, _name: str):
+class FakeMetric1(BaseMetric):
+    def __init__(self, threshold: float = 0.5, _success: bool = True):
+        self.threshold = threshold
         self.success = _success
-        self.name = _name
+
+    def measure(self, test_case: LLMTestCase):
+        self.reason = "This metric looking good!"
+        return self.score
+
+    async def a_measure(self, test_case: LLMTestCase):
+        self.score = 0.5
+        self.reason = "This async metric looking good!"
+        return self.score
 
     def is_successful(self):
         return self.success
 
     @property
     def __name__(self):
-        return self.name
+        return "Fake"
+
+
+class FakeMetric2(BaseMetric):
+    def __init__(self, threshold: float = 0.5, _success: bool = True):
+        self.threshold = threshold
+        self.success = _success
+
+    def measure(self, test_case: LLMTestCase):
+        self.score = 0.5
+        self.reason = "This metric looking good!"
+        return self.score
+
+    async def a_measure(self, test_case: LLMTestCase):
+        self.score = 0.5
+        self.reason = "This async metric looking good!"
+        return self.score
+
+    def is_successful(self):
+        return self.success
+
+    @property
+    def __name__(self):
+        return "Fake"
 
 
 def test_create_dataset():
@@ -42,21 +74,35 @@ def test_create_dataset():
     dataset.add_test_cases_from_json_file(
         file_path,
         input_key_name="query",
-        actual_output_key_name="actual_output",
         expected_output_key_name="expected_output",
         context_key_name="context",
         retrieval_context_key_name="retrieval",
+        actual_output_key_name="actual_output",
     )
     assert len(dataset.test_cases) == 10, "Test Cases not loaded from JSON"
 
 
 def test_aggregate_metric_pass_rates():
+    test_case = LLMTestCase(
+        input="What is the primary difference between a comet and an asteroid?",
+        actual_output="Comets and asteroids are both celestial bodies found in our solar system but differ in composition and behavior. Comets, made up of ice, dust, and small rocky particles, develop glowing comas and tails when near the Sun. In contrast, asteroids are primarily rocky or metallic and are mostly found in the asteroid belt between Mars and Jupiter. Rabbits are found in the wild.",
+        expected_output="Comets and asteroids are both celestial bodies found in our solar system but differ in composition and behavior. Comets, made up of ice, dust, and small rocky particles, develop glowing comas and tails when near the Sun. In contrast, asteroids are primarily rocky or metallic and are mostly found in the asteroid belt between Mars and Jupiter. Rabbits are found in the wild.",
+    )
+    fake_metric_1_fail = FakeMetric1(_success=False)
+    fake_metric_2_fail = FakeMetric2()
+    fake_metric_1_fail.measure(test_case=test_case)
+    fake_metric_2_fail.measure(test_case=test_case)
+
+    fake_metric_1_pass = FakeMetric1()
+    fake_metric_2_pass = FakeMetric2()
+    fake_metric_1_pass.measure(test_case=test_case)
+    fake_metric_2_pass.measure(test_case=test_case)
     test_results = [
         TestResult(
-            success=True,
+            success=False,
             metrics=[
-                StubMetric(_success=True, _name="AnswerRelevancyMetric"),
-                StubMetric(_success=True, _name="BiasMetric"),
+                fake_metric_1_fail,
+                fake_metric_2_fail,
             ],
             input="some input",
             actual_output="some output",
@@ -67,8 +113,8 @@ def test_aggregate_metric_pass_rates():
         TestResult(
             success=True,
             metrics=[
-                StubMetric(_success=False, _name="AnswerRelevancyMetric"),
-                StubMetric(_success=True, _name="BiasMetric"),
+                fake_metric_1_pass,
+                fake_metric_2_pass,
             ],
             input="another input",
             actual_output="another output",
@@ -78,8 +124,6 @@ def test_aggregate_metric_pass_rates():
         ),
     ]
 
-    expected_result = {"AnswerRelevancyMetric": 0.5, "BiasMetric": 1.0}
+    expected_result = {"FakeMetric1": 0.5, "FakeMetric2": 1.0}
     result = aggregate_metric_pass_rates(test_results)
-    assert (
-        result == expected_result
-    ), "The aggregate metric pass rates do not match the expected values."
+    assert result == expected_result
