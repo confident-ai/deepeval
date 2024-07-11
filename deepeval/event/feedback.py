aa1536d58aeca968c67919ad3f9d4b815be7cd92
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-07-11 15:48:21 +0800

    Updated feedback sending

--- deepeval/event/feedback.py
+++ deepeval/event/feedback.py
@@ -6,7 +6,6 @@ from deepeval.event.api import APIFeedback
 
 def send_feedback(
     event_id: str,
-    provider: str,
     rating: int,
     expected_response: Optional[str] = None,
     explanation: Optional[str] = None,
@@ -14,15 +13,8 @@ def send_feedback(
     raise_expection: Optional[bool] = True,
 ) -> str:
     try:
-        if provider != "user" and provider != "reviewer":
-            raise ValueError("'provider' must be either 'user' or 'reviewer'.")
-
-        if rating < 1 or rating > 5:
-            raise ValueError("'rating' must be between 1 and 5, inclusive.")
-
         api_event = APIFeedback(
             eventId=event_id,
-            provider=provider,
             rating=rating,
             expectedResponse=expected_response,
             explanation=explanation,
@@ -34,7 +26,7 @@ def send_feedback(
             # Pydantic version below 2.0
             body = api_event.dict(by_alias=True, exclude_none=True)
 
-        result = api.post_request(
+        api.post_request(
             endpoint=Endpoints.FEEDBACK_ENDPOINT.value,
             body=body,
         )
