f90494be6dfb7a671df54488cb37952295dc5db9
Kritin Vongthongsri <73642562+kritinv@users.noreply.github.com>
2024-09-11 15:36:38 +0700

    task

--- deepeval/metrics/__init__.py
+++ deepeval/metrics/__init__.py
@@ -12,7 +12,7 @@ from .contextual_relevancy.contextual_relevancy import ContextualRelevancyMetric
 from .contextual_precision.contextual_precision import ContextualPrecisionMetric
 from .knowledge_retention.knowledge_retention import KnowledgeRetentionMetric
 from .tool_correctness.tool_correctness import ToolCorrectnessMetric
-from .viescore.viescore import VIEScore
+from .viescore.viescore import VIEScore, VIEScoreTask
 from .conversation_relevancy.conversation_relevancy import (
     ConversationRelevancyMetric,
 )
