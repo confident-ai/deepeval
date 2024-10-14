f10d3dd60f899c1f0528c41ed8a51619dd851b0c
Jeffrey Ip <jeffreyip@confident-ai.com>
2024-10-14 18:30:57 +0800

    updated webhook

--- deepeval/confident/__init__.py
+++ deepeval/confident/__init__.py
@@ -1,1 +1,2 @@
 from .evaluate import confident_evaluate
+from .utils import construct_confident_event
---
+++ deepeval/confident/utils.py
@@ -0,0 +1,7 @@
+from typing import Any
+
+
+def construct_confident_event(
+    payload: Any, confident_ai_signature: str, confident_secret: str
+):
+    raise ValueError("Please contact support@confident-ai.com for beta access.")
