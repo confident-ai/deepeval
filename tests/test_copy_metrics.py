67e9c7cd0c8612aa32a98a25497d88895cca0f94
Vytenis Šliogeris <vytenis@neurotechnology.com>
2024-11-29 15:25:00 +0200

    Made test check model names instead of object addresses

--- tests/test_copy_metrics.py
+++ tests/test_copy_metrics.py
@@ -4,6 +4,7 @@ import pytest
 from deepeval.metrics import GEval
 from deepeval.metrics.utils import copy_metrics
 from deepeval.test_case import LLMTestCaseParams
+from deepeval.models.gpt_model import GPTModel
 
 class DummyMetric(GEval):
     def __init__(self, **kwargs):
@@ -27,5 +28,8 @@ def test_copy_metrics():
     vars_after = vars(metric_after[0])
     for key_before, value_before in vars_before.items():
         value_after = vars_after[key_before]
-        assert value_before == value_after
+        if isinstance(value_before, GPTModel):
+            assert value_before.model_name == value_after.model_name
+        else:
+            assert value_before == value_after
         assert key_before in vars_after.keys()
