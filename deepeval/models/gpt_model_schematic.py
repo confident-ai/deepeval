5be2d4c10f61eb255a2b74168325b0a886bb0ba5
Jack Luar <39641663+luarss@users.noreply.github.com>
2025-01-19 16:16:51 +0000

    openai model updates - make sure gpt_model and gpt_model_schematic share same set of models - update valid multimodal models - update various costs
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

--- deepeval/models/gpt_model_schematic.py
+++ deepeval/models/gpt_model_schematic.py
@@ -33,26 +33,35 @@ valid_gpt_models = [
     "gpt-4o-2024-11-20",
     "gpt-4o-mini",
     "gpt-4o-mini-2024-07-18",
+    "gpt-4-32k",
+    "gpt-4-32k-0613",
 ]
 
 structured_outputs_models = [
     "gpt-4o",
-    "gpt-4o-mini"
+    "gpt-4o-2024-05-13",
+    "gpt-4o-2024-08-06",
+    "gpt-4o-2024-11-20",
+    "gpt-4o-mini",
+    "gpt-4o-mini-2024-07-18",
 ]
 
 json_mode_models = [
-    "gpt-4-turbo",
-    "gpt-4-turbo-preview",
-    "gpt-4-0125-preview",
-    "gpt-4-1106-preview",
-    "gpt-4",
-    "gpt-4-32k",
-    "gpt-4-0613",
-    "gpt-4-32k-0613",
-    "gpt-3.5-turbo-1106",
     "gpt-3.5-turbo",
-    "gpt-3.5-turbo-16k",
     "gpt-3.5-turbo-0125",
+    "gpt-3.5-turbo-1106",
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-instruct",
+    "gpt-3.5-turbo-instruct-0914",
+    "gpt-4",
+    "gpt-4-0125-preview",
+    "gpt-4-0613",
+    "gpt-4-1106-preview",
+    "gpt-4-turbo",
+    "gpt-4-turbo-2024-04-09",
+    "gpt-4-turbo-preview",
+    "gpt-4-32k",
+    "gpt-4-32k-0613",
 ]
 
 default_gpt_model = "gpt-4o"
