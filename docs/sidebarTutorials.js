f676815e942203a9f3b0833e0fdb56d318f75471
Jeffrey Ip <jeffreyip@confident-ai.com>
2025-01-28 10:45:23 -0800

    updated docs

--- docs/sidebarTutorials.js
+++ docs/sidebarTutorials.js
@@ -1,46 +1,21 @@
 module.exports = {
     tutorials: [
       "tutorial-introduction",
+      "tutorial-setup", 
       {
         type: "category",
-        label: "Before We Begin",
-        items: ["tutorial-setup", "tutorial-llm-application-example"],
-        collapsed: false,
-      },
-      {
-        type: "category",
-        label: "Choosing Your Metrics",
+        label: "Medical Chatbot",
         items: [
+          "tutorial-llm-application-example",  
           "tutorial-metrics-defining-an-evaluation-criteria",
-          "tutorial-metrics-selection",
-        ],
-        collapsed: false,
-      },
-      {
-        type: "category",
-        label: "Evaluating Your LLM",
-        items: [
+          "tutorial-metrics-selection",     
           "tutorial-evaluations-running-an-evaluation",
           "tutorial-evaluations-hyperparameters",
-          "tutorial-evaluations-catching-regressions",
-        ],
-        collapsed: false,
-      },
-      {
-        type: "category",
-        label: "Improving Your Evaluation Dataset",
-        items: [
+          "tutorial-evaluations-catching-regressions",           
           "tutorial-dataset-synthesis",
-          "tutorial-dataset-confident",
-        ],
-        collapsed: false,
-      },
-      {
-        type: "category",
-        label: "Continous Evaluation in Production",
-        items: [
+          "tutorial-dataset-confident",           
           "tutorial-production-monitoring",
-          "tutorial-production-evaluation",
+          "tutorial-production-evaluation"
         ],
         collapsed: false,
       },
