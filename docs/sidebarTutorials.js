98cbe4dc5e957c1f56b76450a6bfb7b6f6fd5d7d
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-01-28 22:42:44 -0800

    legal doc summarizer

--- docs/sidebarTutorials.js
+++ docs/sidebarTutorials.js
@@ -1,24 +1,34 @@
 module.exports = {
-    tutorials: [
-      "tutorial-introduction",
-      "tutorial-setup", 
-      {
-        type: "category",
-        label: "Medical Chatbot",
-        items: [
-          "tutorial-llm-application-example",  
-          "tutorial-metrics-defining-an-evaluation-criteria",
-          "tutorial-metrics-selection",     
-          "tutorial-evaluations-running-an-evaluation",
-          "tutorial-evaluations-hyperparameters",
-          "tutorial-evaluations-catching-regressions",           
-          "tutorial-dataset-synthesis",
-          "tutorial-dataset-confident",           
-          "tutorial-production-monitoring",
-          "tutorial-production-evaluation"
-        ],
-        collapsed: false,
-      },
-    ],
-  };
-  
+  tutorials: [
+    "tutorial-introduction",
+    "tutorial-setup",
+    {
+      type: "category",
+      label: "Legal Document Summarizer",
+      items: [
+        "legal-doc-summarizer-introduction",
+        "legal-doc-summarizer-defining-a-summarization-criteria",
+        "legal-doc-summarizer-selecting-your-metrics",
+        "legal-doc-summarizer-running-an-evaluation",
+      ],
+      collapsed: false,
+    },
+    {
+      type: "category",
+      label: "Medical Chatbot",
+      items: [
+        "tutorial-llm-application-example",
+        "tutorial-metrics-defining-an-evaluation-criteria",
+        "tutorial-metrics-selection",
+        "tutorial-evaluations-running-an-evaluation",
+        "tutorial-evaluations-hyperparameters",
+        "tutorial-evaluations-catching-regressions",
+        "tutorial-dataset-synthesis",
+        "tutorial-dataset-confident",
+        "tutorial-production-monitoring",
+        "tutorial-production-evaluation",
+      ],
+      collapsed: false,
+    },
+  ],
+};
