142c9129a9a2f02501be0497a232c3ffb157d0e9
Jeffrey Ip <jeffreyip@confident-ai.com>
2025-02-02 23:44:33 -0800

    reformat

--- deepeval/benchmarks/utils.py
+++ deepeval/benchmarks/utils.py
@@ -7,7 +7,7 @@ def should_use_batch(model: DeepEvalBaseLLM, batch_size: Optional[int] = None):
     if batch_size is None:
         return False
 
-    if not hasattr(model, 'batch_generate'):
+    if not hasattr(model, "batch_generate"):
         return False
 
     return True
