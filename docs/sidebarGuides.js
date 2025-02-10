3e1f95a5f9d0607c835240d04a80bce4e3f52652
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-02-10 14:31:59 -0800

    update docs

--- docs/sidebarGuides.js
+++ docs/sidebarGuides.js
@@ -1,37 +1,36 @@
 module.exports = {
-    guides: [
-        {
-            type: "category",
-            label: "Retrieval Augmented Generation",
-            items: [
-                "guides-rag-evaluation",
-                "guides-rag-triad",
-            ],
-            collapsed: false,
-        },
-        {
-            type: "category",
-            label: "Customizations",
-            items: [
-                "guides-using-custom-llms",
-                "guides-using-custom-embedding-models",
-                "guides-building-custom-metrics",
-            ],
-            collapsed: false,
-        },
-        {
-            type: "category",
-            label: "Others",
-            items: [
-                "guides-using-synthesizer",
-                "guides-optimizing-hyperparameters",
-                "guides-regression-testing-in-cicd",
-                "guides-llm-observability",
-                "guides-red-teaming",
-                "guides-answer-correctness-metric",
-            ],
-            collapsed: false,
-        },
-    ],
-  };
-  
+  guides: [
+    {
+      type: "category",
+      label: "Retrieval Augmented Generation",
+      items: [
+        "guides-rag-evaluation",
+        "guides-rag-triad",
+        "guides-using-synthesizer",
+      ],
+      collapsed: false,
+    },
+    {
+      type: "category",
+      label: "Customizations",
+      items: [
+        "guides-using-custom-llms",
+        "guides-using-custom-embedding-models",
+        "guides-building-custom-metrics",
+      ],
+      collapsed: false,
+    },
+    {
+      type: "category",
+      label: "Others",
+      items: [
+        "guides-optimizing-hyperparameters",
+        "guides-regression-testing-in-cicd",
+        "guides-llm-observability",
+        "guides-red-teaming",
+        "guides-answer-correctness-metric",
+      ],
+      collapsed: false,
+    },
+  ],
+};
