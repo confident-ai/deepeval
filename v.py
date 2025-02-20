c1f62aca9a77b6e2c5efb9fa2832135406ba8cac
Jeffrey Ip <jeffreyip@confident-ai.com>
2025-02-20 00:18:11 -0800

    fix docs

--- v.py
+++
@@ -1,9 +0,0 @@
-from deepeval.guardrails import Guardrails
-from deepeval.guardrails import PrivacyGuard
-
-from deepeval.guardrails import CybersecurityGuard
-from deepeval.guardrails.cybersecurity_guard import CyberattackCategory
-
-
-guardrails = Guardrails(guards=[PrivacyGuard()])
-guard_result = guardrails.guard_input(input="Hey doesn't t his look nice?")
