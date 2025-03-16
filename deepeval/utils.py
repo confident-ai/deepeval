9396d2f5857a0ff84854ecd75ee5ccee7ede5196
Jeffrey Ip <jeffreyip@confident-ai.com>
2025-03-16 14:40:48 -0700

    remove asyncio loop message

--- deepeval/utils.py
+++ deepeval/utils.py
@@ -126,9 +126,6 @@ def get_or_create_event_loop() -> asyncio.AbstractEventLoop:
     try:
         loop = asyncio.get_event_loop()
         if loop.is_running():
-            print(
-                "Event loop is already running. Applying nest_asyncio patch to allow async execution..."
-            )
             nest_asyncio.apply()
 
         if loop.is_closed():
