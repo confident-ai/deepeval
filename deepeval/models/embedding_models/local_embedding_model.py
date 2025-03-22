fb695ac807b02b692161054e2dd9b822661a9b18
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-03-21 21:37:36 -0700

    .

--- deepeval/models/embedding_models/local_embedding_model.py
+++ deepeval/models/embedding_models/local_embedding_model.py
@@ -1,45 +1,52 @@
-from langchain_openai import OpenAIEmbeddings
+from openai import OpenAI
 from typing import List
 
 from deepeval.key_handler import KeyValues, KEY_FILE_HANDLER
 from deepeval.models import DeepEvalBaseEmbeddingModel
 
 class LocalEmbeddingModel(DeepEvalBaseEmbeddingModel):
-    def __init__(
-        self,
-        *args,
-        **kwargs,
-    ):
+    def __init__(self, *args, **kwargs):
         self.base_url = KEY_FILE_HANDLER.fetch_data(KeyValues.LOCAL_EMBEDDING_BASE_URL)
-        self.api_key = KEY_FILE_HANDLER.fetch_data(KeyValues.LOCAL_EMBEDDING_API_KEY)
         model_name = KEY_FILE_HANDLER.fetch_data(KeyValues.LOCAL_EMBEDDING_MODEL_NAME)
+        self.api_key = KEY_FILE_HANDLER.fetch_data(KeyValues.LOCAL_EMBEDDING_API_KEY)
         self.args = args
         self.kwargs = kwargs
         super().__init__(model_name)
 
     def load_model(self):
-        return OpenAIEmbeddings(
-            model=self.model_name,
-            openai_api_base=self.base_url,
-            openai_api_key=self.api_key,
-            **self.kwargs,
-        )
+        return OpenAI(base_url=self.base_url, api_key=self.api_key)
 
     def embed_text(self, text: str) -> List[float]:
         embedding_model = self.load_model()
-        return embedding_model.embed_query(text)
+        response = embedding_model.embeddings.create(
+            model=self.model_name,
+            input=[text],
+        )
+        return response.data[0].embedding
 
     def embed_texts(self, texts: List[str]) -> List[List[float]]:
         embedding_model = self.load_model()
-        return embedding_model.embed_documents(texts)
+        response = embedding_model.embeddings.create(
+            model=self.model_name,
+            input=texts,
+        )
+        return [data.embedding for data in response.data]
 
     async def a_embed_text(self, text: str) -> List[float]:
         embedding_model = self.load_model()
-        return await embedding_model.aembed_query(text)
+        response = embedding_model.embeddings.create(
+            model=self.model_name,
+            input=[text],
+        )
+        return response.data[0].embedding
 
     async def a_embed_texts(self, texts: List[str]) -> List[List[float]]:
         embedding_model = self.load_model()
-        return await embedding_model.aembed_documents(texts)
+        response = embedding_model.embeddings.create(
+            model=self.model_name,
+            input=texts,
+        )
+        return [data.embedding for data in response.data]
 
     def get_model_name(self):
         return self.model_name
