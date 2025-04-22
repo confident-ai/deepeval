55235a807619cd2de8fe3ac1892592b786e90bbb
HadeelAdal <hadeel_adal@hotmail.com>
2025-04-22 11:13:23 +0300

    fixes

--- deepeval/models/llms/bedrock_model.py
+++ deepeval/models/llms/bedrock_model.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 valid_bedrock_models = [
     "eu.anthropic.claude-3-7-sonnet-20250219-v1:0",
     "eu.anthropic.claude-3-5-sonnet-20240620-v1:0",
-    "eu.anthropic.claude-3-haiku-20240307-v1:0"
+    "eu.anthropic.claude-3-haiku-20240307-v1:0",
     "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
     "us.anthropic.claude-3-5-haiku-20241022-v1:0",
     "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
