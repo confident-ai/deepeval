1a45bd2966d59c8acc9795e5734b4ba2830943c9
Jeffrey Ip <jeffreyip@confident-ai.com>
2025-05-09 02:37:02 +0800

    Prompt caching

--- bbbb.py
+++ bbbb.py
@@ -1,25 +1,6 @@
-from deepeval.dataset import Golden
-from deepeval.tracing import observe, update_current_span_test_case
-from deepeval.metrics import AnswerRelevancyMetric
-from deepeval.evaluate import evaluate
-from deepeval.test_case import LLMTestCase
+from deepeval.prompt import Prompt
 
-metric = AnswerRelevancyMetric()
-
-
-@observe(metrics=[metric])
-def inner_component():
-    # Component can be anything from an LLM call, retrieval, agent, tool use, etc.
-    update_current_span_test_case(
-        LLMTestCase(input="Hi!", actual_output="Hi!", expected_output="Hi!")
-    )
-    return
-
-
-@observe
-def llm_app(input: str):
-    inner_component()
-    return
-
-
-evaluate(observed_callback=llm_app, goldens=[Golden(input="Hi!")])
+prompt = Prompt(alias="First Prompt")
+prompt.pull(version="00.00.03")
+prompt_to_llm = prompt.interpolate(asdf="...")
+print(prompt_to_llm)
--- deepeval/_version.py
+++ deepeval/_version.py
@@ -1,1 +1,1 @@
-__version__: str = "2.8.6"
+__version__: str = "2.8.7"
