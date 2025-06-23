d5bbe4faf41ba69e486f407680020865f99a6264
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-06-23 21:00:02 +0700

    best test case model

--- deepeval/metrics/g_eval/schema.py
+++ deepeval/metrics/g_eval/schema.py
@@ -6,6 +6,9 @@ class ReasonScore(BaseModel):
     reason: str
     score: float
 
+class BestTestCase(BaseModel):
+    best_test_case_index: int
+    reason: str
 
 class Steps(BaseModel):
     steps: List[str]
