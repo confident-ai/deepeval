922a47ee166a6e2bb57aea519f0093a799534603
Mayank Solanki <bizmiku@gmail.com>
2025-06-26 18:43:37 +0700

    seperate traces

---
+++ deepeval/integrations/crewai/__init__.py
@@ -0,0 +1,17 @@
+from .patchers import CrewAILogger
+
+try:
+    from crewai import LLM
+    crewai_installed = True
+except:
+    crewai_installed = False
+
+def is_crewai_installed():
+    if not crewai_installed:
+        raise ImportError(
+            "CrewAI is not installed. Please install it with `pip install crewai`."
+        )
+crewai_logger = CrewAILogger()
+
+def instrument():
+    crewai_logger.patch_crewai_LLM("call")
