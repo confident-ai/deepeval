366eda5539ea6c44080f028b3975fbcd3ad0240a
Mayank Solanki <bizmiku@gmail.com>
2025-06-27 17:46:50 +0700

    accumulation

--- deepeval/integrations/crewai/__init__.py
+++ deepeval/integrations/crewai/__init__.py
@@ -1,1 +1,9 @@
-from .patchers import CrewAILogger
+from typing import Optional
+from .handler import CrewAIEventsListener
+import deepeval
+
+def instrumentator(api_key: Optional[str] = None):
+    if api_key:
+        deepeval.login_with_confident_api_key(api_key)
+    
+    CrewAIEventsListener()
