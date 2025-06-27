19bb1278e7644e13187c3b3ae4cb0053ee4a5e96
Mayank Solanki <bizmiku@gmail.com>
2025-06-27 17:46:50 +0700

    accumulation

---
+++ deepeval/integrations/crewai/handler.py
@@ -0,0 +1,112 @@
+from typing import Optional, Callable
+import functools
+
+try:
+    from crewai import LLM
+    from crewai.utilities.events import (
+        CrewKickoffStartedEvent,
+        CrewKickoffCompletedEvent,
+        ToolUsageEvent,
+        ToolUsageFinishedEvent,
+        ToolUsageErrorEvent,
+        ToolUsageStartedEvent,
+        ToolExecutionErrorEvent,
+        ToolSelectionErrorEvent,
+        ToolUsageEvent,
+        ToolValidateInputErrorEvent,
+    )
+    from crewai.utilities.events.base_event_listener import BaseEventListener
+    crewai_installed = True
+except:
+    crewai_installed = False
+
+from deepeval.tracing import trace_manager
+from deepeval.tracing.types import LlmSpan, LlmAttributes, TraceSpanStatus
+from uuid import uuid4
+from time import perf_counter
+
+def is_crewai_installed():
+    if not crewai_installed:
+        raise ImportError(
+            "CrewAI is not installed. Please install it with `pip install crewai`."
+        )
+
+class CrewAIEventsListener(BaseEventListener):
+    active_trace_id: Optional[str] = None
+    
+    def __init__(self):
+        is_crewai_installed()
+        super().__init__()
+
+    def setup_listeners(self, crewai_event_bus):
+        # patch to trace the llm call
+        self.patch_crewai_LLM("call")
+        
+        @crewai_event_bus.on(CrewKickoffStartedEvent)
+        def on_crew_started(source, event):
+            if self.active_trace_id is None:
+                self.active_trace_id = trace_manager.start_new_trace().uuid
+
+        @crewai_event_bus.on(CrewKickoffCompletedEvent)
+        def on_crew_completed(source, event):
+            if self.active_trace_id is not None:
+                trace_manager.end_trace(self.active_trace_id)
+                self.active_trace_id = None
+
+        @crewai_event_bus.on(ToolUsageEvent)
+        def on_tool_usage(source, event):
+            print("-----Tool usage")
+            print(">>>event", event)
+            print(">>>source", source)
+
+        @crewai_event_bus.on(ToolUsageStartedEvent)
+        def on_tool_usage_started(source, event):
+            print("-----Tool usage started")
+            print(">>>event", event)
+            print(">>>source", source)
+            
+        @crewai_event_bus.on(ToolUsageFinishedEvent)
+        def on_tool_usage_finished(source, event):
+            print("-----Tool usage finished")
+            print(">>>event", event)
+            print(">>>source", source)
+
+    def patch_crewai_LLM(self, method_to_patch: str):    
+        original_methods = {}
+
+        method = getattr(LLM, method_to_patch)
+        if callable(method) and not isinstance(method, type):
+            original_methods[method_to_patch] = method
+
+            @functools.wraps(method)
+            def wrapped_method(*args, original_method=method, **kwargs):                
+                if self.active_trace_id is None:
+                    self.active_trace_id = trace_manager.start_new_trace().uuid
+                
+                llm_span = LlmSpan(
+                    uuid=str(uuid4()),
+                    status=TraceSpanStatus.IN_PROGRESS,
+                    children=[],
+                    trace_uuid=self.active_trace_id,
+                    parent_uuid=None,
+                    start_time=perf_counter(),
+                    name="crewai_llm_span_" + str(uuid4()),
+                    # TODO: why model is coming unknown?
+                    model="unknown",
+                    attributes=LlmAttributes(input=args[1], output=""),
+                )
+                trace_manager.add_span(llm_span)
+                trace_manager.add_span_to_trace(llm_span)
+                
+                response = original_method(*args, **kwargs)
+                
+                llm_span.end_time = perf_counter()
+                llm_span.status = TraceSpanStatus.SUCCESS
+                llm_span.set_attributes(
+                    LlmAttributes(input=llm_span.attributes.input, output=response)
+                )
+                trace_manager.remove_span(llm_span.uuid)
+                
+                return response
+            
+            setattr(LLM, method_to_patch, wrapped_method)
