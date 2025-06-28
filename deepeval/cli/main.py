133d3d35f2df0ec5226c13c2a27d2384aef19a9e
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-06-28 13:05:06 +0700

    .

--- deepeval/cli/main.py
+++ deepeval/cli/main.py
@@ -123,7 +123,7 @@ def login(
                 "\n🎉🥳 Congratulations! You've successfully logged in! :raising_hands: "
             )
             print(
-                "You're now using DeepEval with [rgb(106,0,255)]Confident AI[/rgb(106,0,255)]. Follow our quickstart tutorial here: [bold][link=https://documentation.confident-ai.com/docs/getting-started/installation]https://documentation.confident-ai.com/docs/getting-started/installation[/link][/bold]"
+                "You're now using DeepEval with [rgb(106,0,255)]Confident AI[/rgb(106,0,255)]. Follow our quickstart tutorial here: [bold][link=https://documentation.confident-ai.com/docs/getting-started/setup]https://documentation.confident-ai.com/docs/getting-started/setup[/link][/bold]"
             )
         except:
             span.set_attribute("completed", False)
