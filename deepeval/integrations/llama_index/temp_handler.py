c7fd66b718376efc6127df88102f0bf500e1850d
Mayank Solanki <bizmiku@gmail.com>
2025-07-02 19:50:46 +0530

    .

--- deepeval/integrations/llama_index/temp_handler.py
+++ deepeval/integrations/llama_index/temp_handler.py
@@ -1,14 +1,16 @@
 from typing import Any, Dict, Optional
 import inspect
+from time import perf_counter
 
 import deepeval
 from deepeval.telemetry import capture_tracing_integration
 from deepeval.tracing import trace_manager
+from deepeval.tracing.types import BaseSpan, TraceSpanStatus
 try:
     from llama_index.core.instrumentation.events.base import BaseEvent
     from llama_index.core.instrumentation.event_handlers.base import BaseEventHandler
     from llama_index.core.instrumentation.span_handlers.base import BaseSpanHandler
-    from llama_index.core.instrumentation.span.base import BaseSpan
+    from llama_index.core.instrumentation.span.base import BaseSpan as LlamaIndexBaseSpan
     import llama_index.core.instrumentation as instrument
     llama_index_installed = True
 except:
@@ -28,7 +30,7 @@ class LLamaIndexHandler(BaseEventHandler, BaseSpanHandler):
         super().__init__()
 
     def handle(self, event: BaseEvent, **kwargs) -> Any:
-        print("----handle----")
+        pass
     
     def new_span(
         self,
@@ -38,8 +40,25 @@ class LLamaIndexHandler(BaseEventHandler, BaseSpanHandler):
         parent_span_id: Optional[str] = None,
         tags: Optional[Dict[str, Any]] = None,
         **kwargs: Any,
-    ) -> Optional[BaseSpan]:
-        print("----new_span----")
+    ) -> Optional[LlamaIndexBaseSpan]:
+        if parent_span_id is None:
+            self.active_trace_uuid = trace_manager.start_new_trace().uuid
+
+        base_span = BaseSpan(
+            uuid=id_,
+            status=TraceSpanStatus.IN_PROGRESS,
+            children=[],
+            trace_uuid=self.active_trace_uuid,
+            parent_uuid=parent_span_id,
+            start_time=perf_counter(),
+            name="llama-index", # add the name of the class
+            input=None, # add the input 
+        )
+
+        trace_manager.add_span(base_span)
+        trace_manager.add_span_to_trace(base_span)
+        
+        return base_span
 
     def prepare_to_exit_span(
         self,
@@ -48,8 +67,21 @@ class LLamaIndexHandler(BaseEventHandler, BaseSpanHandler):
         instance: Optional[Any] = None,
         result: Optional[Any] = None,
         **kwargs: Any,
-    ) -> Optional[BaseSpan]:
-        print("----prepare_to_exit_span----")
+    ) -> Optional[LlamaIndexBaseSpan]:
+        base_span = trace_manager.get_span_by_uuid(id_)
+        if base_span is None:
+            return None
+        
+        base_span.end_time = perf_counter()
+        base_span.status = TraceSpanStatus.SUCCESS
+        base_span.output = None # add the output
+        trace_manager.remove_span(base_span.uuid)
+
+        if base_span.parent_uuid is None:
+            trace_manager.end_trace(base_span.trace_uuid)
+            self.active_trace_uuid = None
+
+        return base_span
     
     def prepare_to_drop_span(
         self,
@@ -58,8 +90,19 @@ class LLamaIndexHandler(BaseEventHandler, BaseSpanHandler):
         instance: Optional[Any] = None,
         err: Optional[BaseException] = None,
         **kwargs: Any,
-    ) -> Optional[BaseSpan]:
-        pass
+    ) -> Optional[LlamaIndexBaseSpan]:
+        base_span = trace_manager.get_span_by_uuid(id_)
+        if base_span is None:
+            return None
+        
+        base_span.end_time = perf_counter()
+        base_span.status = TraceSpanStatus.SUCCESS # find a way to add error and handle the span without the parent id
+
+        if base_span.parent_uuid is None:
+            trace_manager.end_trace(base_span.trace_uuid)
+            self.active_trace_uuid = None
+
+        return base_span
 
 
 def instrumentator(api_key: Optional[str] = None):
