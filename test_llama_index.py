b445fbea1038e6756192f97c2313786b566ce369
Mayank Solanki <bizmiku@gmail.com>
2025-07-04 13:43:23 +0530

    .

--- test_llama_index.py
+++ test_llama_index.py
@@ -1,18 +1,16 @@
 import os
 import asyncio
 import time
-
 from llama_index.core.agent.workflow import FunctionAgent
 from llama_index.llms.openai import OpenAI
 from deepeval.integrations.llama_index import instrumentator
 
-os.environ["OPENAI_API_KEY"] = "<YOUR_OPENAI_API_KEY>"
-instrumentator(api_key="<YOUR_DEEP_EVAL_API_KEY>")
+os.environ["OPENAI_API_KEY"] = "<your_openai_api_key>"
+instrumentator(api_key="<your_deepeval_api_key>")
 
 def multiply(a: float, b: float) -> float:
     """Useful for multiplying two numbers."""
     return a * b
-
 
 agent = FunctionAgent(
     tools=[multiply],
@@ -21,12 +19,10 @@ agent = FunctionAgent(
     and search through documents to answer questions.""",
 )
 
-
 async def main():
     response = await agent.run("What's 7 * 8?")
     print(response)
 
-
 if __name__ == "__main__":
     asyncio.run(main())
-    time.sleep(12)
+    time.sleep(8)
