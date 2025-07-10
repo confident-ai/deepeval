16fb3261c70a403a72e5eccca02c1c5b2db2c88b
A-Vamshi <vamshi.edustudy@icloud.com>
2025-07-10 15:05:54 +0530

    Summarization agent tutorial introduction completed

--- docs/src/components/index.js
+++ docs/src/components/index.js
@@ -2,4 +2,4 @@ export { default as VideoDisplayer } from './VideoDisplayer';
 export { default as Equation } from './Equation';
 export { default as Envelope } from './Envelope';
 export { default as FeatureComparisonTable } from './FeatureComparisonTable';
-export { default as TutorialCardSection } from "./TutorialCardSection";
+export { default as LinkCards } from "./LinkCards";
