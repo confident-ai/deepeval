788e9afe34ebde41e4f3a88671f1aac3437d5181
Jeffrey Ip <jeffreyip@confident-ai.com>
2025-07-11 19:22:31 +0800

    .

--- deepeval/tracing/tracing.py
+++ deepeval/tracing/tracing.py
@@ -360,6 +360,8 @@ class TraceManager:
                         by_alias=True,
                         exclude_none=True,
                     )
+                    print(body)
+                    return
                 except AttributeError:
                     # Pydantic version below 2.0
                     body = trace_api.dict(by_alias=True, exclude_none=True)
