14b42b8c1426e0d1386514bbd6d3eabaffc22b85
A-Vamshi <vamshi.edustudy@icloud.com>
2025-07-16 23:31:51 +0530

    lint

--- deepeval/tracing/tracing.py
+++ deepeval/tracing/tracing.py
@@ -891,13 +891,23 @@ class Observer:
             )
         elif self.span_type == SpanType.LLM.value:
             model = self.observe_kwargs.get("model", None)
-            cost_per_input_token = self.observe_kwargs.get("cost_per_input_token", None)
-            cost_per_output_token = self.observe_kwargs.get("cost_per_output_token", None)
+            cost_per_input_token = self.observe_kwargs.get(
+                "cost_per_input_token", None
+            )
+            cost_per_output_token = self.observe_kwargs.get(
+                "cost_per_output_token", None
+            )
             if model is None and not trace_manager.openai_client:
                 raise ValueError(
                     "Either provide a model in observe or configure an openai_client in trace_manager. For more information on openai_client, see https://documentation.confident-ai.com/docs/llm-tracing/integrations/openai"
                 )
-            return LlmSpan(**span_kwargs, attributes=None, model=model, cost_per_input_token=cost_per_input_token, cost_per_output_token=cost_per_output_token)
+            return LlmSpan(
+                **span_kwargs,
+                attributes=None,
+                model=model,
+                cost_per_input_token=cost_per_input_token,
+                cost_per_output_token=cost_per_output_token,
+            )
         elif self.span_type == SpanType.RETRIEVER.value:
             embedder = self.observe_kwargs.get("embedder", None)
             if embedder is None:
