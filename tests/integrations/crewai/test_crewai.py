fce98131244fbcd5de7c9bfe5c6683079315ad2f
Mayank Solanki <bizmiku@gmail.com>
2025-07-21 14:20:39 +0530

    .

---
+++ tests/integrations/crewai/test_crewai.py
@@ -0,0 +1,33 @@
+from crewai import Task, Crew
+from deepeval.integrations.crewai.agent import Agent
+import os
+from deepeval.integrations.crewai import instrumentator
+import time
+ 
+os.environ["OPENAI_API_KEY"] = "<your-api-key>"
+instrumentator(api_key="<your-api-key>")
+# Define your agents with roles and goals
+coder = Agent(
+    role='Consultant',
+    goal='Write clear, concise explanation.',
+    backstory='An expert consultant with a keen eye for software trends.',
+    metric_collection="test_collection_1"
+)
+
+# Create tasks for your agents
+task1 = Task(
+    description="Explain the latest trends in AI.",
+    expected_output="A clear and concise explanation.",
+    agent=coder
+)
+ 
+# Instantiate your crew
+crew = Crew(
+    agents=[coder],
+    tasks=[task1],
+)
+ 
+# Kickoff your crew
+result = crew.kickoff()
+ 
+time.sleep(7) # Wait for traces to be posted to observatory
