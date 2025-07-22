9b1a8869ccf58d958e09beb307f050d58bb8c56d
A-Vamshi <vamshi.edustudy@icloud.com>
2025-07-22 19:51:19 +0530

    Added TechStackCards

---
+++ docs/src/components/TechStackCards/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import styles from './TechStackCards.module.css';
+
+const TechStackCards = ({ techStack }) => {
+  return (
+    <div className={styles.section}>
+      <div className={styles.list}>
+        {techStack.map((tech) => (
+          <TechStackCard key={tech.name} {...tech} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const TechStackCard = ({ name, logo, website }) => {
+  return (
+    <Link to={website} className={styles.card} target="_blank" rel="noopener noreferrer">
+      <div className={styles.cardContent}>
+        <img src={logo} alt={`${name} logo`} className={styles.logo} />
+        <h3 className={styles.cardTitle}>{name}</h3>
+      </div>
+    </Link>
+  );
+};
+
+export default TechStackCards;
