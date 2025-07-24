e0e92056388e167000da92e1861e431392b70f60
Mayank Solanki <bizmiku@gmail.com>
2025-07-24 17:45:33 +0530

    .

--- tests/integrations/pydaticai/test_pydaticai.py
+++ tests/integrations/pydaticai/test_pydaticai.py
@@ -1,6 +1,6 @@
 import os
 import time
-from pydantic_ai.agent import Agent
+from deepeval.integrations.pydantic_ai.agent import PydanticAIAgent as Agent
 from deepeval.integrations.pydantic_ai import setup_instrumentation
 
 setup_instrumentation(api_key="<your-api-key>")
@@ -12,10 +12,11 @@ Agent.instrument_all()
 agent = Agent(  
     'openai:gpt-4o-mini',
     system_prompt='Be concise, reply with one sentence.',  
+    metric_collection="test_collection_1"
 )
 
 result = agent.run_sync('Where does "hello world" come from?')  
 print(result.output)
 
-time.sleep(5)
+time.sleep(10)
 
