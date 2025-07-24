2ada09abf2e468d0317fb49331318d4a4ed25319
Mayank Solanki <bizmiku@gmail.com>
2025-07-24 13:00:00 +0530

    .

---
+++ deepeval/tracing/otel/exporter_v1.py
@@ -0,0 +1,61 @@
+from time import perf_counter
+from datetime import datetime, timezone
+import json
+import typing
+from typing import List, Optional
+from opentelemetry.sdk.trace.export import SpanExporter, SpanExportResult, ReadableSpan
+from deepeval.telemetry import capture_tracing_integration
+from deepeval.tracing import trace_manager
+from deepeval.tracing.types import BaseSpan, TraceSpanStatus
+from deepeval.tracing.otel.utils import to_hex_string
+from deepeval.tracing.tracing import to_zod_compatible_iso
+
+class ConfidentSpanExporterV1(SpanExporter):
+    active_trace_id: Optional[str] = None
+
+    def __init__(self):
+        capture_tracing_integration("deepeval.tracing.otel.exporter_v1")
+        super().__init__()
+
+    def check_active_trace_id(self):
+        if self.active_trace_id is None:
+            self.active_trace_id = trace_manager.start_new_trace().uuid
+    
+    def add_span_to_trace(self, span: BaseSpan):
+        trace_manager.add_span(span)
+        trace_manager.add_span_to_trace(span)
+    
+    def end_trace(self):
+        trace_manager.end_trace(self.active_trace_id)
+        self.active_trace_id = None
+    
+
+    def export(self, spans: typing.Sequence[ReadableSpan]) -> SpanExportResult:
+        self.check_active_trace_id()
+        spans_list: List[BaseSpan] = []
+        for span in spans:
+            base_span = BaseSpan(
+                uuid=to_hex_string(span.context.span_id, 16),
+                status=TraceSpanStatus.SUCCESS,
+                children=[],
+                trace_uuid=self.active_trace_id,
+                start_time=span.start_time/1e9,
+                end_time=span.end_time/1e9,
+                parent_uuid=to_hex_string(span.parent.span_id, 16) if span.parent else None,
+                name=span.name,
+                metadata=json.loads(span.to_json())
+            )
+            spans_list.append(base_span)
+        
+        for base_span in reversed(spans_list):
+            self.add_span_to_trace(base_span)
+        self.end_trace()
+
+        return SpanExportResult.SUCCESS
+    
+    def shutdown(self):
+        pass
+
+    def force_flush(self, timeout_millis: int = 30000) -> bool:
+        return True
+    
