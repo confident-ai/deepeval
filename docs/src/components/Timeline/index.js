e9dcf7a4e768cf46f07a3a73c44fa46e434b0c2c
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-07-25 19:33:07 +0700

    fix code blocks

--- docs/src/components/Timeline/index.js
+++ docs/src/components/Timeline/index.js
@@ -1,25 +1,21 @@
 import React from "react";
+import MDXContent from "@theme/MDXContent";
 import styles from "./index.module.css";
 
 export function Timeline({ children }) {
-  const childrenWithNumbers = React.Children.map(children, (child, index) => {
-    return React.cloneElement(child, {
-      number: index + 1,
-    });
-  });
-  return <div className={styles.timeline}>{childrenWithNumbers}</div>;
+  return <div className={styles.timeline}>{children}</div>;
 }
 
-export function TimelineItem({ title, children, number }) {
+export function TimelineItem({ title, children }) {
   return (
     <div className={styles.timelineItem}>
       <div className={styles.timelineStep}>
-        <div className={styles.timelineNumber}>{number}</div>
+        <div className={styles.timelineNumber}></div>
         <div className={styles.timelineLine}></div>
       </div>
       <div className={styles.timelineContent}>
         <h3>{title}</h3>
-        {children}
+        <MDXContent>{children}</MDXContent>
       </div>
     </div>
   );
