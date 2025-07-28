9b699190c0f943b71629d3b46af0c52e78508ec7
Mayank Solanki <bizmiku@gmail.com>
2025-07-28 10:39:33 +0530

    .

---
+++ tests/integrations/llama_index/test_react_agent.py
@@ -0,0 +1,28 @@
+import os
+import llama_index.core.instrumentation as instrument
+import asyncio
+import time
+
+os.environ["OPENAI_API_KEY"] = "<OPENAI_API_KEY>"
+
+from deepeval.integrations.llama_index import ReActAgent
+from deepeval.integrations.llama_index import instrument_llama_index
+from llama_index.llms.openai import OpenAI
+
+import deepeval
+
+deepeval.login_with_confident_api_key("<CONFIDENT_API_KEY>")
+instrument_llama_index(instrument.get_dispatcher())
+
+agent = ReActAgent(
+    llm=OpenAI(model="gpt-4o-mini"),
+    metric_collection="test_collection_1",
+)
+
+async def main():
+    response = await agent.run("What is the capital of France?")
+    print(response)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    time.sleep(7)
