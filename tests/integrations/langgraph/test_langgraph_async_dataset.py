91186344232b83e687a0d3aef019f77d02c7895b
Mayank Solanki <bizmiku@gmail.com>
2025-07-28 10:24:28 +0530

    .

---
+++ tests/integrations/langgraph/test_langgraph_async_dataset.py
@@ -0,0 +1,53 @@
+import os
+import asyncio
+import time
+from deepeval.integrations.langchain.callback import CallbackHandler
+from deepeval.metrics import TaskCompletionMetric
+import deepeval
+from langgraph.prebuilt import create_react_agent
+from deepeval.evaluate import dataset, test_run
+from deepeval.dataset import Golden
+from deepeval.evaluate.configs import AsyncConfig
+
+os.environ["OPENAI_API_KEY"] = "<OPENAI_API_KEY>"
+deepeval.login_with_confident_api_key("<CONFIDENT_API_KEY>")
+
+task_completion = TaskCompletionMetric(
+    threshold=0.7, model="gpt-4o-mini", include_reason=True
+)
+
+
+def get_weather(city: str) -> str:
+    """Returns the weather in a city"""
+    return f"It's always sunny in {city}!"
+
+
+agent = create_react_agent(
+    model="openai:gpt-4o-mini",
+    tools=[get_weather],
+    prompt="You are a helpful assistant",
+)
+
+goldens = [
+    Golden(input="What is the weather in Bogotá, Colombia?"),
+    Golden(input="What is the weather in Paris, France?"),
+]
+
+
+def main():
+    for golden in dataset(goldens=goldens):
+        test_run.append(asyncio.create_task(agent.ainvoke(
+            input={"messages": [{"role": "user", "content": golden.input}]},
+            config={
+                "callbacks": [
+                    CallbackHandler(
+                        metrics=[task_completion],
+                        metric_collection="task_completion",
+                    )
+                ]
+            },
+        )))
+
+if __name__ == "__main__":
+    main()
+    time.sleep(7)
