bc4047a9ad2a65370c02ccf7485a3027c4f4caa9
Mayank Solanki <bizmiku@gmail.com>
2025-08-06 20:23:52 +0530

    .

--- deepeval/integrations/crewai/handler.py
+++ deepeval/integrations/crewai/handler.py
@@ -6,6 +6,8 @@ from deepeval.integrations.crewai.agent import (
 )
 from deepeval.telemetry import capture_tracing_integration
 
+from deepeval.tracing.types import AgentSpan, LlmAttributes, LlmSpan, ToolSpan
+
 try:
     from crewai.crew import Crew
     from crewai.llm import LLM
@@ -13,7 +15,12 @@ try:
     from crewai.utilities.events import AgentExecutionCompletedEvent
     from crewai.utilities.events.base_event_listener import BaseEventListener
     from crewai.task import Task
-
+    from crewai.agents.crew_agent_executor import CrewAgentExecutor
+    from crewai.tools.base_tool import BaseTool
+    from crewai.tools.structured_tool import CrewStructuredTool
+    from crewai.utilities.events import ToolUsageFinishedEvent, ToolUsageStartedEvent
+    from crewai.tools.tool_usage import ToolUsage
+    from crewai.utilities.events import LLMCallCompletedEvent
     crewai_installed = True
 except:
     crewai_installed = False
@@ -47,10 +54,16 @@ class CrewAIEventsListener(BaseEventListener):
             source, event: AgentExecutionCompletedEvent
         ):
             current_span = current_span_context.get()
+            
+            if isinstance(current_span, AgentSpan):
+                if isinstance(source, Agent):
+                    current_span.name = source.role
+                    current_span.available_tools = [tool.name for tool in source.tools]
+            
             if current_span:
                 # set llm test case
                 input = None
-                output = None
+                actual_output = None
                 expected_output = None
 
                 if isinstance(event.task, Task):
@@ -76,6 +89,29 @@ class CrewAIEventsListener(BaseEventListener):
                         trace_manager.integration_traces_to_evaluate.append(
                             current_trace_context.get()
                         )
+        
+        @crewai_event_bus.on(ToolUsageFinishedEvent)
+        def on_tool_usage_finished(source, event: ToolUsageFinishedEvent):
+            current_span = current_span_context.get()
+            current_span.input = event.tool_args
+            current_span.output = event.output
+            current_span.name = event.tool_name
+
+        @crewai_event_bus.on(LLMCallCompletedEvent)
+        def on_llm_call_finished(source, event: LLMCallCompletedEvent):
+            current_span = current_span_context.get()
+            
+            if isinstance(current_span, LlmSpan):
+                if isinstance(source, LLM):
+                    current_span.model = source.model
+                
+                current_span.set_attributes(
+                    LlmAttributes(
+                        input=event.messages,
+                        output=event.response,
+                    )
+                )
+            
 
 
 def instrument_crewai(api_key: Optional[str] = None):
@@ -84,6 +120,8 @@ def instrument_crewai(api_key: Optional[str] = None):
         deepeval.login(api_key)
 
     Crew.kickoff = observe(Crew.kickoff)
-    LLM.call = observe(LLM.call)
-    Agent.execute_task = observe(Agent.execute_task)
+    LLM.call = observe(LLM.call, type="llm", model="")
+    Agent.execute_task = observe(Agent.execute_task, type="agent")
+    CrewAgentExecutor.invoke = observe(CrewAgentExecutor.invoke)
+    ToolUsage.use = observe(ToolUsage.use, type="tool")
     CrewAIEventsListener()
--- tests/integrations/crewai/test_crewai_sync_tracing.py
+++ tests/integrations/crewai/test_crewai_sync_tracing.py
@@ -1,24 +1,35 @@
 import os
 import time
 from crewai import Task, Crew, Agent
+from crewai.tools import tool
 
 from deepeval.integrations.crewai import instrument_crewai
 
-os.environ["OPENAI_API_KEY"] = "<YOUR_OPENAI_API_KEY>"
+from dotenv import load_dotenv
 
-instrument_crewai(api_key="<YOUR_CONFIDENT_API_KEY>")
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+instrument_crewai(api_key=os.getenv("CONFIDENT_API_KEY"))
+
+@tool
+def get_weather(city: str):
+    """Get the weather"""
+    return f"The weather is of {city} is sunny"
 
 # Define your agents with roles and goals
 coder = Agent(
-    role="Consultant",
-    goal="Write clear, concise explanation.",
-    backstory="An expert consultant with a keen eye for software trends.",
+    role="Weather Agent",
+    goal="Get the weather",
+    backstory="An agent that gets the weather",
+    tools=[get_weather],
 )
 
 # Create tasks for your agents
 task1 = Task(
-    description="Explain the latest trends in AI.",
-    expected_output="A clear and concise explanation.",
+    description="Get the weather",
+    expected_output="The weather is of San Francisco",
     agent=coder,
 )
 
