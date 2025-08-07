29c21cff63ada417c0c1f5e1f6ba61562f9577eb
A-Vamshi <vamshi.edustudy@icloud.com>
2025-08-07 10:41:24 +0530

    types.py added

---
+++ deepeval/test_case/types.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, AnyUrl
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Literal
+
+
+class MCPToolCall(BaseModel):
+    name: str
+    args: Dict
+    result: object
+
+class MCPPromptCall(BaseModel):
+    name: str
+    result: object
+
+class MCPResourceCall(BaseModel):
+    uri: AnyUrl
+    result: object
+
+@dataclass
+class MCPMetaData:
+    server_name: str
+    transport: Optional[Literal["stdio", "sse", "streamable-http"]] = None
+    available_tools: Optional[List] = None
+    available_resources: Optional[List] = None
+    available_prompts: Optional[List] = None
