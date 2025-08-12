1d49786605f43202cdadcd0c2d517fe7e761cadd
Mayank Solanki <bizmiku@gmail.com>
2025-08-12 22:23:00 +0530

    .

--- tests/integrations/pydantic_ai/test_async_offline_metrics.py
+++ tests/integrations/pydantic_ai/test_async_offline_metrics.py
@@ -1,13 +1,15 @@
 import os
 import asyncio
 import time
+
 from dotenv import load_dotenv
+load_dotenv()
+
 from deepeval.integrations.pydantic_ai import instrument_pydantic_ai
 from deepeval.integrations.pydantic_ai import Agent
 from deepeval.metrics import AnswerRelevancyMetric
 from deepeval.dataset import Golden, EvaluationDataset
 
-load_dotenv()
 
 instrument_pydantic_ai(api_key=os.getenv("CONFIDENT_API_KEY"))
 
@@ -19,14 +21,6 @@ answer_relavancy_metric = AnswerRelevancyMetric()
 agent = Agent(
     "openai:gpt-4o-mini",
     system_prompt="Be concise, reply with one sentence.",
-    metric_collection="test_collection_1",
-    trace_attributes={
-        "name": "test_name",
-        "tags": ["test_tag_1", "test_tag_2"],
-        "metadata": {"test_metadata_key": "test_metadata_value"},
-        "thread_id": "test_thread_id",
-        "user_id": "test_user_id",
-    },
     metrics=[answer_relavancy_metric]
 )
 
