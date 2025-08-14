42634a0e39dd1c2805f41a6a290580ecf807625d
Mayank Solanki <bizmiku@gmail.com>
2025-08-14 12:31:20 +0530

    test llamaindex

---
+++ tests/test_docs/test_deepeval/test_ai_agent_evals/test_llamaindex/test_llamaindex_e2e_sync.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from llama_index.llms.openai import OpenAI
+import llama_index.core.instrumentation as instrument
+
+from deepeval.integrations.llama_index import instrument_llama_index, FunctionAgent
+
+instrument_llama_index(instrument.get_dispatcher())
+
+def multiply(a: float, b: float) -> float:
+    """Useful for multiplying two numbers."""
+    return a * b
+
+agent = FunctionAgent(
+    tools=[multiply],
+    llm=OpenAI(model="gpt-4o-mini"),
+    system_prompt="You are a helpful assistant that can perform calculations.",
+)
+
+async def llm_app(input: str):
+    return await agent.run(input)
+
+from deepeval.dataset import EvaluationDataset, Golden
+
+dataset = EvaluationDataset(goldens=[
+    Golden(input="What is 3 * 12?"),
+    Golden(input="What is 4 * 13?")
+])
+
+for golden in dataset.evals_iterator():
+    task = asyncio.run(llm_app(golden.input))
