c984a2867cce0978364d24c6d90ab06631374e3a
A-Vamshi <vamshi.edustudy@icloud.com>
2025-09-03 15:05:22 +0530

    utils added

---
+++ deepeval/metrics/conversational_dag/__init__.py
@@ -0,0 +1,8 @@
+from .nodes import (
+    ConversationalBaseNode,
+    ConversationalVerdictNode,
+    ConversationalTaskNode,
+    ConversationalBinaryJudgementNode,
+    ConversationalNonBinaryJudgementNode,
+)
+from .graph import DeepAcyclicGraph
---
+++ deepeval/metrics/conversational_dag/utils.py
@@ -0,0 +1,125 @@
+from typing import Set, Dict, Optional
+import inspect
+
+from deepeval.metrics.conversational_dag import (
+    ConversationalBaseNode,
+    ConversationalBinaryJudgementNode,
+    ConversationalNonBinaryJudgementNode,
+    ConversationalVerdictNode,
+    ConversationalTaskNode,
+    DeepAcyclicGraph,
+)
+from deepeval.test_case import TurnParams
+
+
+def is_valid_dag_from_roots(root_nodes: list[ConversationalBaseNode]) -> bool:
+    visited = set()
+    for root in root_nodes:
+        if not is_valid_dag(root, visited, set()):
+            return False
+    return True
+
+
+def is_valid_dag(
+    node: ConversationalBaseNode, visited=None, stack=None
+) -> bool:
+    if visited is None:
+        visited = set()
+    if stack is None:
+        stack = set()
+
+    if node in stack:
+        return False
+    if node in visited:
+        return True
+
+    visited.add(node)
+    stack.add(node)
+    if (
+        isinstance(node, ConversationalTaskNode)
+        or isinstance(node, ConversationalBinaryJudgementNode)
+        or isinstance(node, ConversationalNonBinaryJudgementNode)
+    ):
+        for child in node.children:
+            if not is_valid_dag(child, visited, stack):
+                return False
+
+    stack.remove(node)
+    return True
+
+
+def extract_required_params(
+    nodes: list[ConversationalBaseNode],
+    required_params: Optional[Set[TurnParams]] = None,
+) -> Set[TurnParams]:
+    if required_params is None:
+        required_params = set()
+
+    for node in nodes:
+        if (
+            isinstance(node, ConversationalTaskNode)
+            or isinstance(node, ConversationalBinaryJudgementNode)
+            or isinstance(node, ConversationalNonBinaryJudgementNode)
+        ):
+            if node.evaluation_params is not None:
+                required_params.update(node.evaluation_params)
+            extract_required_params(node.children, required_params)
+
+    return required_params
+
+
+def copy_graph(original_dag: DeepAcyclicGraph) -> DeepAcyclicGraph:
+    # This mapping avoids re-copying nodes that appear in multiple places.
+    visited: Dict[ConversationalBaseNode, ConversationalBaseNode] = {}
+
+    def copy_node(node: ConversationalBaseNode) -> ConversationalBaseNode:
+        if node in visited:
+            return visited[node]
+
+        node_class = type(node)
+        args = vars(node)
+        superclasses = node_class.__mro__
+        valid_params = []
+        for superclass in superclasses:
+            signature = inspect.signature(superclass.__init__)
+            superclass_params = signature.parameters.keys()
+            valid_params.extend(superclass_params)
+        valid_params = set(valid_params)
+        valid_args = {
+            key: args[key]
+            for key in valid_params
+            if key in args
+            and key
+            not in [
+                "children",
+                "child",
+                "_parents",
+                "_parent",
+                "_verdict",
+                "_indegree",
+                "_depth",
+            ]
+        }
+        if (
+            isinstance(node, ConversationalTaskNode)
+            or isinstance(node, ConversationalBinaryJudgementNode)
+            or isinstance(node, ConversationalNonBinaryJudgementNode)
+        ):
+            copied_node = node_class(
+                **valid_args,
+                children=[copy_node(child) for child in node.children]
+            )
+        else:
+            if isinstance(node, ConversationalVerdictNode) and node.child:
+                copied_node = node_class(
+                    **valid_args, child=copy_node(node.child)
+                )
+            else:
+                copied_node = node_class(**valid_args)
+
+        visited[node] = copied_node
+        return copied_node
+
+    # Copy all root nodes (the recursion handles the rest).
+    new_root_nodes = [copy_node(root) for root in original_dag.root_nodes]
+    return DeepAcyclicGraph(new_root_nodes)
