d19d36f28f4bf987998d784b8b786bcfcb816001
Mayank Solanki <bizmiku@gmail.com>
2025-09-10 08:09:59 +0530

    .

--- tests/test_integrations/test_langgraph/test_langgraph_online.py
+++ tests/test_integrations/test_langgraph/test_langgraph_online.py
@@ -1,11 +1,9 @@
 from langgraph.prebuilt import create_react_agent
-from deepeval.integrations.langchain.callback import CallbackHandler
-
+from deepeval.integrations.langchain import CallbackHandler
 
 def get_weather(city: str) -> str:
     """Returns the weather in a city"""
     return f"It's always sunny in {city}!"
-
 
 agent = create_react_agent(
     model="openai:gpt-4o-mini",
@@ -13,9 +11,14 @@ agent = create_react_agent(
     prompt="You are a helpful assistant",
 )
 
+# run for testing (not needed for docs)
 result = agent.invoke(
     input={
         "messages": [{"role": "user", "content": "what is the weather in sf"}]
     },
-    config={"callbacks": [CallbackHandler()]},
+    config={
+        "callbacks": [CallbackHandler(metric_collection="task_completion")]
+    },
 )
+
+print(result)
