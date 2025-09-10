9a0dc65edf2abfdf2a7e416d71252ffaea8f3875
Mayank Solanki <bizmiku@gmail.com>
2025-09-10 15:19:41 +0530

    .

--- tests/test_integrations/test_langgraph/test_langgraph.py
+++ tests/test_integrations/test_langgraph/test_langgraph.py
@@ -1,16 +1,25 @@
 import os
 import tempfile
-from deepeval.tracing.utils import run_in_mode, compare_trace_files
+import sys
+import time
+from deepeval.tracing.utils import compare_trace_files
 from langgraph_app import execute_agent
 
 def test_exec_agent_logs():
+        
         tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
         tmp_path = tmp.name
         tmp.close()
+        
         try:
-                run_in_mode("gen", execute_agent, file_path=tmp_path)
-                expected_path = os.path.join(os.path.dirname(__file__), "langgraph_app.json")
-                compare_trace_files(expected_path, tmp_path)
+            original_argv = list(sys.argv)
+            sys.argv = ["--mode=gen", f"--file-name={tmp_path}"]
+            execute_agent()
+            time.sleep(15)
+            sys.argv = original_argv
+            expected_path = os.path.join(os.path.dirname(__file__), "langgraph_app.json")
+            compare_trace_files(expected_path, tmp_path)
         finally:
-                if os.path.exists(tmp_path):
-                        os.remove(tmp_path)
+            if os.path.exists(tmp_path):
+                os.remove(tmp_path)
+                print(f"Removed temp file: {tmp_path}")
