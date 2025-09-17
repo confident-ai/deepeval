62fdd606054619e5969bb8db77b54ae2a79bd76c
Mayank Solanki <bizmiku@gmail.com>
2025-09-17 15:35:13 +0800

    .

--- tests/test_integrations/test_langgraph/langgraph_app.py
+++ tests/test_integrations/test_langgraph/langgraph_app.py
@@ -2,14 +2,19 @@ from langgraph.prebuilt import create_react_agent
 from deepeval.integrations.langchain import CallbackHandler, tool
 # from deepeval.tracing.tracing import observe
 from langchain_openai import ChatOpenAI
+from deepeval.prompt import Prompt
 
+prompt = Prompt(alias="asd")
+prompt.pull(version="00.00.01")
 
 @tool(metric_collection="test_collection_1")
 def get_weather(city: str) -> str:
     """Returns the weather in a city"""
     return f"It's always sunny in {city}!"
 
-llm = ChatOpenAI(model="gpt-4o-mini", metadata={"metric_collection": "test_collection_1"})
+llm = ChatOpenAI(model="gpt-4o-mini",
+ metadata={"metric_collection": "test_collection_1", "prompt": prompt})
+ 
 agent = create_react_agent(
     model=llm,
     tools=[get_weather],
@@ -31,3 +36,5 @@ def execute_agent():
             )],
         },
     )
+
+execute_agent()
