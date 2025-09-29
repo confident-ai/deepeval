3ed5ff2d3ea5e73bf092988c330fa57c514a023b
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-09-29 16:07:29 +0700

    .

--- a.py
+++ a.py
@@ -1,4 +1,33 @@
-from deepeval.prompt import Prompt
+from deepeval import evaluate
+from deepeval.metrics import TurnRelevancyMetric
+from deepeval.test_case import LLMTestCase, ConversationalTestCase, Turn
 
-prompt = Prompt(alias="Message Prompt")
-prompt.pull(refresh=0)
+evaluate(
+    test_cases=[
+        ConversationalTestCase(
+            scenario="You are a helpful assistant.",
+            user_description="What is the weather in San Francisco?",
+            context=["It is sunny and 70 degrees."],
+            tags=["weather"],
+            turns=[
+                Turn(
+                    role="user",
+                    content="What is the weather in San Francisco?",
+                ),
+            ],
+        ),
+        ConversationalTestCase(
+            scenario="You are a helpful assistant.",
+            user_description="What is the weather in San Francisco?",
+            context=["It is sunny and 70 degrees."],
+            tags=["something else"],
+            turns=[
+                Turn(
+                    role="user",
+                    content="What is the weather in San Francisco?",
+                ),
+            ],
+        ),
+    ],
+    metrics=[TurnRelevancyMetric()],
+)
