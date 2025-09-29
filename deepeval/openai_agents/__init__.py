a61afccbfd002cb99c73f21cf14841154e8875c7
Mayank Solanki <bizmiku@gmail.com>
2025-09-29 17:50:05 +0530

    .

--- deepeval/openai_agents/__init__.py
+++ deepeval/openai_agents/__init__.py
@@ -3,4 +3,4 @@ from deepeval.openai_agents.callback_handler import DeepEvalTracingProcessor
 # from deepeval.openai_agents.patch import function_tool
 # from deepeval.openai_agents.agent import DeepEvalAgent as Agent
 
-__all__ = ["DeepEvalTracingProcessor", "Runner", "function_tool", "Agent"]
+__all__ = ["DeepEvalTracingProcessor"]
