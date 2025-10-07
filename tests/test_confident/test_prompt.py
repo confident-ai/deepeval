0f2de4d81cb9fa6ef5805ea8bb819f0797e153e7
Jeffrey Ip <jeffreyip@confident-ai.com>
2025-10-07 15:32:40 +0800

    Added tests

--- tests/test_confident/test_prompt.py
+++ tests/test_confident/test_prompt.py
@@ -11,6 +11,7 @@ from deepeval.prompt.api import (
 class TestPromptText:
     ALIAS = "test_prompt_text"
     ALIAS_WITH_INTERPOLATION_TYPE = "test_prompt_text_interpolation_type"
+    LABEL = "STAGE"
 
     def test_push(self):
         prompt = Prompt(alias=self.ALIAS)
@@ -48,10 +49,47 @@ class TestPromptText:
         assert prompt._type == PromptType.TEXT
         assert prompt._interpolation_type == PromptInterpolationType.MUSTACHE
 
+    def test_pull_by_label(self):
+        """Test pulling text prompt by label"""
+        prompt = Prompt(alias=self.ALIAS)
+
+        # Pull by label
+        prompt.pull(label=self.LABEL)
+
+        assert prompt.label == self.LABEL
+        assert prompt.version is not None
+        assert prompt._text_template is not None
+        assert prompt._type == PromptType.TEXT
+        assert prompt._prompt_version_id is not None
+        assert prompt._interpolation_type is not None
+
+    def test_version_vs_label_pull(self):
+        """Test that version and label pulls work independently"""
+        # Pull by version (latest)
+        prompt_by_version = Prompt(alias=self.ALIAS)
+        prompt_by_version.pull()
+
+        # Pull by label
+        prompt_by_label = Prompt(alias=self.ALIAS)
+        prompt_by_label.pull(label=self.LABEL)
+
+        # Version pull should not have label
+        assert prompt_by_version.label is None
+        assert prompt_by_version.version is not None
+
+        # Label pull should have both
+        assert prompt_by_label.label == self.LABEL
+        assert prompt_by_label.version is not None
+
+        # Both should have valid content
+        assert prompt_by_version._text_template is not None
+        assert prompt_by_label._text_template is not None
+
 
 class TestPromptList:
     ALIAS = "test_prompt_list"
     ALIAS_WITH_INTERPOLATION_TYPE = "test_prompt_list_interpolation_type"
+    LABEL = "STAGE"
 
     def test_push(self):
         prompt = Prompt(alias=self.ALIAS)
@@ -96,3 +134,39 @@ class TestPromptList:
         assert prompt._prompt_version_id is not None
         assert prompt._type == PromptType.LIST
         assert prompt._interpolation_type == PromptInterpolationType.MUSTACHE
+
+    def test_pull_by_label(self):
+        """Test pulling list prompt by label"""
+        prompt = Prompt(alias=self.ALIAS)
+
+        # Pull by label
+        prompt.pull(label=self.LABEL)
+
+        assert prompt.label == self.LABEL
+        assert prompt.version is not None
+        assert prompt._messages_template is not None
+        assert prompt._type == PromptType.LIST
+        assert prompt._prompt_version_id is not None
+        assert prompt._interpolation_type is not None
+
+    def test_version_vs_label_pull(self):
+        """Test that version and label pulls work independently"""
+        # Pull by version (latest)
+        prompt_by_version = Prompt(alias=self.ALIAS)
+        prompt_by_version.pull()
+
+        # Pull by label
+        prompt_by_label = Prompt(alias=self.ALIAS)
+        prompt_by_label.pull(label=self.LABEL)
+
+        # Version pull should not have label
+        assert prompt_by_version.label is None
+        assert prompt_by_version.version is not None
+
+        # Label pull should have both
+        assert prompt_by_label.label == self.LABEL
+        assert prompt_by_label.version is not None
+
+        # Both should have valid content
+        assert prompt_by_version._messages_template is not None
+        assert prompt_by_label._messages_template is not None
