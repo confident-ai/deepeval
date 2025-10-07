e739268b5c165471424fb9f1f82ee23073a69a98
A-Vamshi <vamshi.edustudy@icloud.com>
2025-10-07 17:56:34 +0530

    .

--- deepeval/dataset/dataset.py
+++ deepeval/dataset/dataset.py
@@ -1274,7 +1274,9 @@ class EvaluationDataset:
                 else:
                     confident_link = test_run_id = None
                 return EvaluationResult(
-                    test_results=test_results, confident_link=confident_link, test_run_id=test_run_id
+                    test_results=test_results,
+                    confident_link=confident_link,
+                    test_run_id=test_run_id,
                 )
 
     def evaluate(self, task: Task):
