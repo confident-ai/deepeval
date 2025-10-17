04437ae0d291d4d621d96980ad69abb99771f90f
Mayank Solanki <bizmiku@gmail.com>
2025-10-17 18:36:23 +0530

    llama index test deprecated

--- tests/test_integrations/test_llamaindex/test_llama_index.py
+++ tests/test_integrations/test_llamaindex/test_llama_index.py
@@ -1,5 +1,7 @@
 import os
 import json
+
+import pytest
 from tests.test_integrations.utils import (
     assert_json_object_structure,
     load_trace_data,
@@ -35,6 +37,7 @@ _current_dir = os.path.dirname(os.path.abspath(__file__))
 json_path = os.path.join(_current_dir, "llama_index.json")
 
 
+@pytest.mark.skip(reason="LlamaIndex is deprecated")
 async def test_json_schema():
     """
     Test the json schema of the trace. Raises an exception if the schema is invalid.
