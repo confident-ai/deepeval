2f113b1864b4170bb438f4feb61a477406d18b41
Mayank Solanki <bizmiku@gmail.com>
2025-10-28 00:08:20 +0530

    llama index

--- tests/test_integrations/test_llamaindex/test_llama_index.py
+++ tests/test_integrations/test_llamaindex/test_llama_index.py
@@ -4,20 +4,18 @@ import json
 import pytest
 import asyncio
 from tests.test_integrations.utils import (
-    assert_json_object_structure,
-    load_trace_data,
+    assert_trace_json,
+    generate_trace_json,
 )
-from deepeval.tracing.trace_test_manager import trace_testing_manager
 from llama_index.llms.openai import OpenAI
 from llama_index.core.agent import FunctionAgent
 from deepeval.tracing import trace
 from deepeval.tracing.trace_context import LlmSpanContext, AgentSpanContext
 
-# from deepeval.integrations.llama_index import instrument_llama_index
+from deepeval.integrations.llama_index import instrument_llama_index
+
 # import llama_index.core.instrumentation as instrument
-
 # instrument_llama_index(instrument.get_dispatcher())
-
 
 def multiply(a: float, b: float) -> float:
     """Useful for multiplying two numbers."""
@@ -51,33 +49,7 @@ _current_dir = os.path.dirname(os.path.abspath(__file__))
 json_path = os.path.join(_current_dir, "llama_index.json")
 
 
-async def test_json_schema():
-    """
-    Test the json schema of the trace. Raises an exception if the schema is invalid.
-    """
-    try:
-        trace_testing_manager.test_name = json_path
-        await llm_app("What is 3 * 12?")
-        actual_dict = await trace_testing_manager.wait_for_test_dict()
-        expected_dict = load_trace_data(json_path)
-
-        assert assert_json_object_structure(expected_dict, actual_dict)
-    finally:
-        trace_testing_manager.test_name = None
-        trace_testing_manager.test_dict = None
-
-
-################################ Generate Actual JSON Dump Code #################################
-
-
-async def generate_actual_json_dump():
-    try:
-        trace_testing_manager.test_name = json_path
-        await llm_app("What is 3 * 12?")
-        actual_dict = await trace_testing_manager.wait_for_test_dict()
-
-        with open(json_path, "w") as f:
-            json.dump(actual_dict, f)
-    finally:
-        trace_testing_manager.test_name = None
-        trace_testing_manager.test_dict = None
+# @generate_trace_json(json_path)
+@assert_trace_json(json_path)
+async def test_json_schema():    
+    await llm_app("What is 3 * 12?")
