4cf5bb02105eab7b235f0715b91a7f0d3020a2b0
Kritin_Vongthongsri <kritinv@princeton.edu>
2025-11-17 23:53:02 +0700

    .

--- deepeval/integrations/pydantic_ai/instrumentator.py
+++ deepeval/integrations/pydantic_ai/instrumentator.py
@@ -2,18 +2,16 @@ import json
 import logging
 import os
 from time import perf_counter
-from typing import Literal, Optional, List
+from typing import Optional, List, Dict
 
 from deepeval.config.settings import get_settings
 from deepeval.confident.api import get_confident_api_key
 from deepeval.metrics.base_metric import BaseMetric
 from deepeval.prompt import Prompt
 from deepeval.tracing.context import current_trace_context
-from deepeval.tracing.types import Trace
+from deepeval.tracing.types import Trace, BaseSpan
 from deepeval.tracing.otel.utils import to_hex_string
 from deepeval.tracing.tracing import trace_manager
-from deepeval.tracing.otel.utils import normalize_pydantic_ai_messages
-from deepeval.tracing.otel.exporter import ConfidentSpanExporter
 
 
 logger = logging.getLogger(__name__)
@@ -63,6 +61,7 @@ from deepeval.tracing.types import Trace
 from deepeval.tracing.otel.utils import to_hex_string
 from deepeval.tracing.types import TraceSpanStatus, ToolCall
 from deepeval.tracing.perf_epoch_bridge import init_clock_bridge
+from deepeval.tracing.otel.exporter import ConfidentSpanExporter
 
 # OTLP_ENDPOINT = "http://127.0.0.1:4318/v1/traces"
 OTLP_ENDPOINT = "https://otel.confident-ai.com/v1/traces"
@@ -87,6 +86,9 @@ class ConfidentInstrumentationSettings(InstrumentationSettings):
         trace_metric_collection: Optional[str] = None,
         is_test_mode: Optional[bool] = False,
         agent_metrics: Optional[List[BaseMetric]] = None,
+        llm_metrics: Optional[List[BaseMetric]] = None,
+        tool_metrics: Optional[List[BaseMetric]] = None,
+        trace_metrics: Optional[List[BaseMetric]] = None,
     ):
         is_dependency_installed()
 
@@ -112,6 +114,9 @@ class ConfidentInstrumentationSettings(InstrumentationSettings):
         self.trace_metric_collection = trace_metric_collection
         self.is_test_mode = is_test_mode
         self.agent_metrics = agent_metrics
+        self.llm_metrics = llm_metrics
+        self.tool_metrics = tool_metrics
+        self.trace_metrics = trace_metrics
 
         if not api_key:
             api_key = get_confident_api_key()
@@ -138,12 +143,30 @@ class ConfidentInstrumentationSettings(InstrumentationSettings):
         super().__init__(tracer_provider=trace_provider)
 
 
+spans_by_trace_id: Dict[str, List[ReadableSpan]] = {}
+agent_span_ids_by_trace_id: Dict[str, List[int]] = {}
+
+
 class SpanInterceptor(SpanProcessor):
     def __init__(self, settings_instance: ConfidentInstrumentationSettings):
         # Keep a reference to the settings instance instead of copying values
         self.settings = settings_instance
 
     def on_start(self, span, parent_context):
+        # track agent spans to find the root
+        trace_id = span.context.trace_id
+        print(trace_id)
+        span_id = span.context.span_id
+        is_agent_span = (
+            span.attributes.get("agent_name") is not None
+            if span.attributes
+            else False
+        )
+        if is_agent_span:
+            if trace_id not in agent_span_ids_by_trace_id:
+                agent_span_ids_by_trace_id[trace_id] = []
+            agent_span_ids_by_trace_id[trace_id].append(span_id)
+
         # set trace uuid
         _current_trace_context = current_trace_context.get()
         if _current_trace_context and isinstance(_current_trace_context, Trace):
@@ -228,57 +251,72 @@ class SpanInterceptor(SpanProcessor):
                     str(tool_metric_collection),
                 )
 
-    def on_end(self, span):
-        if self.settings.is_test_mode:
-            if span.attributes.get("confident.span.type") == "agent":
+    def on_end(
+        self,
+        span: ReadableSpan,
+    ):
+        trace_id = span.context.trace_id
+        span_id = span.context.span_id
+        agent_span_ids = agent_span_ids_by_trace_id.get(trace_id)
+        if agent_span_ids is not None and span_id in agent_span_ids:
+            agent_span_ids.remove(span_id)
+        if not self.settings.is_test_mode:
+            return
 
-                def create_agent_span_for_evaluation(
-                    span: ReadableSpan,
-                ) -> AgentSpan:
+        # store spans by trace id
+        if trace_id not in spans_by_trace_id:
+            spans_by_trace_id[trace_id] = [span]
+        else:
+            spans_by_trace_id[trace_id].append(span)
 
-                    agent_span = (
-                        ConfidentSpanExporter.prepare_boilerplate_base_span(
-                            span
-                        )
-                    )
+        # detect root span
+        if agent_span_ids is not None and len(agent_span_ids) == 0:
+            print("Root span detected")
+            # create a trace for evaluation if trace for uuid doesn't exist
+            trace_uuid = to_hex_string(span.context.trace_id, 32)
+            trace = trace_manager.get_trace_by_uuid(trace_uuid)
+            if not trace:
+                trace = trace_manager.start_new_trace(trace_uuid=trace_uuid)
 
-                    # tools called
-                    normalized_messages = normalize_pydantic_ai_messages(span)
-                    tools_called = []
+            # add spans to trace in the correct order (parent before children)
+            exporter = ConfidentSpanExporter()
+            spans_in_trace: List[ReadableSpan] = spans_by_trace_id[trace_id]
+            spans_in_trace.sort(key=lambda s: s.start_time)
+            for otel_span in spans_in_trace:
+                deepeval_span_wrapper = (
+                    exporter._convert_readable_span_to_base_span(otel_span)
+                )
+                deepeval_span = deepeval_span_wrapper.base_span
+                self._set_metrics(
+                    deepeval_span,
+                    otel_span.attributes.get("confident.span.type"),
+                )
+                if span_id == otel_span.context.span_id:
+                    deepeval_span.parent_uuid = None
+                # set parent uuid for root span
+                trace_manager.add_span(deepeval_span)
+                trace_manager.add_span_to_trace(deepeval_span)
 
-                    for message in normalized_messages:
-                        for part in message.get("parts", []):
-                            if part.get("type") == "tool_call":
-                                name = part.get("name")
-                                try:
-                                    input_parameters = json.loads(
-                                        part.get("arguments")
-                                    )
-                                except Exception:
-                                    input_parameters = {}
+            # Finalize trace for evaluation
+            trace.status = TraceSpanStatus.SUCCESS
+            trace.end_time = perf_counter()
+            trace_manager.traces_to_evaluate.append(trace)
+            print(len(trace_manager.traces_to_evaluate))
+            print(trace_manager.traces_to_evaluate)
+            del spans_by_trace_id[trace_id]
+            del agent_span_ids_by_trace_id[trace_id]
 
-                                tools_called.append(
-                                    ToolCall(
-                                        name=name,
-                                        input_parameters=input_parameters,
-                                    )
-                                )
-
-                    # agent_span.tools_called = tools_called
-                    return agent_span
-
-                agent_span = create_agent_span_for_evaluation(span)
-                agent_span.metrics = self.settings.agent_metrics
-
-                # create a trace for evaluation
-                trace = trace_manager.get_trace_by_uuid(agent_span.trace_uuid)
-                if not trace:
-                    trace = trace_manager.start_new_trace(
-                        trace_uuid=agent_span.trace_uuid
-                    )
-
-                trace.root_spans.append(agent_span)
-                trace.status = TraceSpanStatus.SUCCESS
-                trace.end_time = perf_counter()
-                trace_manager.traces_to_evaluate.append(trace)
-                test_exporter.clear_span_json_list()
+    def _set_metrics(
+        self,
+        base_span: BaseSpan,
+        span_type: str,
+    ):
+        if span_type == "agent":
+            if self.settings.agent_metrics:
+                base_span.metrics = self.settings.agent_metrics
+        elif span_type == "llm":
+            if self.settings.llm_metrics:
+                base_span.metrics = self.settings.llm_metrics
+        elif span_type == "tool":
+            if self.settings.tool_metrics:
+                base_span.metrics = self.settings.tool_metrics
