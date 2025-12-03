e6200f6566bf2e67cbbc99fd7b996000e3b68374
A-Vamshi <vamshi.edustudy@icloud.com>
2025-12-03 19:09:35 +0530

    test_run updated

--- deepeval/test_run/api.py
+++ deepeval/test_run/api.py
@@ -18,19 +18,19 @@ class LLMApiTestCase(BaseModel):
     token_cost: Optional[float] = Field(None, alias="tokenCost")
     completion_time: Optional[float] = Field(None, alias="completionTime")
     tags: Optional[List[str]] = Field(None)
-    multimodal_input: Optional[List[Union[str, MLLMImage]]] = Field(
+    multimodal_input: Optional[str] = Field(
         None, alias="multimodalInput"
     )
-    multimodal_input_actual_output: Optional[List[Union[str, MLLMImage]]] = (
+    multimodal_input_actual_output: Optional[str] = (
         Field(None, alias="multimodalActualOutput")
     )
-    multimodal_expected_output: Optional[List[Union[str, MLLMImage]]] = Field(
+    multimodal_expected_output: Optional[str] = Field(
         None, alias="multimodalExpectedOutput"
     )
-    multimodal_retrieval_context: Optional[List[Union[str, MLLMImage]]] = Field(
+    multimodal_retrieval_context: Optional[List[str]] = Field(
         None, alias="multimodalRetrievalContext"
     )
-    multimodal_context: Optional[List[Union[str, MLLMImage]]] = Field(
+    multimodal_context: Optional[List[str]] = Field(
         None, alias="multimodalContext"
     )
 
