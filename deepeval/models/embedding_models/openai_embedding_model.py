b0fc3eb050247ccf62f5ae029c65534def644834
BloggerBust <trevor.wilson@bloggerbust.ca>
2025-12-03 20:09:59 -0700

    fix(models): pass effective model_name to OpenAIEmbeddingModel base ctor

--- deepeval/models/embedding_models/openai_embedding_model.py
+++ deepeval/models/embedding_models/openai_embedding_model.py
@@ -65,7 +65,7 @@ class OpenAIEmbeddingModel(DeepEvalBaseEmbeddingModel):
             )
         self.kwargs = normalized_kwargs
         self.generation_kwargs = generation_kwargs or {}
-        super().__init__(model_name)
+        super().__init__(self.model_name)
 
     @retry_openai
     def embed_text(self, text: str) -> List[float]:
