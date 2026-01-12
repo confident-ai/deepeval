a881cc67e9891b0c303d558082e0c9bbcb5f535a
BloggerBust <trevor.wilson@bloggerbust.ca>
2026-01-11 20:19:05 -0700

    chore(docs): Add changelog for 2024
    Update script default output dir to generate in new changelog location

--- .scripts/changelog/generate.py
+++ .scripts/changelog/generate.py
@@ -1417,7 +1417,7 @@ def main() -> int:
     )
 
     ap.add_argument(
-        "--output-dir", default="docs/docs/changelog", help="Docs changelog dir"
+        "--output-dir", default="docs/changelog", help="Docs changelog dir"
     )
     ap.add_argument(
         "--github",
