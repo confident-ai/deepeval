0d1a5f2a3a009525d8c8e367607c139e32ccc702
A-Vamshi <vamshi.edustudy@icloud.com>
2026-03-24 18:47:09 +0530

    .

--- docs/src/components/SchemaInjector/SchemaInjector.tsx
+++ docs/src/components/SchemaInjector/SchemaInjector.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Head from '@docusaurus/Head';
 
-export default function SchemaInjector({ schema }) {
+interface SchemaInjectorProps {
+  schema: Record<string, any> | null | undefined;
+}
+
+export default function SchemaInjector({ schema }: SchemaInjectorProps) {
   if (!schema || Object.keys(schema).length === 0) {
     return null;
   }
