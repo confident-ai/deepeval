1996a05226e1f96521e017347ce5f2803e96503d
A-Vamshi <vamshi.edustudy@icloud.com>
2026-05-03 20:00:44 +0530

    simba updated

---
+++ deepeval/optimizer/algorithms/simba/schema.py
@@ -0,0 +1,6 @@
+from typing import Union, List, Dict
+from pydantic import BaseModel
+
+class SIMBARewriteSchema(BaseModel):
+    discussion: str
+    revised_prompt: Union[str, List[Dict[str, str]]]
