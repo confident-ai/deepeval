ae3ad9b4272df62c4d6b75c2899b4f7c93131a0c
A-Vamshi <vamshi.edustudy@icloud.com>
2026-05-11 22:42:02 +0800

    .

--- model_callback.py
+++ model_callback.py
@@ -2,4 +2,8 @@ from deepeval.test_case import Turn
 from typing import List
 
 def model_callback(input: str, turns: List[Turn] = None) -> Turn:
+    if "France" in input:
+        return Turn(role="assistant", content="Paris")
+    elif "Germany" in input:
+        return Turn(role="assistant", content="Berlin")
     return Turn(role="assistant", content="Don't care bro, get lost!")
