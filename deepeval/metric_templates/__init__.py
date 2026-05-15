a40811fdd0ccb9be385158e226100a76a578f970
A-Vamshi <vamshi.edustudy@icloud.com>
2026-05-15 18:02:38 +0800

    fix init

--- deepeval/metric_templates/__init__.py
+++ deepeval/metric_templates/__init__.py
@@ -14,11 +14,8 @@ __all__ = [
     "MetricTemplateNotFoundError",
     "clear_metric_template_cache",
     "get_bundle_only_template",
-    "get_fragment",
     "get_raw_template",
     "iter_bundle_template_methods",
     "list_methods",
-    "load_bundle",
-    "merge_hidden_template_file",
     "resolve_template",
 ]
