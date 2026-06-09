a4df1e41b8ce6c54c901f11df3c5940a8aea3a53
A-Vamshi <vamshi.edustudy@icloud.com>
2026-06-09 19:48:13 +0530

    .

--- deepeval/confident/api.py
+++ deepeval/confident/api.py
@@ -128,7 +128,6 @@ class Endpoints(Enum):
     DATASET_ALIAS_VERSIONS_ENDPOINT = "/v1/datasets/:alias/versions"
 
     TEST_RUN_ENDPOINT = "/v1/test-run"
-
     EXPERIMENT_ENDPOINT = "/v1/experiment"
     TRACES_ENDPOINT = "/v1/traces"
     ANNOTATIONS_ENDPOINT = "/v1/annotations"
@@ -148,7 +147,6 @@ class Endpoints(Enum):
 
     METRICS_ENDPOINT = "/v1/metrics"
     METRIC_ENDPOINT = "/v1/metric/:name"
-
 
 
 def _sanitize_body(obj):
