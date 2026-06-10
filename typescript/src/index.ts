dbc5f4c1bdded240b6fb4cc6a1ee52cf06084915
A-Vamshi <vamshi.edustudy@icloud.com>
2026-06-11 00:30:22 +0530

    .

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,6 +5,7 @@ export * as dataset from "./dataset";
 export * as testCase from "./test-case";
 export * as tracing from "./tracing";
 export * as openai from "./openai";
+export * as metrics from "./metrics";
 
 // Export common utilities
 export * from "./utils";
@@ -19,6 +20,11 @@ export {
   TurnParams,
   Turn,
 } from "./test-case";
-export { evaluate } from "./confident";
+export {
+  evaluate,
+  type EvaluationResult,
+  type TestResult,
+  type EvaluateOptions,
+} from "./evaluate";
 export { Prompt } from "./prompt";
 export { ConversationSimulator } from "./simulate";
