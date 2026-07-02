6217450b53f2bc3ad0b08df87f5074828dbd21a7
anurag <anurags9693@gmail.com>
2026-07-02 09:37:58 +0530

    fix: add missing confident/evaluate module to fix CI

---
+++ typescript/src/confident/evaluate.ts
@@ -0,0 +1,21 @@
+import { isConfident } from "../utils";
+import { Api, Endpoints, HttpMethods } from "./api";
+import { ConfidentEvaluateRequestData, ConfidentEvaluateResponseData } from "./types";
+
+export async function evaluate(
+  data: ConfidentEvaluateRequestData,
+): Promise<ConfidentEvaluateResponseData | undefined> {
+  if (!isConfident()) {
+    console.error("Confident AI API key not found.");
+    return undefined;
+  }
+
+  const apiKey = process.env.CONFIDENT_API_KEY!;
+  const api = new Api(apiKey);
+  const response = await api.sendRequest(
+    HttpMethods.POST,
+    Endpoints.EVALUATE_ENDPOINT,
+    data,
+  );
+  return response as ConfidentEvaluateResponseData;
+}
