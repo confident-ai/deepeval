f3679776a09cb7c89c3eb227986b227134d0afdd
tanayvaswani <vaswani.tanay9@gmail.com>
2026-08-28 15:35:10 +0530

    feat(dataset): add golden_persona for golden-likes without a persona field

--- deepeval/dataset/__init__.py
+++ deepeval/dataset/__init__.py
@@ -6,6 +6,7 @@ from .golden import (
     Persona,
     BackgroundNoiseSettings,
     InterruptionBehavior,
+    golden_persona,
 )
 
 __all__ = [
@@ -15,5 +16,6 @@ __all__ = [
     "Persona",
     "BackgroundNoiseSettings",
     "InterruptionBehavior",
+    "golden_persona",
     "get_current_golden",
 ]
